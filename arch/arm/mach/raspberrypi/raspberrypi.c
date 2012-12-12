c1b2d97d3df3d3d113c1c949ad6f8e21b8996a2a
James Walmsley <james@fullfat-fs.co.uk>
2012-12-12 22:05:05 +0100

    Added initial zynq and raspberrypi machine descriptions.

---
+++ arch/arm/mach/raspberrypi/raspberrypi.c
@@ -0,0 +1,16 @@
+/**
+ *	This file describes the raspberrypi machine.
+ *
+ *	As well as describing the integrated peripherals, available during boot
+ *	it also provides basic routines for configuring and using them.
+ *
+ **/
+
+#include <bitthunder.h>
+
+
+
+BT_MACHINE_START(ARM, 0, "RaspberryPi Embedded Computer")
+
+
+BT_MACHINE_END
---
+++ arch/arm/mach/zynq/zynq.c
@@ -0,0 +1,24 @@
+#include <bitthunder.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BT_MACHINE_START(ARM, 1, "Xilinx Embedded Zynq Platform")
+
+
+BT_MACHINE_END
