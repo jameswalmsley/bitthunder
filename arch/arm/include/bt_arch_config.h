ddad0f6c9d33a23a4aea4555f80f2861c2a3a306
James Walmsley <james@fullfat-fs.co.uk>
2012-12-12 19:16:20 +0100

    Added primary architecture files for ARM based machines.

---
+++ arch/arm/include/bt_arch_config.h
@@ -0,0 +1,13 @@
+#ifndef _BT_ARCH_CONFIG_H_
+#define _BT_ARCH_CONFIG_H_
+
+
+//#define BT_CONFIG_ARCH_LITTLE_ENDIAN
+#define BT_CONFIG_ARCH_BIG_ENDIAN
+
+
+
+
+
+
+#endif
---
+++ arch/arm/include/bt_arch_types.h
@@ -0,0 +1,20 @@
+#ifndef _BT_ARCH_TYPES_H_
+#define _BT_ARCH_TYPES_H_
+
+typedef long long			BT_i64;
+typedef unsigned long long	BT_u64;
+typedef signed long long	BT_s64;
+
+typedef unsigned long		BT_u32;
+typedef signed long			BT_s32;
+typedef long				BT_i32;
+
+typedef unsigned short		BT_u16;
+typedef signed short		BT_d16;
+typedef short				BT_i16;
+
+typedef unsigned char		BT_u8;
+typedef signed char			BT_s8;
+typedef	char				BT_i8;
+
+#endif
