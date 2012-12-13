2e319725ce566b97cc1aa5b2a2864a55277ed6d4
James Walmsley <james@fullfat-fs.co.uk>
2012-12-13 10:56:58 +0100

    Added automatic arch including in build system.

---
+++ os/.config.mk
@@ -0,0 +1,1 @@
+CFLAGS += -I $(BASE)os/include/
--- os/objects.mk
+++ os/objects.mk
@@ -5,6 +5,9 @@
 #
 #	If we are building a fully fledged OS, then include the real memory manager.
 #
+include $(BASE)os/.config.mk
+
 OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_mm.o
 
-OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)arch/$(ARCH)/mach/$(SUBARCH)/$(SUBARCH).o
+SUB_OBJDIRS += $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/
+#OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)arch/$(ARCH)/mach/$(SUBARCH)/$(SUBARCH).o
