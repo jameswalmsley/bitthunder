0a4d1ce9a7732ae0863ed5edcb19ab73e3c829eb
James Walmsley <james@fullfat-fs.co.uk>
2012-12-13 19:17:58 +0100

    Some special auto-includes for including arch and subarch specific object files. Also define BT_CONFIG_OS for source code level configuration.

--- os/.config.mk
+++ os/.config.mk
@@ -1,1 +1,3 @@
 CFLAGS += -I $(BASE)os/include/
+CFLAGS += -I $(BASE)arch/$(ARCH)/include/
+CFLAGS += -D BT_CONFIG_OS
--- os/objects.mk
+++ os/objects.mk
@@ -9,5 +9,7 @@ include $(BASE)os/.config.mk
 
 OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_mm.o
 
+-include $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/.config.mk
 SUB_OBJDIRS += $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/
+SUB_OBJDIRS += $(BASE)arch/$(ARCH)/
 #OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)arch/$(ARCH)/mach/$(SUBARCH)/$(SUBARCH).o
