ba516c80ae75a2eaecb03231594549d7cd3ae9e3
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 15:40:03 +0100

    Added BT_CloseHandle() API.

--- lib/src/handles/bt_handles.c
+++ lib/src/handles/bt_handles.c
@@ -5,6 +5,9 @@
 
 #include <bitthunder.h>
 
+#ifdef BT_CONFIG_OS
+#include <mm/bt_heap.h>
+#endif
 
 struct _BT_OPAQUE_HANDLE {
 	BT_HANDLE_HEADER h;
@@ -12,5 +15,16 @@ struct _BT_OPAQUE_HANDLE {
 
 BT_HANDLE BT_CreateHandle(const BT_IF_HANDLE *pIf, BT_u32 ulHandleMemory, BT_ERROR *pError) {
 	BT_HANDLE hHandle = BT_Calloc(ulHandleMemory);
+	hHandle->h.pIf = pIf;
+	hHandle->h.ulClaimedMemory = ulHandleMemory;
 	return hHandle;
+}
+
+
+BT_ERROR BT_CloseHandle(BT_HANDLE hHandle) {
+	BT_ERROR Error = hHandle->h.pIf->pfnCleanup(hHandle);
+	if(!Error) {
+		BT_kFree(hHandle);
+	}
+	return Error;
 }
