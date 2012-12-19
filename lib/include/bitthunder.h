6e14bb56bd3064b521d4159817050495500d3a87
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 11:47:51 +0100

    Added a BT_EXPORT_SYMBOL macro, may be useful in the future.
    Also added bt_struct to the usual bithunder includes.

--- lib/include/bitthunder.h
+++ lib/include/bitthunder.h
@@ -4,11 +4,13 @@
 #include "bt_config.h"
 #include "bt_types.h"
 #include "bt_error.h"
+#include "bt_struct.h"
 #include "mm/bt_mm.h"
 #include "handles/bt_handle.h"
 #include "machines/bt_machines.h"
 
 #ifdef BT_CONFIG_OS
+#include "bt_export.h"
 #include "devman/bt_devman.h"
 #endif
 
---
+++ os/include/bt_export.h
@@ -0,0 +1,16 @@
+#ifndef _BT_EXPORT_H_
+#define _BT_EXPORT_H_
+
+
+
+#define BT_EXPORT_SYMBOL(sym)
+
+
+
+
+
+
+
+
+#endif
+
