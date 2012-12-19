315ce1bae8e16274b9d9f24ba673a937d344d11f
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 12:52:22 +0100

    Top-level .config.mk file uses a full include, instead of the SUB_OBJDIRS system.
    Allows better customisation of the build process.

--- .config.mk
+++ .config.mk
@@ -3,15 +3,11 @@
 #
 ifeq ($(BT_CONFIG_OS),y)
 BT_CONFIG_LIB=n
-BT_CONFIG_KERNEL=y
-SUB_OBJDIRS += $(BASE)kernel/
 else
 BT_CONFIG_LIB=y
 endif
 
 
-
-
-
 include $(BASE)/os/objects.mk
 include $(BASE)/lib/objects.mk
+include $(BASE)/kernel/objects.mk
