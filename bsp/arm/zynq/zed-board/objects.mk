ace0b9333db05adb3c08a8c46d6da557ff0873a3
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 16:18:30 +0100

    Changes to the ZED linker script to work with some of the BitThunder special sections.

--- bsp/arm/zynq/zed-board/objects.mk
+++ bsp/arm/zynq/zed-board/objects.mk
@@ -1,4 +1,4 @@
-OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/crtinit.o
-#OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/startup.o
+#OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/crtinit.o
+OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/startup.o
 OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/test.o
 OBJECTS += $(BUILD_DIR)bsp/arm/zynq/zed-board/ps7_init.o
--- bsp/arm/zynq/zed-board/test.c
+++ bsp/arm/zynq/zed-board/test.c
@@ -1,16 +1,26 @@
+#include <bitthunder.h>
+
+extern BT_u32 BT_ZYNQ_GetArmPLLFrequency();
 
 
+/**
+ *	This will be running within a FreeRTOS Thread.
+ *
+ *	It is the boot/startup thread, and you can return from it after starting all your own processes.
+ *	However, you can just use it as an already running process.
+ **/
 
-
-int main(void) {
+int main(int argc, char **argv) {
 
 	int i;
 
-	while(1) {
+	int y = BT_ZYNQ_GetArmPLLFrequency();
 
+	while(1) {
+		y += 1;
 		i++;
+		y -=1;
 	}
 }
-
 
 
