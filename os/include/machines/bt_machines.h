bbcb00ee16fbff8ca7c7076a5e6086a164c25cb1
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 16:03:28 +0100

    Updated the machine description definition.

--- os/include/machines/bt_machines.h
+++ os/include/machines/bt_machines.h
@@ -5,6 +5,7 @@
 #include "bt_types.h"
 #include "mach/bt_machine_types.h"
 #include "interfaces/bt_interfaces.h"
+#include "interrupts/bt_interrupts.h"
 
 
 typedef enum {
@@ -20,22 +21,29 @@ typedef struct _BT_MACHINE_DESCRIPTION {
 	BT_MACH_TYPE				eMachType;
 	const BT_i8				   *szpName;
 
+	BT_u32						ulSystemClockHz;
+
+
+	const BT_INTC 				   *pInterruptController;
+
 	BT_u32						ulTotalIRQs;
 
 	// Integrated IRQ Interface
 
 	// Integrated Timer Interface
+	const BT_IF_DEVICE		   *pSystemTimer;
+	BT_u32						ulTimerID;
 
 	// Integrated Terminal Device -- Usually a uart driver.
-	const BT_IF_DEVICE		   *pBootTerminalDevice;			/// Pointer to device supporting chardev if.
-	BT_u32						ulBootTerminalNumber;			/// Which instance number to use.
+	const BT_IF_DEVICE		   *pBootUart;				/// Pointer to device supporting chardev if.
+	BT_u32						ulBootUartID;			/// Which instance number to use.
 
 } BT_MACHINE_DESCRIPTION;
 
 
 
 #define BT_MACHINE_START(_arch,_type,_name)				\
-static const BT_MACHINE_DESCRIPTION __mach_desc##_type	\
+static const BT_MACHINE_DESCRIPTION __bt_mach_desc_##_type	\
 BT_ATTRIBUTE_SECTION(".bt.arch.init") = {				\
 	.eArchitecture 	= BT_ARCH_##_arch,					\
 	.eMachType     	= BT_MACH_##_type,					\
