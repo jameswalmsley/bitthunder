6b35c0c17bcd419dae181ef44a6dbae45db3d91d
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 13:13:19 +0100

    Moved MODULE definitions to bt_module.h.

--- lib/include/interfaces/bt_interfaces.h
+++ lib/include/interfaces/bt_interfaces.h
@@ -4,17 +4,9 @@
 #include "interfaces/bt_if_device.h"
 #include "interfaces/bt_if_module.h"
 
+#include <bt_module.h>
+
 typedef BT_ERROR (*BT_HANDLE_CLEANUP)	(BT_HANDLE hHandle);
-
-#define BT_MODULE_NAME					g_m_ModuleName
-#define BT_MODULE_DESCRIPTION			g_m_ModuleDescription
-#define BT_MODULE_AUTHOR				g_m_ModuleAuthor
-#define BT_MODULE_EMAIL					g_m_ModuleEmail
-
-#define BT_DEF_MODULE_NAME(title)		static const BT_s8 BT_MODULE_NAME[] 		= title;
-#define BT_DEF_MODULE_DESCRIPTION(info)	static const BT_s8 BT_MODULE_DESCRIPTION[] 	= info;
-#define BT_DEF_MODULE_AUTHOR(name) 		static const BT_s8 BT_MODULE_AUTHOR[] 		= name;
-#define BT_DEF_MODULE_EMAIL(email) 		static const BT_s8 BT_MODULE_EMAIL[] 		= email;
 
 typedef enum _BT_HANDLE_TYPE {
 	BT_HANDLE_T_INVALID 	= 0,
@@ -32,10 +24,10 @@ typedef union _BT_IF_INTERFACES {
 } BT_UN_IFS;
 
 typedef struct _BT_IF_HANDLE {
-	const BT_s8		   *szpModuleName;
-	const BT_s8		   *szpDescription;
-	const BT_s8		   *szpAuthor;
-	const BT_s8		   *szpEmail;
+	const BT_i8		   *szpModuleName;
+	const BT_i8		   *szpDescription;
+	const BT_i8		   *szpAuthor;
+	const BT_i8		   *szpEmail;
 
 	const BT_UN_IFS	   *pIfs;
 
