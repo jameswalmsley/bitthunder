751e2b91f765dcea49e1bf61ba79100d8307654f
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 16:04:15 +0100

    Added a bt_main() function as the kernel entry point.
    Currently this code is purely for testing and getting things booted,
    but it will probably become the standard BitThunder kernel entry point.

--- os/objects.mk
+++ os/objects.mk
@@ -7,9 +7,29 @@
 #
 include $(BASE)os/.config.mk
 
-OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_mm.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/bt_main.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_mm.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_heap.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/devman/bt_devman.o
 
--include $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/.config.mk
-SUB_OBJDIRS += $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/
-SUB_OBJDIRS += $(BASE)arch/$(ARCH)/
-#OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)arch/$(ARCH)/mach/$(SUBARCH)/$(SUBARCH).o
+include $(BASE)os/src/interfaces/objects.mk
+
+
+include $(BASE)arch/$(ARCH)/objects.mk
+include $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/.config.mk
+include $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/objects.mk
+#SUB_OBJDIRS += $(BASE)arch/$(ARCH)/mach/$(SUBARCH)/
+#SUB_OBJDIRS += $(BASE)arch/$(ARCH)/
+
+test2:
+	echo $(BT_CONFIG_KERNEL)
+ifeq ($(BT_CONFIG_KERNEL), FreeRTOS)
+$(BUILD_DIR)os/src/mm/bt_heap.o: CFLAGS += -DBT_CONFIG_KERNEL_FREERTOS
+$(BUILD_DIR)os/src/mm/bt_mm.o: CFLAGS += -DBT_CONFIG_KERNEL_FREERTOS
+endif
+
+
+BT_OS_OBJECTS += $(BT_OS_OBJECTS-y)
+$(BT_OS_OBJECTS): MODULE_NAME="BitThunder"
+
+OBJECTS += $(BT_OS_OBJECTS)
---
+++ os/src/bt_main.c
@@ -0,0 +1,74 @@
+/**
+ *	This is the operating system main entry point, from where it will boot and load
+ *	kernel modules.
+ *
+ *	Once all kernel modules are loaded and initialised, we shall look user application
+ *	processes to be loaded.
+ *
+ **/
+
+#include <bitthunder.h>
+#include <mm/bt_heap.h>
+#include <string.h>
+
+extern int main(int argc, char **argv);
+
+/**
+ *	@ Note: argc and argv may be used for kernel booting parameters at a later date.
+ *
+ *
+ **/
+int bt_main(int argc, char **argv) {
+
+	BT_ERROR Error;
+
+	// Get Machine Description.
+	BT_MACHINE_DESCRIPTION *pMachine = BT_GetMachineDescription(&Error);
+	if(pMachine->szpName) {
+		Error = 0;
+	}
+
+	BT_HANDLE hUart = pMachine->pBootUart->pfnOpen(pMachine->ulBootUartID, &Error);
+	if(!hUart) {
+		// Well, we're completely out of luck! We cannot report this to anything.
+		return -1;
+	}
+
+	BT_UART_CONFIG oConfig;
+	//BT_SetPowerState(hUart, BT_POWER_STATE_AWAKE);
+
+	oConfig.eMode 		= BT_UART_MODE_POLLED;
+	oConfig.ucDataBits 	= 8;
+	oConfig.ulBaudrate 	= 115200;
+
+	BT_UartSetConfiguration(hUart, &oConfig);
+
+	BT_UartEnable(hUart);
+
+	char *string = "BitThunder v0.0.1\r\n";
+	BT_CharDeviceWrite(hUart, 0, strlen(string), (BT_u8 *)string);
+
+	string = "Start Loading kernel modules...\r\n";
+	BT_CharDeviceWrite(hUart, 0, strlen(string), (BT_u8 *)string);
+
+	string = "Initialise interrupt controller...\r\n";
+	BT_CharDeviceWrite(hUart, 0, strlen(string), (BT_u8 *)string);
+
+	Error = pMachine->pInterruptController->pfnInitialise();
+
+	// Start Scheduler
+	string = "Enter user-mode, and start user-space application...\r\n";
+	BT_CharDeviceWrite(hUart, 0, strlen(string), (BT_u8 *)string);
+
+	string = "Relinquish control of the boot UART device...(Goodbye)\r\n";
+	BT_CharDeviceWrite(hUart, 0, strlen(string), (BT_u8 *)string);
+
+	BT_CloseHandle(hUart);
+
+	int retval = main(argc, argv);
+
+	// Write a debug message to the debugger port,
+	//	The main application has quit on us!
+
+	return retval;
+}
