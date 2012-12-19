211b6614c8f83416d5a838793ce54ccc5c57b930
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 16:12:12 +0100

    BSP:ZED-BOARD - Updated the DarkBuild compiler configuration for ZED-BOARD BSP.

--- bsp/arm/zynq/zed-board/dbuild.config.mk
+++ bsp/arm/zynq/zed-board/dbuild.config.mk
@@ -1,1 +1,1 @@
-CFLAGS += -g -Wall -Werror -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
+CFLAGS += -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=softfp -g -Wall -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
