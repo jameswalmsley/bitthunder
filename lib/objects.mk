bd4d071af0ae254565acf25516950784ea075a57
James Walmsley <james@fullfat-fs.co.uk>
2012-12-19 15:39:28 +0100

    Update the BitThunder library objects.mk file.

--- lib/objects.mk
+++ lib/objects.mk
@@ -1,8 +1,13 @@
-OBJECTS += $(BUILD_DIR)lib/src/handles/bt_handles.o
+BT_LIB_OBJECTS += $(BUILD_DIR)lib/src/handles/bt_handles.o
 
 
 #
 #	Include the correct Memory Manager implementation.
 #
-OBJECTS-$(BT_CONFIG_LIB) += $(BUILD_DIR)lib/src/mm/bt_mm.o
+BT_LIB_OBJECTS-$(BT_CONFIG_LIB) += $(BUILD_DIR)lib/src/mm/bt_mm.o
 
+BT_LIB_OBJECTS += $(BT_LIB_OBJECTS-y)
+
+$(BT_LIB_OBJECTS): MODULE_NAME="BitThunder"
+
+OBJECTS += $(BT_LIB_OBJECTS)
