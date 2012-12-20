0dab322c1414ec43f803cbad22d4696f03be94de
James Walmsley <james@fullfat-fs.co.uk>
2012-12-20 15:03:24 +0100

    Added a configuration flag for the interrupt controller API.
    BT_CONFIG_MAX_INTERRUPT_CONTROLLERS.
    
    This defines the size of the interrupt controller array in the bt_interrupts.c file.
    Defaults to 1.
    
    During kernel startup, this machine/system interrupt controller will be registered.
    Any other interrupt sources must be later registered by a kernel module,
    or simply from the boot thread of the user application.

--- os/objects.mk
+++ os/objects.mk
@@ -10,6 +10,7 @@ include $(BASE)os/.config.mk
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/bt_main.o
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_mm.o
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/mm/bt_heap.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/interrupts/bt_interrupts.o
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/devman/bt_devman.o
 
 include $(BASE)os/src/interfaces/objects.mk
@@ -28,6 +29,7 @@ $(BUILD_DIR)os/src/mm/bt_heap.o: CFLAGS += -DBT_CONFIG_KERNEL_FREERTOS
 $(BUILD_DIR)os/src/mm/bt_mm.o: CFLAGS += -DBT_CONFIG_KERNEL_FREERTOS
 endif
 
+$(BUILD_DIR)os/src/interrupts/bt_interrupts.o: CFLAGS += -DBT_CONFIG_MAX_INTERRUPT_CONTROLLERS=$(BT_CONFIG_MAX_INTERRUPT_CONTROLLERS)
 
 BT_OS_OBJECTS += $(BT_OS_OBJECTS-y)
 $(BT_OS_OBJECTS): MODULE_NAME="BitThunder"
