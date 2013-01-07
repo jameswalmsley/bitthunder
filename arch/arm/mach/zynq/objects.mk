1d0832fe953589b3a8759c5131e1ea72e6568611
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:15:56 +0100

    [BSP-ZYNQ] Updates objects.mk and also added crtinit.s should it really be here?

--- arch/arm/mach/zynq/objects.mk
+++ arch/arm/mach/zynq/objects.mk
@@ -11,8 +11,11 @@ MACH_ZYNQ_OBJECTS-$(BT_CONFIG_MACH_ZYNQ_USE_STARTUP) += $(BUILD_DIR)arch/arm/mac
 MACH_ZYNQ_OBJECTS += $(BUILD_DIR)arch/arm/mach/zynq/slcr.o
 MACH_ZYNQ_OBJECTS += $(BUILD_DIR)arch/arm/mach/zynq/uart.o
 MACH_ZYNQ_OBJECTS += $(BUILD_DIR)arch/arm/mach/zynq/timer.o
+MACH_ZYNQ_OBJECTS += $(BUILD_DIR)arch/arm/mach/zynq/gpio.o
 
-
+$(BUILD_DIR)arch/arm/mach/zynq/zynq.o: CFLAGS += -DBT_CONFIG_ARCH_ARM_GIC_BASE=$(BT_CONFIG_ARCH_ARM_GIC_BASE)
+$(BUILD_DIR)arch/arm/mach/zynq/zynq.o: CFLAGS += -DBT_CONFIG_ARCH_ARM_GIC_DIST_BASE=$(BT_CONFIG_ARCH_ARM_GIC_DIST_BASE)
+$(BUILD_DIR)arch/arm/mach/zynq/zynq.o: CFLAGS += -DBT_CONFIG_ARCH_ARM_GIC_TOTAL_IRQS=$(BT_CONFIG_ARCH_ARM_GIC_TOTAL_IRQS)
 
 MACH_ZYNQ_OBJECTS += $(MACH_ZYNQ_OBJECTS-y)
 
