150f73aac9caff4c26d0f31ee4733a8b8cb24f93
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:22:57 +0100

    [BSP-ZED] Updated configuration to provide correct config variables to build.

--- bsp/arm/zynq/zed-board/.config.mk
+++ bsp/arm/zynq/zed-board/.config.mk
@@ -12,10 +12,14 @@ BT_CONFIG_OS=y
 BT_CONFIG_KERNEL=FreeRTOS
 #BT_CONFIG_KERNEL=BtKernel
 
+#
+#	Define the maximum number of IRQs allowed in the system.
+#	This should cover all interrupts controllers.
+#
+BT_CONFIG_MAX_IRQ=95
 
 ARCH=arm
 SUBARCH=zynq
-
 #
 #	Arch Specific configuration.
 #
@@ -29,6 +33,7 @@ BT_CONFIG_MACH_ZYNQ_SYSCLOCK_FREQ=33333333
 BT_CONFIG_MACH_ZYNQ_SYSTICK_TIMER_ID=0
 BT_CONFIG_MACH_ZYNQ_BOOT_UART_ID=1
 BT_CONFIG_MAX_INTERRUPT_CONTROLLERS=2
+BT_CONFIG_MAX_GPIO_CONTROLLERS=1
 
 
 include $(BASE).config.mk
