966ec53abe9b14f21f02f56c42546d96d633700b
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 19:20:21 +0100

    Cleanup up the CharDevice interface API definition.

--- os/include/interfaces/bt_if_chardev.h
+++ os/include/interfaces/bt_if_chardev.h
@@ -7,19 +7,14 @@
  *	Here we define the main "function" prototypes that such a device may encounter.
  **/
 
-typedef BT_ERROR (*BT_IF_CHARDEV_READ)		(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, BT_u8 *pucDest);
-typedef BT_ERROR (*BT_IF_CHARDEV_WRITE)		(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, const BT_u8 *pucSource);
-typedef BT_ERROR (*BT_IF_CHARDEV_GETCH)		(BT_HANDLE hDevice, BT_u32 ulFlags);
-typedef BT_ERROR (*BT_IF_CHARDEV_PUTCH)		(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u8 ucData);
-
 typedef struct {
-	BT_IF_CHARDEV_READ	pfnRead;
-	BT_IF_CHARDEV_WRITE	pfnWrite;
-	BT_IF_CHARDEV_GETCH	pfnGetch;
-	BT_IF_CHARDEV_PUTCH	pfnPutch;
+	BT_ERROR	(*pfnRead) 	(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, BT_u8 *pucDest);
+	BT_ERROR	(*pfnWrite)	(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, const BT_u8 *pucSource);
+	BT_ERROR	(*pfnGetch)	(BT_HANDLE hDevice, BT_u32 ulFlags);
+	BT_ERROR	(*pfnPutch)	(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u8 ucData);
 } BT_IF_CHARDEV;
 
-BT_ERROR BT_CharDeviceRead(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, BT_u8 *pucDest);
-BT_ERROR BT_CharDeviceWrite(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, const BT_u8 *pucSource);
+BT_ERROR BT_CharDeviceRead	(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, BT_u8 *pucDest);
+BT_ERROR BT_CharDeviceWrite	(BT_HANDLE hDevice, BT_u32 ulFlags, BT_u32 ulSize, const BT_u8 *pucSource);
 
 #endif
