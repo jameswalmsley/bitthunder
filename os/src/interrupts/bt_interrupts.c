d3812d8c19a910f1960085bb0910b0379ed0197b
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:19:41 +0100

    Interrupt API management implementation.

--- os/src/interrupts/bt_interrupts.c
+++ os/src/interrupts/bt_interrupts.c
@@ -1,38 +1,59 @@
 #include <bitthunder.h>
 #include <interrupts/bt_interrupts.h>
 
-typedef struct _BT_INTERRUPT_CONTROLLERS {
-	const BT_INTERRUPT_CONTROLLER  *pIntc;			///< Interrupt controller's device interface.
+typedef struct _BT_INTERRUPT_CONTROLLER {
+	BT_HANDLE						hIRQ;
 	BT_u32							ulBaseIRQ;		///< Base IRQ number.
 	BT_u32							ulTotalIRQs;	///< Total IRQ lines.
-} BT_INTERRUPT_CONTROLLERS;
+} BT_INTERRUPT_CONTROLLER;
 
-static BT_INTERRUPT_CONTROLLERS g_oControllers[BT_CONFIG_MAX_INTERRUPT_CONTROLLERS];
+static BT_INTERRUPT_CONTROLLER g_oControllers[BT_CONFIG_MAX_INTERRUPT_CONTROLLERS];
 static BT_u32 g_ulRegistered = 0;
 
-BT_ERROR BT_RegisterInterruptController(BT_u32 ulBaseIRQ, BT_u32 ulTotalIRQs, const BT_INTERRUPT_CONTROLLER *pIntc) {
+struct _BT_OPAQUE_HANDLE {
+	BT_HANDLE_HEADER h;
+};
 
-	BT_ERROR Error;
+void BT_IRQHandler(BT_u32 ulIRQ) {
+
+}
+
+BT_ERROR BT_RegisterInterruptController(BT_u32 ulBaseIRQ, BT_u32 ulTotalIRQs, BT_HANDLE hIRQ) {
+
+	BT_ERROR Error = BT_ERR_NONE;
 
 	if(g_ulRegistered >= BT_CONFIG_MAX_INTERRUPT_CONTROLLERS) {
 		return -1;	/// Maximum controllers already registered.
 	}
 
-	g_oControllers[g_ulRegistered].pIntc 		= pIntc;
+	g_oControllers[g_ulRegistered].hIRQ       	= hIRQ;
+
+	// Ensure there is no conflict.
 	g_oControllers[g_ulRegistered].ulBaseIRQ 	= ulBaseIRQ;
 	g_oControllers[g_ulRegistered].ulTotalIRQs 	= ulTotalIRQs;
 
 	g_ulRegistered++;
 
-	Error = pIntc->pfnInitialise(ulTotalIRQs);
-	if(Error) {
-		g_ulRegistered--;
-	}
-
 	return Error;
 }
 
-static const BT_INTERRUPT_CONTROLLERS *getInterruptController(BT_u32 ulIRQ) {
+BT_ERROR BT_CleanupInterruptControllers() {
+	BT_u32 i;
+	BT_ERROR Error;
+
+	for(i=0; i < g_ulRegistered; i++) {
+
+		BT_CloseHandle(g_oControllers[i].hIRQ);
+		if(Error) {
+			//BT_kPrintf("Error cleaning %s", );
+		}
+	}
+
+	return BT_ERR_NONE;
+}
+
+
+static BT_INTERRUPT_CONTROLLER *getInterruptController(BT_u32 ulIRQ) {
 	BT_u32 i;
 
 	for(i=0; i < g_ulRegistered; i++) {
@@ -49,34 +70,41 @@ static const BT_INTERRUPT_CONTROLLERS *getInterruptController(BT_u32 ulIRQ) {
 }
 
 BT_ERROR BT_RegisterInterrupt(BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
 
-	return pIntc->pIntc->pfnRegister(ulIRQ-pIntc->ulBaseIRQ, pfnHandler, pParam);
+	return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnRegister(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ, pfnHandler, pParam);
 }
 
 BT_ERROR BT_UnregisterInterrupt(BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+
+	BT_ERROR Error;
+
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
 
-	return pIntc->pIntc->pfnUnregister(ulIRQ-pIntc->ulBaseIRQ, pfnHandler, pParam);
+	BT_DisableInterrupt(ulIRQ);
+
+	Error = pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnUnregister(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ, pfnHandler, pParam);
+
+	return Error;
 }
 
 BT_ERROR BT_SetInterruptPriority(BT_u32 ulIRQ, BT_u32 ulPriority) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
 
-	return pIntc->pIntc->pfnSetPriority(ulIRQ-pIntc->ulBaseIRQ, ulPriority);
+	return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnSetPriority(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ, ulPriority);
 }
 
 BT_u32 BT_GetInterruptPriority(BT_u32 ulIRQ, BT_ERROR *pError) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		if(pError) {
 			*pError = -1;
@@ -84,33 +112,33 @@ BT_u32 BT_GetInterruptPriority(BT_u32 ulIRQ, BT_ERROR *pError) {
 		return 0;
 	}
 
-	return pIntc->pIntc->pfnGetPriority(ulIRQ-pIntc->ulBaseIRQ, pError);
+	return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnGetPriority(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ, pError);
 }
 
 BT_ERROR BT_EnableInterrupt(BT_u32 ulIRQ) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
-	return pIntc->pIntc->pfnEnable(ulIRQ-pIntc->ulBaseIRQ);
+	return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnEnable(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ);
 }
 
 BT_ERROR BT_DisableInterrupt(BT_u32 ulIRQ) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
-	return pIntc->pIntc->pfnDisable(ulIRQ-pIntc->ulBaseIRQ);
+	return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnDisable(pIntc->hIRQ, ulIRQ-pIntc->ulBaseIRQ);
 }
 
 BT_ERROR BT_SetInterruptAffinity(BT_u32 ulIRQ, BT_u32 ulCPU) {
-	const BT_INTERRUPT_CONTROLLERS *pIntc = getInterruptController(ulIRQ);
+	const BT_INTERRUPT_CONTROLLER *pIntc = getInterruptController(ulIRQ);
 	if(!pIntc) {
 		return -1;
 	}
 
-	if(pIntc->pIntc->pfnSetAffinity) {
-		return pIntc->pIntc->pfnSetAffinity(ulIRQ, ulCPU);
+	if(pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnSetAffinity) {
+		return pIntc->hIRQ->h.pIf->oIfs.pDevIF->unConfigIfs.pIRQIF->pfnSetAffinity(pIntc->hIRQ, ulIRQ, ulCPU);
 	}
 
 	return -1;
