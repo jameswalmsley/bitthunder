3578b45a731d42131a98e3c5672f59c0e4344f65
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:12:26 +0100

    Added a BCM2835 machine type (raspberrypi).

--- arch/arm/include/mach/bt_machine_types.h
+++ arch/arm/include/mach/bt_machine_types.h
@@ -1,19 +1,12 @@
 #ifndef _BT_MACHINE_TYPES_H_
 #define _BT_MACHINE_TYPES_H_
 
-
-
-
 typedef enum _BT_MACH_TYPE {
 	BT_MACH_NONE=0,
 	BT_MACH_CORTEX_M3,
 	BT_MACH_ZYNQ,
+	BT_MACH_BCM2835,
 } BT_MACH_TYPE;
-
-
-
-
-
 
 
 
