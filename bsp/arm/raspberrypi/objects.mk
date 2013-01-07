d08129a99023455875b71e2bd07c57082e3e824e
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:25:59 +0100

    [BSP-RPi] Cleanup objects for RPi

--- bsp/arm/raspberrypi/objects.mk
+++ bsp/arm/raspberrypi/objects.mk
@@ -1,24 +1,2 @@
-ARCH=arm
-SUBARCH=raspberrypi
-
 OBJECTS += $(BUILD_DIR)bsp/arm/raspberrypi/startup.o
 OBJECTS += $(BUILD_DIR)bsp/arm/raspberrypi/init.o
-
-
-
-#
-#	Compile in the BitThunder library code.
-#
-#	The actual objects included depends on your .config.mk file.
-#
-SUB_OBJDIRS += $(BASE)lib/
-
-#
-#	Compile in the BitThunder as an Operating System code.
-#
-#
-#	This is found in your .config.mk file...
-#	Note almost all if this will be excluded if BT_CONFIG_OS != y
-#
-SUB_OBJDIRS += $(BASE)os/
-
