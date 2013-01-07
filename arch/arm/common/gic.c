1ab00dcdbef5bef192e8261692cb5fda6ec7e2db
James Walmsley <james@fullfat-fs.co.uk>
2013-01-07 20:11:58 +0100

    Major updates to ARM GIC implementation. Basic functionality working.

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -1,70 +1,150 @@
+/**
+ *	ARM Generic Interrupt Controller Driver for BitThunder.
+ *
+ *	@author	James Walmsley
+ *
+ **/
 #include <bitthunder.h>
 #include <arch/common/gic.h>
 #include <interrupts/bt_interrupts.h>
 #include <bt_module.h>
+#include <mm/bt_heap.h>
 
 BT_DEF_MODULE_NAME				("Arm GIC")
 BT_DEF_MODULE_DESCRIPTION		("Provides a complete interrupt controller interface for BitThunder")
 BT_DEF_MODULE_AUTHOR			("James Walmsley")
 BT_DEF_MODULE_EMAIL				("james@fullfat-fs.co.uk")
 
-#ifndef BT_CONFIG_ARCH_ARM_GIC_BASE
-#error "Error: BT_CONFIG_ARCH_ARM_GIC_BASE not defined in HAL."
+#ifndef BT_CONFIG_ARCH_ARM_GIC_TOTAL_IRQS
+#error "BT_CONFIG_ARCH_GIC_TOTAL_IRQS not defined in platform abstraction layer."
 #endif
 
-#ifndef BT_CONFIG_ARCH_ARM_GIC_DIST_BASE
-#error "Error: BT_CONFIG_ARCH_ARM_GIC_DIST_BASE not defined in HAL."
-#endif
+static BT_HANDLE				g_hActiveHandle = NULL;
+static BT_INTERRUPT_VECTOR 		g_oVectorTable[BT_CONFIG_ARCH_ARM_GIC_TOTAL_IRQS];
 
-#define GIC_BASE 		BT_CONFIG_ARCH_ARM_GIC_BASE
-#define GIC_DIST_BASE	BT_CONFIG_ARCH_ARM_GIC_DIST_BASE
+struct _BT_OPAQUE_HANDLE {
+	BT_HANDLE_HEADER 			h;
+	BT_u32						ulBaseIRQ;
+	BT_u32						ulGicIRQs;
+	GICC_REGS 		   	   	   *pGICC;
+	GICD_REGS 		   	   	   *pGICD;
+};
 
-#define GICC	((GICC_REGS *) GIC_BASE)
-#define GICD	((GICD_REGS *) GIC_DIST_BASE)
+void test_gic(void) {
+	BT_HANDLE hGic = g_hActiveHandle;
+	BT_u32 i = 0;
+	i += 16;
+}
 
-static BT_ERROR gic_initialise(BT_u32 ulTotalIRQs) {
-	//GICC_REGS *pCRegs = GICC;
-	//GICD_REGS *pDRegs = GICD;
+extern void enable_irq(void);
+
+void BT_ARCH_ARM_GIC_IRQHandler() {
+	BT_HANDLE hGic = g_hActiveHandle;
+
+	BT_u32 ulStatus, ulIRQ;
+
+	do {
+		ulStatus 	= hGic->pGICC->IAR;		// receive the first interrupt from the IAR.
+		ulIRQ 		= ulStatus & 0x01FF;	// Get the IRQ number.
+		ulIRQ 	   += hGic->ulBaseIRQ;		// Remap the IRQn into logical IRQ# space.
+		g_oVectorTable[ulIRQ].pfnHandler(ulIRQ, g_oVectorTable[ulIRQ].pParam);
+		hGic->pGICC->EOIR = ulIRQ;
+	} while(ulIRQ < 1020);
+}
+
+static BT_ERROR gic_stubhandler(BT_u32 ulIRQ, void *pParam) {
+	return BT_ERR_NONE;
+}
+
+static BT_ERROR gic_cleanup(BT_HANDLE hGic) {
+	return BT_ERR_NONE;
+}
+
+static void gic_dist_init(BT_HANDLE hGic) {
+	BT_u32 i;
+	BT_u32 ulGicIRQs = hGic->ulGicIRQs;
+
+	hGic->pGICD->CTLR = 0;
+
+	// Set all global interrupts to be level triggered (active low).
+	for(i = 32; i < ulGicIRQs; i += 16) {
+		hGic->pGICD->ICFGR[i/16] = 0;
+	}
+
+	BT_u32 ulCPUMask = 1 	<< 0;
+	ulCPUMask |= ulCPUMask 	<< 8;
+	ulCPUMask |= ulCPUMask 	<< 16;
+
+	for(i = 32; i < ulGicIRQs; i += 4) {
+		hGic->pGICD->ITARGETSR[i/4] = ulCPUMask;
+		hGic->pGICD->IPRIORITYR[i/4] = 0xA0A0A0A0;
+	}
+
+	for(i = 32; i < ulGicIRQs; i += 32) {
+		hGic->pGICD->ICENABLER[i/32] = 0xFFFFFFFF;
+	}
+
+	hGic->pGICD->CTLR = 1;
+}
+
+static void gic_cpu_init(BT_HANDLE hGic) {
+
+	BT_u32 i;
+
+	hGic->pGICD->ICENABLER[0] = 0xFFFF0000;
+	hGic->pGICD->ISENABLER[0] = 0x0000FFFF;
+
+	for(i = 0; i < 32; i += 4) {
+		hGic->pGICD->IPRIORITYR[i/4] = 0xA0A0A0A0;
+	}
+
+	hGic->pGICC->PMR 	= 0xF0;
+	hGic->pGICC->CTLR 	= 1;
+}
+
+static BT_ERROR gic_register(BT_HANDLE hGic, BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
+	if(g_oVectorTable[ulIRQ].pfnHandler != gic_stubhandler) {
+		return BT_ERR_GENERIC;
+	}
+
+	g_oVectorTable[ulIRQ].pfnHandler 	= pfnHandler;
+	g_oVectorTable[ulIRQ].pParam 		= pParam;
 
 	return BT_ERR_NONE;
 }
 
-static BT_ERROR gic_cleanup() {
+static BT_ERROR gic_unregister(BT_HANDLE hGic, BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
+	if(g_oVectorTable[ulIRQ].pfnHandler != pfnHandler) {
+		return BT_ERR_GENERIC;
+	}
+
+	g_oVectorTable[ulIRQ].pfnHandler 	= gic_stubhandler;
+	g_oVectorTable[ulIRQ].pParam 		= NULL;
+
 	return BT_ERR_NONE;
 }
 
-static BT_ERROR gic_register(BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
+static BT_ERROR gic_setpriority(BT_HANDLE hGic, BT_u32 ulIRQ, BT_u32 ulPriority) {
 	return BT_ERR_NONE;
 }
 
-static BT_ERROR gic_unregister(BT_u32 ulIRQ, BT_FN_INTERRUPT_HANDLER pfnHandler, void *pParam) {
-	return BT_ERR_NONE;
-}
-
-static BT_ERROR gic_setpriority(BT_u32 ulIRQ, BT_u32 ulPriority) {
-	return BT_ERR_NONE;
-}
-
-static BT_u32 gic_getpriority(BT_u32 ulIRQ, BT_ERROR *pError) {
+static BT_u32 gic_getpriority(BT_HANDLE hGic, BT_u32 ulIRQ, BT_ERROR *pError) {
 	return 0;
 }
 
-static BT_ERROR gic_enable(BT_u32 ulIRQ) {
+static BT_ERROR gic_enable(BT_HANDLE hGic, BT_u32 ulIRQ) {
+	BT_u32 ulMask = 1 << (ulIRQ %32);
+	hGic->pGICD->ISENABLER[ulIRQ/32] = ulMask;
 	return BT_ERR_NONE;
 }
 
-static BT_ERROR gic_disable(BT_u32 ulIRQ) {
+static BT_ERROR gic_disable(BT_HANDLE hGic, BT_u32 ulIRQ) {
+	BT_u32 ulMask = 1 << (ulIRQ % 32);
+	hGic->pGICD->ICENABLER[ulIRQ/32] = ulMask;
 	return BT_ERR_NONE;
 }
 
-static BT_u32 gic_get_total_irqs(BT_ERROR *pError) {
-	return 0;
-}
-
-static const BT_INTERRUPT_CONTROLLER oDeviceInterface = {
-	.oModuleInfo 		= BT_MODULE_DEF_INFO,
-	.pfnInitialise 		= gic_initialise,
-	.pfnCleanup			= gic_cleanup,
+static const BT_DEV_IF_IRQ oDeviceOps = {
 	.pfnRegister		= gic_register,
 	.pfnUnregister		= gic_unregister,
 	.pfnSetPriority		= gic_setpriority,
@@ -72,5 +152,116 @@ static const BT_INTERRUPT_CONTROLLER oDeviceInterface = {
 	.pfnEnable			= gic_enable,
 	.pfnDisable			= gic_disable,
 	.pfnSetAffinity		= NULL,						///< An option interface, GIC could implement this.
-	.pfnGetTotalIRQs	= gic_get_total_irqs,
+};
+
+static const BT_IF_DEVICE oDeviceInterface = {
+	.eConfigType = BT_DEV_IF_T_INTC,
+	.unConfigIfs = {
+		.pIRQIF = &oDeviceOps,
+	},
+};
+
+static const BT_IF_HANDLE oHandleInterface = {
+	BT_MODULE_NAME,
+	BT_MODULE_DESCRIPTION,
+	BT_MODULE_AUTHOR,
+	BT_MODULE_EMAIL,
+	.pfnCleanup = gic_cleanup,
+	.oIfs = {
+		(BT_HANDLE_INTERFACE) &oDeviceInterface,
+	},
+};
+
+static const BT_INTEGRATED_DRIVER oIntegratedDriver;
+
+static BT_HANDLE gic_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError) {
+
+	BT_ERROR Error;
+
+	if(g_hActiveHandle) {
+		if(pError) {
+			*pError = BT_ERR_GENERIC;
+		}
+		return NULL;
+	}
+
+	BT_HANDLE hGic = BT_CreateHandle(&oHandleInterface, sizeof(struct _BT_OPAQUE_HANDLE), pError);
+	if(!hGic) {
+		goto err_out;
+	}
+
+	const BT_RESOURCE *pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_MEM, 0);
+	if(!pResource) {
+		Error = BT_ERR_NO_MEMORY;
+		goto  err_free_chip;
+	}
+
+	hGic->pGICC = (GICC_REGS *) pResource->ulStart;
+
+	pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_MEM, 1);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_chip;
+	}
+
+	hGic->pGICD = (GICD_REGS *) pResource->ulStart;
+
+	// Set private members.
+
+	pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_IRQ, 0);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_chip;
+	}
+
+	BT_u32 base 	= pResource->ulStart;
+	BT_u32 total 	= (pResource->ulEnd - pResource->ulStart) + 1;
+
+	hGic->ulBaseIRQ = base;
+
+	BT_u32 i;
+	for(i = 0; i < total; i++) {
+		g_oVectorTable[i].pfnHandler 	= gic_stubhandler;
+		g_oVectorTable[i].pParam 	 	= NULL;
+	}
+
+	Error = BT_RegisterInterruptController(base, total, hGic);
+	if(Error) {
+		goto err_free_chip;
+	}
+
+	g_hActiveHandle = hGic;
+
+	// Enable the Distributor and CPU interfaces.
+
+	BT_u32 ulGicIRQs = hGic->pGICD->TYPER;
+	ulGicIRQs = (ulGicIRQs + 1) * 32;
+
+	if(ulGicIRQs > 1020) {
+		ulGicIRQs = 1020;
+	}
+
+	hGic->ulGicIRQs = ulGicIRQs;
+
+	gic_dist_init(hGic);
+	gic_cpu_init(hGic);
+
+	enable_irq();
+
+	return hGic;
+
+err_free_chip:
+	BT_kFree(hGic);
+
+	if(pError) {
+		*pError = Error;
+	}
+
+err_out:
+	return NULL;
+}
+
+BT_INTEGRATED_DRIVER_DEF oIntegratedDriver = {
+	.name		= "arm,common,gic",
+	.pfnProbe 	= gic_probe,
 };
