eb40b1ec3d1ec42b16fa1ae4ac149be84ea5d278
James Walmsley <james@fullfat-fs.co.uk>
2013-01-23 15:00:48 +0100

    Added a kernel level API, for kernel plugin abstraction.

---
+++ os/include/bt_kernel.h
@@ -0,0 +1,19 @@
+#ifndef _BT_KERNEL_H_
+#define _BT_KERNEL_H_
+
+#include <bt_types.h>
+#include <process/bt_threads.h>
+
+typedef void (*BT_FN_TASK_ENTRY)(void *pParam);
+
+BT_ERROR 	BT_kStartScheduler	();
+void 	   *BT_kTaskCreate		(BT_FN_TASK_ENTRY pfnStartRoutine, const BT_i8 *szpName, BT_THREAD_CONFIG *pConfig, BT_ERROR *pError);
+void		BT_kTaskDelete 		(void *pTaskHandle);
+
+BT_TICK		BT_kTickCount		();
+void		BT_kTaskDelay		(BT_TICK ulTicks);
+void		BT_kTaskDelayUntil 	(BT_TICK *pulPreviousWakeTime, BT_TICK ulTimeIncrement);
+
+
+
+#endif
