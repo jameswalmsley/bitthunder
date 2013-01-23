5c0f38b30ce3d2974ec94fc14b680e7bb7a261b5
James Walmsley <james@fullfat-fs.co.uk>
2013-01-23 15:28:09 +0100

    [LIB] Updated handle API definition.

--- lib/include/handles/bt_handle.h
+++ lib/include/handles/bt_handle.h
@@ -4,24 +4,17 @@
 #include "bt_types.h"
 #include "interfaces/bt_interfaces.h"
 
-
-
+/**
+ *	This is the primary object of BitThunder.
+ *	Every HANDLE must be derived/based on this structure.
+ **/
 typedef struct _BT_HANDLE_HEADER {
 	const BT_IF_HANDLE	   *pIf;				///< Pointer to the handle interface.
 	BT_u32					ulClaimedMemory;	///< Amount of memory claimed by this handle.
 	BT_HANDLE				hNext;				///< Pointer to the next handle in the list.
 } BT_HANDLE_HEADER;
 
-
-
-
-
-BT_HANDLE BT_CreateHandle(const BT_IF_HANDLE *pIf, BT_u32 ulHandleMemory, BT_ERROR *pError);
-BT_ERROR 	BT_CloseHandle(BT_HANDLE hHandle);
-
-
-
-
-
+BT_HANDLE 	BT_CreateHandle	(const BT_IF_HANDLE *pIf, BT_u32 ulHandleMemory, BT_ERROR *pError);
+BT_ERROR 	BT_CloseHandle	(BT_HANDLE hHandle);
 
 #endif
