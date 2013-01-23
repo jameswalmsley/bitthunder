97a4eed0f11c1d44d88c8caeb31e7254ea4c005d
James Walmsley <james@fullfat-fs.co.uk>
2013-01-23 15:30:54 +0100

    [API] Added some machine API.

---
+++ os/src/machines/bt_machines.c
@@ -0,0 +1,21 @@
+/**
+ *	Some APIs for accessing information from the architectures machine description.
+ *
+ *	@author James Walmsley
+ **/
+
+#include <bt_types.h>
+#include <machines/bt_machines.h>
+#include <devman/bt_devman.h>
+
+BT_u32 BT_GetCpuClockFrequency() {
+
+	BT_ERROR Error;
+
+	BT_MACHINE_DESCRIPTION *pMachine = BT_GetMachineDescription(&Error);
+	if(!pMachine) {
+		return 0;
+	}
+
+	return pMachine->pfnGetCpuClockFrequency();
+}
