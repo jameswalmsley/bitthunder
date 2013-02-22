2bc36c987f904e5a2adfbd38596327ec9d2b4831
Robert Steinbauer <rsteinbauer@riegl.com>
2013-02-22 15:18:07 +0100

    [HAL] LPC11xx - NVIC, Systick and system clock frequency in machine file

--- arch/arm/mach/lpc11xx/lpc11xx.c
+++ arch/arm/mach/lpc11xx/lpc11xx.c
@@ -44,17 +44,55 @@ BT_INTEGRATED_DEVICE_DEF oLPC11xx_gpio_device = {
 	.pResources 			= oLPC11xx_gpio_resources,
 };
 
-static BT_u32 zynq_get_cpu_clock_frequency() {
-	return 0;
+static const BT_RESOURCE oLPC11xx_nvic_resources[] = {
+	{
+		.ulStart			= BT_CONFIG_ARCH_ARM_NVIC_BASE,
+		.ulEnd				= BT_CONFIG_ARCH_ARM_NVIC_BASE + BT_SIZE_4K,
+		.ulFlags			= BT_RESOURCE_MEM,
+	},
+	{
+		.ulStart			= 0,
+		.ulEnd				= BT_CONFIG_ARCH_ARM_NVIC_TOTAL_IRQS - 1,
+		.ulFlags			= BT_RESOURCE_IRQ,
+	},
+};
+
+static const BT_INTEGRATED_DEVICE oLPC11xx_nvic_device = {
+	.name					= "arm,common,nvic",
+	.ulTotalResources	   	= BT_ARRAY_SIZE(oLPC11xx_nvic_resources),
+	.pResources				= oLPC11xx_nvic_resources,
+};
+
+static const BT_RESOURCE oLPC11xx_systick_resources[] = {
+	{
+		.ulStart 			= 0xE000E010,
+		.ulEnd				= 0xE000E01F,
+		.ulFlags			= BT_RESOURCE_MEM,
+	},
+	{
+		.ulStart			= 16,
+		.ulEnd				= 16,
+		.ulFlags			= BT_RESOURCE_IRQ,
+	},
+};
+
+static const BT_INTEGRATED_DEVICE oLPC11xx_systick_device = {
+	.name					= "arm,cortex-mx,systick",
+	.ulTotalResources		= BT_ARRAY_SIZE(oLPC11xx_systick_resources),
+	.pResources				= oLPC11xx_systick_resources,
+};
+
+static BT_u32 lpc11xx_get_cpu_clock_frequency() {
+	return 12000000;
 }
 
 BT_MACHINE_START(ARM, CORTEX_M0, "LPC Microcontroller Platform")
-    .ulSystemClockHz 			= 8000000,
-	.pfnGetCpuClockFrequency 	= zynq_get_cpu_clock_frequency,
+    .ulSystemClockHz 			= 12000000,
+	.pfnGetCpuClockFrequency 	= lpc11xx_get_cpu_clock_frequency,
 
-	.pInterruptController		= NULL,
+	.pInterruptController		= &oLPC11xx_nvic_device,
 
-	.pSystemTimer 				= NULL,
+	.pSystemTimer 				= &oLPC11xx_systick_device,
 
 	.pBootLogger				= NULL,
 	.ulBootUartID				= 0,
