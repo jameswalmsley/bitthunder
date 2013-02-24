35a1df819f26bd0c5b64e54dcfd1638233057dc1
Robert Steinbauer <rsteinbauer@riegl.com>
2013-02-24 12:14:48 +0000

    [OS] HOTFIX - Cannot use sprintf on some platforms currently.

--- os/src/devman/bt_devman.c
+++ os/src/devman/bt_devman.c
@@ -81,7 +81,7 @@ BT_ERROR BT_ProbeIntegratedDevices(BT_HANDLE hLogDevice) {
 	BT_u32 size = (BT_u32) ((BT_u32) &__bt_arch_devices_end - (BT_u32) &__bt_arch_devices_start);
 	BT_u32 i;
 
-	BT_i8 buffer[512];
+	//BT_i8 buffer[512];
 
 	BT_i8 *string = "";
 
@@ -100,9 +100,9 @@ BT_ERROR BT_ProbeIntegratedDevices(BT_HANDLE hLogDevice) {
 			}
 
 			if(hDevice) {
-				int len = sprintf(buffer, "Registered %s with %s driver\r\n", pDevice->name, hDevice->h.pIf->oInfo.szpModuleName);
+				/*int len = sprintf(buffer, "Registered %s with %s driver\r\n", pDevice->name, hDevice->h.pIf->oInfo.szpModuleName);
 				string = "Registered device with driver\r\n";
-				BT_CharDeviceWrite(hLogDevice, 0, len,  (BT_u8 *) buffer);
+				BT_CharDeviceWrite(hLogDevice, 0, len,  (BT_u8 *) buffer);*/
 			}
 
 
