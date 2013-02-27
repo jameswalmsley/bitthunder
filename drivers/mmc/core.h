4c5599f53db2e39d78f8e5bc5f821745baff5577
James Walmsley <james@fullfat-fs.co.uk>
2013-02-27 09:10:42 +0100

    [DRIVERS] SDCard - Added ability to register an SD-Card interface, and signal card presence.

--- drivers/mmc/core.h
+++ drivers/mmc/core.h
@@ -1,8 +1,29 @@
 #ifndef _CORE_H_
 #define _CORE_H_
 
+//struct _MMC_HOST;
+typedef struct _MMC_HOST MMC_HOST;
 
+typedef struct _MMC_COMMAND {
+	BT_u32		opcode;
+	BT_u32		arg;
+	BT_u32		response[4];
+} MMC_COMMAND;
 
+typedef enum _BT_MMC_CARD_EVENT {
+	BT_MMC_CARD_DETECTED,
+	BT_MMC_CARD_REMOVED,
+} BT_MMC_CARD_EVENT;
+
+typedef void (*BT_MMC_CARD_EVENTRECEIVER)(MMC_HOST *pHost, BT_MMC_CARD_EVENT eEvent);
+
+typedef struct _BT_MMC_OPS {
+	BT_u32 ulCapabilites1;		///< Primary Capability flags.
+	BT_ERROR (*pfnEventSubscribe)(BT_HANDLE hSDIO, BT_MMC_CARD_EVENTRECEIVER pfnReceiver, MMC_HOST *pHost);
+	BT_ERROR (*pfnRequest)(BT_HANDLE hSDIO, MMC_COMMAND *pCommand);
+} BT_MMC_OPS;
+
+BT_ERROR BT_RegisterSDHostController(BT_HANDLE hHost, const BT_MMC_OPS *pOps);
 
 
 #endif
--- drivers/mmc/sdcard.c
+++ drivers/mmc/sdcard.c
@@ -6,16 +6,68 @@
  **/
 
 #include <bitthunder.h>
+#include "core.h"
+
+BT_DEF_MODULE_NAME			("BitThunder SDCARD Manager")
+BT_DEF_MODULE_DESCRIPTION	("SDCARD abstraction layer for BitThunder")
+BT_DEF_MODULE_AUTHOR		("James Walmsley")
+BT_DEF_MODULE_EMAIL			("james@fullfat-fs.co.uk")
 
 typedef struct _MMC_HOST {
+	BT_LIST_ITEM	oItem;
 	BT_HANDLE 		hHost;
-	//BT_IF_SDIO_OPS *pOps;
+	const BT_MMC_OPS		*pOps;
 } MMC_HOST;
 
+static BT_LIST g_oSDHosts = {0};
+
+static void card_event_handler(MMC_HOST *pHost, BT_MMC_CARD_EVENT eEvent) {
+	switch(eEvent) {
+
+	case BT_MMC_CARD_DETECTED:
+		// Signal SD-Card manager to attempt to initialise this host.
+		break;
+
+	case BT_MMC_CARD_REMOVED:
+		// Signal SD-Card manager to invalidate all handles dependent on this host.
+		break;
+
+	default:
+		break;
+	}
+
+}
 
 
+BT_ERROR BT_RegisterSDHostController(BT_HANDLE hHost, const BT_MMC_OPS *pOps) {
+
+	MMC_HOST *pHost = (MMC_HOST *) BT_kMalloc(sizeof(MMC_HOST));
+	if(!pHost) {
+		return BT_ERR_GENERIC;
+	}
+
+	pHost->hHost = hHost;
+	pHost->pOps = pOps;
+
+	if(pHost->pOps->pfnEventSubscribe) {
+		pHost->pOps->pfnEventSubscribe(hHost, card_event_handler, pHost);
+	}
 
 
-BT_ERROR BT_RegisterSDHostController(BT_HANDLE hSDIO) {
+	BT_ListAddItem(&g_oSDHosts, &pHost->oItem);
+
 	return BT_ERR_NONE;
 }
+
+
+static BT_ERROR bt_sdcard_manager_init() {
+
+	BT_ListInit(&g_oSDHosts);
+
+	return BT_ERR_NONE;
+}
+
+BT_MODULE_INIT_DEF oModuleEntry = {
+	BT_MODULE_NAME,
+	bt_sdcard_manager_init,
+};
