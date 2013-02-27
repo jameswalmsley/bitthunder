143e1d5ac6488d03775b4f3384eb41ba470bd29a
James Walmsley <james@fullfat-fs.co.uk>
2013-02-27 10:47:03 +0100

    [RELEASE] Version 0.4.0 Milestone Completed.
    This marks the beginning of the 0.4.0 series kernel.
    This series is named Deoxyribonucleicacid of DNA for short.

--- lib/include/bt_version.h
+++ lib/include/bt_version.h
@@ -5,13 +5,13 @@
 #define BT_V(x)				BT_V_STRINGIFY(x)
 
 #define BT_VERSION_MAJOR	0
-#define BT_VERSION_MINOR	3
+#define BT_VERSION_MINOR	4
 #define BT_VERSION_REVISION	0
 #ifndef BT_VERSION_SUFFIX
-#define BT_VERSION_SUFFIX	"pre"
+#define BT_VERSION_SUFFIX	"stable"
 #endif
 
-#define BT_VERSION_NAME		"Blinking Tortoise"
+#define BT_VERSION_NAME		"Deoxyribonucleicacid"
 #define BT_VERSION_STRING	"BitThunder v"BT_V(BT_VERSION_MAJOR)"."BT_V(BT_VERSION_MINOR)"."BT_V(BT_VERSION_REVISION)"-"BT_VERSION_SUFFIX
 
 #endif
