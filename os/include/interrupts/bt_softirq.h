81418bfe9222ea5894186e019442044d9bc13586
James Walmsley <james@fullfat-fs.co.uk>
2013-03-05 10:45:16 +0100

    [OS] SoftIRQ - Added initial APIs and implemented Open, Raise and Raise from ISR.

--- os/include/interrupts/bt_softirq.h
+++ os/include/interrupts/bt_softirq.h
@@ -7,10 +7,21 @@
 #ifndef _BT_SOFTIRQ_H_
 #define _BT_SOFTIRQ_H_
 
+#ifndef BT_CONFIG_SOFTIRQ_MAX
+#define BT_CONFIG_SOFTIRQ_MAX	32
+#endif
+
+struct _BT_SOFTIRQ;
+
+typedef void (*BT_SOFTIRQ_HANDLER)(struct _BT_SOFTIRQ *pSoftIRQ);
+
 typedef struct _BT_SOFTIRQ {
-	BT_ERROR   (*pfnAction)	(struct _BT_SOFTIRQ *pSoftIRQ);
-	void 	   *pData;
+	BT_SOFTIRQ_HANDLER 	pfnHandler;
+	void 	   		   *pData;
 } BT_SOFTIRQ;
 
+BT_ERROR BT_OpenSoftIRQ			(BT_u32 ulSoftIRQ, BT_SOFTIRQ_HANDLER pfnHandler, void *pData);
+BT_ERROR BT_RaiseSoftIRQ		(BT_u32 ulSoftIRQ);
+BT_ERROR BT_RaiseSoftIRQFromISR	(BT_u32 ulSoftIRQ);
 
 #endif
--- os/src/interrupts/bt_softirq.c
+++ os/src/interrupts/bt_softirq.c
@@ -1,5 +1,39 @@
 /**
  *	BitThunder SoftIRQ implementation.
  *
+ *	The API is modelled after the linux softirq implementation, to allow
+ *	kernel developer skills to be relatively easily transferred.
  *
  **/
+
+#include <bt_types.h>
+#include <intterupts/bt_softirq.h>
+
+static BT_SOFTIRQ 	g_SoftIRQ[BT_CONFIG_SOFTIRQ_MAX];
+static BT_u32 		ulIRQPending;
+
+BT_ERROR BT_OpenSoftIRQ(BT_u32 ulSoftIRQ, BT_SOFTIRQ_HANDLER pfnHandler, void *pData) {
+	g_SoftIRQ[ulSoftIRQ].pfnHandler = pfnHandler;
+	g_SoftIRQ[ulSoftIRQ].pData		= pData;
+	return BT_ERR_NONE;
+}
+
+BT_ERROR BT_RaiseSoftIRQ(BT_u32 ulSoftIRQ) {
+
+	BT_ERROR Error;
+
+	// Enter Critical Section
+	{
+		Error = BT_RaiseSoftIRQFromISR(ulSoftIRQ);
+	}
+	// Exit Critical Section
+	return Error;
+}
+
+BT_ERROR BT_RaiseSoftIRQFromISR(BT_u32 ulSoftIRQ) {
+	if(ulSoftIRQ < BT_CONFIG_SOFTIRQ_MAX) {
+		ulIRQPending |= (1 << ulSoftIRQ);
+		return BT_ERR_NONE;
+	}
+	return BT_ERR_GENERIC;
+}
