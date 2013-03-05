6b5dc5b70f743f4f32cc6bd20f19f31466e38208
James Walmsley <james@fullfat-fs.co.uk>
2013-03-05 16:33:49 +0100

    [OS] Tasklets - Added a tasklet action implementation. Tasklets should now work...

--- os/src/interrupts/bt_tasklets.c
+++ os/src/interrupts/bt_tasklets.c
@@ -8,6 +8,8 @@
 #include <interrupts/bt_tasklets.h>
 #include <interrupts/bt_softirq.h>
 
+BT_DEF_MODULE_NAME	("Tasklets")
+
 static BT_TASKLET *g_Tasklets = NULL;
 static BT_TASKLET *g_HighTasklets = NULL;
 
@@ -15,11 +17,15 @@ BT_ERROR BT_TaskletSchedule(BT_TASKLET *pTasklet) {
 
 	BT_kEnterCritical();
 	{
-		if(!g_Tasklets) {
-			g_Tasklets = pTasklet;
-		} else {
-			pTasklet->pNext = g_Tasklets;
-			g_Tasklets = pTasklet;
+		if(pTasklet->eState == BT_TASKLET_IDLE) {
+			pTasklet->eState = BT_TASKLET_SCHEDULED;
+
+			if(!g_Tasklets) {
+				g_Tasklets = pTasklet;
+			} else {
+				pTasklet->pNext = g_Tasklets;
+				g_Tasklets = pTasklet;
+			}
 		}
 	}
 	BT_kExitCritical();
@@ -34,11 +40,15 @@ BT_ERROR BT_TaskletHighSchedule(BT_TASKLET *pTasklet) {
 
 	BT_kEnterCritical();
 	{
-		if(!g_HighTasklets) {
-			g_HighTasklets = pTasklet;
-		} else {
-			pTasklet->pNext = g_HighTasklets;
-			g_HighTasklets = pTasklet;
+		if(pTasklet->eState == BT_TASKLET_IDLE) {
+			pTasklet->eState = BT_TASKLET_SCHEDULED;
+
+			if(!g_HighTasklets) {
+				g_HighTasklets = pTasklet;
+			} else {
+				pTasklet->pNext = g_HighTasklets;
+				g_HighTasklets = pTasklet;
+			}
 		}
 	}
 	BT_kExitCritical();
@@ -48,3 +58,58 @@ BT_ERROR BT_TaskletHighSchedule(BT_TASKLET *pTasklet) {
 	return BT_ERR_NONE;
 }
 
+static void tasklet_action(struct _BT_SOFTIRQ *pSoftIRQ) {
+
+	BT_TASKLET *pTasklets;
+
+	BT_kEnterCritical();
+	{
+		pTasklets = g_Tasklets;
+		g_Tasklets = NULL;
+	}
+	BT_kExitCritical();
+
+	while(pTasklets) {
+		BT_TASKLET *t = pTasklets;
+		pTasklets = pTasklets->pNext;
+
+		t->eState = BT_TASKLET_RUNNING;
+		t->pfnHandler(t->pData);
+		t->pNext = NULL;
+		t->eState = BT_TASKLET_IDLE;
+	}
+}
+
+static void tasklet_action_hi(struct _BT_SOFTIRQ *pSoftIRQ) {
+	BT_TASKLET *pTasklets;
+
+	BT_kEnterCritical();
+	{
+		pTasklets = g_HighTasklets;
+		g_HighTasklets = NULL;
+	}
+	BT_kExitCritical();
+
+	while(pTasklets) {
+		BT_TASKLET *t = pTasklets;
+		pTasklets = pTasklets->pNext;
+
+		t->eState = BT_TASKLET_RUNNING;
+		t->pfnHandler(t->pData);
+		t->pNext = NULL;
+		t->eState = BT_TASKLET_IDLE;
+	}
+}
+
+static BT_ERROR bt_tasklets_init() {
+
+	BT_OpenSoftIRQ(BT_SOFTIRQ_HI, 		tasklet_action_hi, NULL);
+	BT_OpenSoftIRQ(BT_SOFTIRQ_TASKLET, 	tasklet_action, NULL);
+
+	return BT_ERR_NONE;
+}
+
+BT_MODULE_INIT_DEF oModuleEntry = {
+	BT_MODULE_NAME,
+	bt_tasklets_init,
+};
