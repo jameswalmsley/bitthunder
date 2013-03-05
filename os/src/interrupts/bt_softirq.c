fb2f3f59335e8a97af4c8fe6fd4e8a8a876bce8c
James Walmsley <james@fullfat-fs.co.uk>
2013-03-05 10:58:49 +0100

    [OS] SoftIRQ - Created the softIRQ kernel module entry point, and initialise dispatcher thread.

--- os/src/interrupts/bt_softirq.c
+++ os/src/interrupts/bt_softirq.c
@@ -11,6 +11,7 @@
 
 static BT_SOFTIRQ 	g_SoftIRQ[BT_CONFIG_SOFTIRQ_MAX];
 static BT_u32 		ulIRQPending;
+static BT_HANDLE	g_hMutex;
 
 BT_ERROR BT_OpenSoftIRQ(BT_u32 ulSoftIRQ, BT_SOFTIRQ_HANDLER pfnHandler, void *pData) {
 	g_SoftIRQ[ulSoftIRQ].pfnHandler = pfnHandler;
@@ -35,5 +36,40 @@ BT_ERROR BT_RaiseSoftIRQFromISR(BT_u32 ulSoftIRQ) {
 		ulIRQPending |= (1 << ulSoftIRQ);
 		return BT_ERR_NONE;
 	}
+
+	BT_ReleaseMutexFromISR(g_hMutex);
+
 	return BT_ERR_GENERIC;
 }
+
+
+static void softirq_dispatcher(void *pParam) {
+	while(1) {
+
+
+	}
+}
+
+static BT_ERROR bt_softirq_init() {
+	BT_ERROR Error;
+	BT_THREAD_CONFIG oConfig;
+
+	oConfig.ulStackDepth 	= 128;
+	oConfig.ulPriority 		= BT_CONFIG_SOFTIRQ_PRIORITY;
+
+	g_hMutex = BT_CreateMutex(&Error);
+	if(!g_hMutex) {
+		return BT_ERR_GENERIC;
+	}
+
+	BT_PendMutex(g_hMutex, 0);
+
+	BT_kTaskCreate(softirq_dispatcher, "ksoftirqd", &oConfig, &Error);
+
+	return BT_ERR_NONE;
+}
+
+BT_MODULE_INIT_DEF oModuleEntry = {
+	BT_MODULE_NAME,
+	bt_softirq_init,
+};
