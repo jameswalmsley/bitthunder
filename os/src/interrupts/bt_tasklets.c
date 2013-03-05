ec1909f98190e8d36bba4d46822f4ddc035658a1
James Walmsley <james@fullfat-fs.co.uk>
2013-03-05 15:48:30 +0100

    [OS] Tasklets - Schedule tasklets by adding to queues, and raising the tasklet softirq.

--- os/src/interrupts/bt_tasklets.c
+++ os/src/interrupts/bt_tasklets.c
@@ -4,10 +4,27 @@
  *
  **/
 
-
+#include <bitthunder.h>
 #include <interrupts/bt_tasklets.h>
+#include <interrupts/bt_softirq.h>
+
+static BT_TASKLET *g_Tasklets = NULL;
+static BT_TASKLET *g_HighTasklets = NULL;
 
 BT_ERROR BT_TaskletSchedule(BT_TASKLET *pTasklet) {
+
+	BT_kEnterCritical();
+	{
+		if(!g_Tasklets) {
+			g_Tasklets = pTasklet;
+		} else {
+			pTasklet->pNext = g_Tasklets;
+			g_Tasklets = pTasklet;
+		}
+	}
+	BT_kExitCritical();
+
+	BT_RaiseSoftIRQ(BT_SOFTIRQ_TASKLET);
 
 	return BT_ERR_NONE;
 }
@@ -15,5 +32,19 @@ BT_ERROR BT_TaskletSchedule(BT_TASKLET *pTasklet) {
 
 BT_ERROR BT_TaskletHighSchedule(BT_TASKLET *pTasklet) {
 
+	BT_kEnterCritical();
+	{
+		if(!g_HighTasklets) {
+			g_HighTasklets = pTasklet;
+		} else {
+			pTasklet->pNext = g_HighTasklets;
+			g_HighTasklets = pTasklet;
+		}
+	}
+	BT_kExitCritical();
+
+	BT_RaiseSoftIRQ(BT_SOFTIRQ_HI);
+
 	return BT_ERR_NONE;
 }
+
