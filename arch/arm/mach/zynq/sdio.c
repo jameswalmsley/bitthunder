b4ce44b2efa256ed6c29195f21a58c7bd02f7085
James Walmsley <james@fullfat-fs.co.uk>
2013-03-14 14:28:28 +0100

    [DRIVERS] SDIO - Host operations can be passed as a driver parameter....
    This is working for now, but we should probably just allow driver sub-systems to actually export
    their own api interfaces.
    
    It would be simpler.

--- arch/arm/mach/zynq/sdio.c
+++ arch/arm/mach/zynq/sdio.c
@@ -19,10 +19,15 @@ static const BT_RESOURCE oZynq_sdio_resources_0[] = {
 		.ulEnd				= 56,
 		.ulFlags			= BT_RESOURCE_IRQ,
 	},
+	{
+		.ulStart 			= 0,
+		.ulEnd				= 0,
+		.ulFlags			= BT_RESOURCE_ENUM,
+	},
 };
 
 BT_INTEGRATED_DEVICE_DEF oZynq_sdio_device_0 = {
-	.name				= "mmc,sdhci",
+	.name				= "zynq,mmc,sdhci",
 	.ulTotalResources	= BT_ARRAY_SIZE(oZynq_sdio_resources_0),
 	.pResources			= oZynq_sdio_resources_0,
 };
@@ -40,12 +45,72 @@ static const BT_RESOURCE oZynq_sdio_resources_1[] = {
 		.ulEnd				= 79,
 		.ulFlags			= BT_RESOURCE_IRQ,
 	},
+	{
+		.ulStart 			= 1,
+		.ulEnd				= 1,
+		.ulFlags			= BT_RESOURCE_ENUM,
+	},
 };
 
 BT_INTEGRATED_DEVICE_DEF oZynq_sdio_device_1 = {
-	.name				= "mmc,sdhci",
+	.name				= "zynq,mmc,sdhci",
 	.ulTotalResources	= BT_ARRAY_SIZE(oZynq_sdio_resources_1),
 	.pResources			= oZynq_sdio_resources_1,
 };
 #endif
 
+
+/**
+ *	This is really acting as a filter driver, so only the zynq HAL can know if the SDHCI
+ *	controller is enabled.
+ *
+ *	If it is, we ask the generic SDHCI driver to do its stuff, otherwise we return an error.
+ **/
+static BT_HANDLE zynq_sdhci_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError) {
+
+	BT_ERROR Error;
+
+	BT_BOOL bEnabled = BT_FALSE;
+	const BT_RESOURCE *pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_ENUM, 0);
+	if(!pResource) {
+		goto err_out;
+	}
+
+	switch(pResource->ulStart) {
+	case 0:
+		bEnabled = (ZYNQ_SLCR->SDIO_CLK_CTRL & ZYNQ_SLCR_CLK_CTRL_CLKACT_0);
+		break;
+
+	case 1:
+		bEnabled = (ZYNQ_SLCR->SDIO_CLK_CTRL & ZYNQ_SLCR_CLK_CTRL_CLKACT_0);
+		break;
+
+	default:
+		break;
+	}
+
+	if(!bEnabled) {
+		Error = BT_ERR_GENERIC;
+		goto err_out;
+	}
+
+	BT_INTEGRATED_DRIVER *pDriver = BT_GetIntegratedDriverByName("mmc,sdhci");
+	if(!pDriver) {
+		Error = BT_ERR_GENERIC;
+		goto err_out;
+	}
+
+	return pDriver->pfnProbe(pDevice, pError);
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
+	return NULL;
+}
+
+BT_INTEGRATED_DRIVER_DEF zynq_sdhci_driver = {
+	.name 				= "zynq,mmc,sdhci",
+	.pfnProbe			= zynq_sdhci_probe,
+};
--- drivers/mmc/host.h
+++ drivers/mmc/host.h
@@ -2,10 +2,9 @@
 #define _HOST_H_
 
 
-
-
-
-
+typedef struct _BT_MMC_HOST_OPS {
+	BT_u32	(*pfnGetInputClock)(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError);
+} BT_MMC_HOST_OPS;
 
 
 
