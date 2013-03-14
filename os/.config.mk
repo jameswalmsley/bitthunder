5f27c47356aa4370ba22559736f156059931c4f2
James Walmsley <james@fullfat-fs.co.uk>
2013-03-14 14:33:06 +0100

    [OS] Include drivers folder.

--- os/.config.mk
+++ os/.config.mk
@@ -1,5 +1,6 @@
 CFLAGS += -I $(BASE)os/include/
 CFLAGS += -I $(BASE)arch/$(ARCH)/include/
+CFLAGS += -I $(BASE)drivers/
 CFLAGS += -DBT_CONFIG_OS
 CFLAGS += -DBT_CONFIG_ARCH="$(ARCH)"
 CFLAGS += -DBT_CONFIG_SUBARCH="$(SUBARCH)"
