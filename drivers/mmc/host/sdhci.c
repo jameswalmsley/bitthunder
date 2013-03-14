a07561c540d2badd09621de3508a35fab6fb92e7
James Walmsley <james@fullfat-fs.co.uk>
2013-03-14 14:39:38 +0100

    [DRIVERS] SD/MMC - Added card detection and initialisation.

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1,10 +1,18 @@
 /**
  *	SD Host Controller Interface Driver Implementation for BitThunder.
+ *
+ *	This allows commands to be sent and received, as well as large data blocks
+ *	to be transferred.
+ *
+ *	This is a generic layer, on-top of which the SD-Card driver sits, but
+ *	its possible for a Bluetooth/Wifi/GPS drivers to sit above this, to
+ *	provide support for such cards.
  **/
 
 #include <bitthunder.h>
 #include "sdhci.h"
 #include "../core.h"
+#include "../host.h"
 
 BT_DEF_MODULE_NAME				("Generic SDHCI Controller Driver")
 BT_DEF_MODULE_DESCRIPTION		("Implements a driver for SDCARD abstraction for SDHCI controllers")
@@ -17,6 +25,7 @@ struct _BT_OPAQUE_HANDLE {
 	SDHCI_REGS 		   		   *pRegs;
 	BT_MMC_CARD_EVENTRECEIVER 	pfnEventReceiver;
 	MMC_HOST				   *pHost;
+	BT_MMC_HOST_OPS			   *pHostOps;
 };
 
 static const BT_IF_HANDLE oHandleInterface;
@@ -28,26 +37,40 @@ static BT_ERROR sdhci_irq_handler(BT_u32 ulIRQ, void *pParam) {
 		return -1;
 	}
 
-	if(hSDHCI->pRegs->NORMAL_ERROR_INTSTAT & NORMAL_INT_CARD_INSERTED) {
+	if(hSDHCI->pRegs->NORMAL_INT_STATUS & NORMAL_INT_CARD_INSERTED) {
 		// Signal to SDCARD driver that we have inserted a card,
 		// and the card can be initialised.
-		hSDHCI->pRegs->NORMAL_ERROR_INTSTAT |= NORMAL_INT_CARD_INSERTED;
+		hSDHCI->pRegs->NORMAL_INT_STATUS |= NORMAL_INT_CARD_INSERTED;
 
 		if(hSDHCI->pRegs->PRESENT_STATE & STATE_CARD_INSERTED) {
+
+			// Enable the SDHCI clock.
+			hSDHCI->pRegs->CLOCK_CONTROL |=  CLOCK_INTERNAL_ENABLE;
+			//
+			// On insertion, the SD controllers clock is enabled!
+			//
+			// This does not guarantee that the clock is stable, this must be determined before
+			// card-initialisation begins.
+			//
+
 			if(hSDHCI->pfnEventReceiver) {
-				hSDHCI->pfnEventReceiver(hSDHCI->pHost, BT_MMC_CARD_DETECTED);
+				hSDHCI->pfnEventReceiver(hSDHCI->pHost, BT_MMC_CARD_DETECTED, BT_TRUE);
 			}
 		}
 	}
 
-	if(hSDHCI->pRegs->NORMAL_ERROR_INTSTAT & NORMAL_INT_CARD_REMOVED) {
+	if(hSDHCI->pRegs->NORMAL_INT_STATUS & NORMAL_INT_CARD_REMOVED) {
 		// Signal to SDCARD driver that we have removed a card.
 		// and all data can be flagged for cleanup.
-		hSDHCI->pRegs->NORMAL_ERROR_INTSTAT |= NORMAL_INT_CARD_REMOVED;
+		hSDHCI->pRegs->NORMAL_INT_STATUS |= NORMAL_INT_CARD_REMOVED;
 
 		if(!(hSDHCI->pRegs->PRESENT_STATE & STATE_CARD_INSERTED)) {
+
+			// Disable the SDHCI clock.
+			hSDHCI->pRegs->CLOCK_CONTROL &= ~CLOCK_INTERNAL_ENABLE;
+
 			if(hSDHCI->pfnEventReceiver) {
-				hSDHCI->pfnEventReceiver(hSDHCI->pHost, BT_MMC_CARD_REMOVED);
+				hSDHCI->pfnEventReceiver(hSDHCI->pHost, BT_MMC_CARD_REMOVED, BT_TRUE);
 			}
 		}
 	}
@@ -67,7 +90,49 @@ static BT_ERROR sdhci_cleanup(BT_HANDLE hSDIO) {
 	return BT_ERR_NONE;
 }
 
+static void sdhci_enable_clock(BT_HANDLE hSDIO) {
+	hSDIO->pRegs->CLOCK_CONTROL |= 4;
+}
+
+static void sdhci_disable_clock(BT_HANDLE hSDIO) {
+	hSDIO->pRegs->CLOCK_CONTROL &= ~4;
+}
+
 static BT_ERROR sdhci_request(BT_HANDLE hSDIO, MMC_COMMAND *pCommand) {
+
+	// Wait until the the command is not inhibited.
+	while(hSDIO->pRegs->PRESENT_STATE & STATE_COMMAND_INHIBIT_CMD) {
+		;
+	}
+
+	hSDIO->pRegs->ARGUMENT = pCommand->arg;
+
+	BT_u16 cmd_reg = pCommand->opcode << 8;
+	if(pCommand->bCRC) {
+		cmd_reg |= 0x8;
+	}
+
+	if(pCommand->ulResponseType == 48) {
+		cmd_reg |= 0x2;
+	}
+
+	if(pCommand->ulResponseType == 136) {
+		cmd_reg |= 0x1;
+	}
+
+	hSDIO->pRegs->COMMAND = cmd_reg;
+
+	while(!(hSDIO->pRegs->NORMAL_INT_STATUS & NORMAL_INT_COMMAND_COMPLETE)) {
+		BT_GpioSet(7, BT_TRUE);
+	}
+
+	hSDIO->pRegs->NORMAL_INT_STATUS |= NORMAL_INT_COMMAND_COMPLETE;
+
+	// Wait until command is complete.
+	pCommand->response[0] = hSDIO->pRegs->RESPONSE[0] | (hSDIO->pRegs->RESPONSE[1] << 16);
+	pCommand->response[1] = hSDIO->pRegs->RESPONSE[2] | (hSDIO->pRegs->RESPONSE[3] << 16);
+	pCommand->response[2] = hSDIO->pRegs->RESPONSE[4] | (hSDIO->pRegs->RESPONSE[5] << 16);
+	pCommand->response[3] = hSDIO->pRegs->RESPONSE[6] | (hSDIO->pRegs->RESPONSE[7] << 16);
 
 	return BT_ERR_NONE;
 }
@@ -82,9 +147,104 @@ static BT_ERROR sdhci_event_subscribe(BT_HANDLE hSDIO, BT_MMC_CARD_EVENTRECEIVER
 
 	if(hSDIO->pRegs->PRESENT_STATE & STATE_CARD_INSERTED) {
 		if(hSDIO->pfnEventReceiver) {
-			hSDIO->pfnEventReceiver(hSDIO->pHost, BT_MMC_CARD_DETECTED);
+			hSDIO->pfnEventReceiver(hSDIO->pHost, BT_MMC_CARD_DETECTED, BT_FALSE);
 		}
 	}
+
+	return BT_ERR_NONE;
+}
+
+static BT_BOOL sdhci_is_card_present(BT_HANDLE hSDIO, BT_ERROR *pError) {
+	return (hSDIO->pRegs->PRESENT_STATE & STATE_CARD_INSERTED);
+}
+
+static BT_ERROR sdhci_reset(BT_HANDLE hSDIO, BT_u8 ucMask) {
+	hSDIO->pRegs->SOFTWARE_RESET = ucMask;
+
+	// Wait for reset to complete
+
+	while(hSDIO->pRegs->SOFTWARE_RESET & ucMask) {
+		BT_ThreadSleep(1);
+	}
+
+	hSDIO->pRegs->AUTO_CMD12_ERROR = 0;	// Clear Auto CMD12 status.
+
+	// Enable the Card detection IRQs.
+	hSDIO->pRegs->NORMAL_INT_ENABLE 		|=	NORMAL_INT_CARD_INSERTED
+												| 	NORMAL_INT_CARD_REMOVED
+												| 	NORMAL_INT_COMMAND_COMPLETE;
+
+	// Ensure we don't have any card-detection interrupts on init,
+	// For some reason this can send the interrupt controller wild, even though
+	// we have already registered our IRQ!
+
+	hSDIO->pRegs->NORMAL_INT_STATUS		|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+
+	// Enable the interrupts to be signalled to the CPU.
+	hSDIO->pRegs->NORMAL_INT_SIGNAL_ENABLE 	|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+
+	return BT_ERR_NONE;
+}
+
+static BT_ERROR sdhci_initialise(BT_HANDLE hSDIO) {
+
+	// Completely reset the SDIO hardware.
+	sdhci_reset(hSDIO, RESET_ALL);
+
+	BT_ThreadSleep(25);	// Sometimes card is not really debounced in some hardware.
+
+	if(!sdhci_is_card_present(hSDIO, NULL)) {
+		return BT_ERR_GENERIC;
+	}
+
+	// Enable the SDIO clock, and attempt to reset the card if present.
+	BT_u32 reg = hSDIO->pRegs->CLOCK_CONTROL;
+	reg &= 0x00FF;	// Mask out the clock selection freq!
+	reg |= (128 << 8);	// Set SDClk to be base (50mhz / 128 ~400khz).
+	reg |= 1;			// Enable internal SDHCI clock.
+	hSDIO->pRegs->CLOCK_CONTROL = reg;
+
+	while(!(hSDIO->pRegs->CLOCK_CONTROL & 2)) {	// Spin until the clock is stable.
+		;
+	}
+
+	hSDIO->pRegs->TIMEOUT_CONTROL = 0x7;	// Timeout at TMCLK 2 ^ 20;
+
+	BT_u32 caps = hSDIO->pRegs->CAPABILITIES;
+	if(caps & CAPS_1_8V) {
+		POWER_SELECT_SET(hSDIO->pRegs->POWER_CONTROL, POWER_SELECT_1_8V);
+	}
+
+	if(caps & CAPS_3_0V) {
+		POWER_SELECT_SET(hSDIO->pRegs->POWER_CONTROL, POWER_SELECT_3_0V);
+	}
+
+	if(caps & CAPS_3_3V) {
+		POWER_SELECT_SET(hSDIO->pRegs->POWER_CONTROL, POWER_SELECT_3_3V);
+	}
+
+	// Enable bus power!
+	POWER_ENABLE_SET(hSDIO->pRegs->POWER_CONTROL, 1);
+
+	sdhci_enable_clock(hSDIO);	// Enable the clock to allow the SDCard to initialise.
+
+	BT_ThreadSleep(10);			// Need to provide atlease 72 clocks, we'll just delay a really long time.
+
+
+
+	// Enable the Card detection IRQs.
+	hSDIO->pRegs->NORMAL_INT_ENABLE 		|=	NORMAL_INT_CARD_INSERTED
+											| 	NORMAL_INT_CARD_REMOVED
+											| 	NORMAL_INT_COMMAND_COMPLETE;
+
+	// Ensure we don't have any card-detection interrupts on init,
+	// For some reason this can send the interrupt controller wild, even though
+	// we have already registered our IRQ!
+
+	hSDIO->pRegs->NORMAL_INT_STATUS		|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+
+	// Enable the interrupts to be signalled to the CPU.
+	hSDIO->pRegs->NORMAL_INT_SIGNAL_ENABLE 	|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
 
 	return BT_ERR_NONE;
 }
@@ -93,6 +253,8 @@ static const BT_MMC_OPS sdhci_mmc_ops = {
 	.ulCapabilites1 	= 0,
 	.pfnRequest			= sdhci_request,
 	.pfnEventSubscribe 	= sdhci_event_subscribe,
+	.pfnIsCardPresent	= sdhci_is_card_present,
+	.pfnInitialise 		= sdhci_initialise,
 };
 
 static BT_HANDLE sdhci_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError) {
@@ -133,17 +295,23 @@ static BT_HANDLE sdhci_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pErr
 		goto err_free_irq;
 	}
 
-	// Enable the Card detection IRQs.
-	hSDIO->pRegs->NORMAL_ERROR_INTSTAT_EN 	|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+	sdhci_reset(hSDIO, RESET_ALL);
 
-	// Ensure we don't have any card-detection interrupts on init,
-	// For some reason this can send the interrupt controller wild, even though
-	// we have already registered our IRQ!
+	BT_u32 hc_version 	= hSDIO->pRegs->SLOT_INT_STAT_HCVERSION;
+	BT_u32 vendor 		= HCVERSION_VENDOR_GET(hc_version);
+	BT_u32 sdversion 	= HCVERSION_SPECV_GET(hc_version);
 
-	hSDIO->pRegs->NORMAL_ERROR_INTSTAT 		|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+	// We could check controller version to ensure compatibility, but
+	// in all likely-hood (clutching at straws) newer versions would remain mostly
+	// compatible.
 
-	// Enable the interrupts to be signalled to the CPU.
-	hSDIO->pRegs->NORMAL_ERROR_INTSIG_EN 	|= NORMAL_INT_CARD_INSERTED | NORMAL_INT_CARD_REMOVED;
+	pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_PARAM, 0);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_irq;
+	}
+
+	hSDIO->pHostOps = (BT_MMC_HOST_OPS *) pResource->pParam;
 
 	// Register with the SD Host controller.
 	Error = BT_RegisterSDHostController(hSDIO, &sdhci_mmc_ops);
--- drivers/mmc/sdcard.c
+++ drivers/mmc/sdcard.c
@@ -6,6 +6,7 @@
  **/
 
 #include <bitthunder.h>
+#include <interrupts/bt_tasklets.h>
 #include "core.h"
 
 BT_DEF_MODULE_NAME			("BitThunder SDCARD Manager")
@@ -14,30 +15,37 @@ BT_DEF_MODULE_AUTHOR		("James Walmsley")
 BT_DEF_MODULE_EMAIL			("james@fullfat-fs.co.uk")
 
 typedef struct _MMC_HOST {
-	BT_LIST_ITEM	oItem;
-	BT_HANDLE 		hHost;
-	const BT_MMC_OPS		*pOps;
+	BT_LIST_ITEM		oItem;
+	BT_HANDLE 			hHost;
+	const BT_MMC_OPS   *pOps;
+	BT_u32 				ulFlags;
+#define MMC_HOST_FLAGS_INITIALISE_REQUEST 	0x00000001		///< This host has generated an initialisation request.
+#define MMC_HOST_FLAGS_INVALIDATE			0x00000002		///< State-machine should attempt to invalidate resources dependent on this host.
 } MMC_HOST;
 
-static BT_LIST g_oSDHosts = {0};
+static BT_TASKLET 	sm_tasklet;
 
-static void card_event_handler(MMC_HOST *pHost, BT_MMC_CARD_EVENT eEvent) {
+static BT_LIST 		g_oSDHosts 	= {0};
+
+static void card_event_handler(MMC_HOST *pHost, BT_MMC_CARD_EVENT eEvent, BT_BOOL bInterruptContext) {
 	switch(eEvent) {
 
 	case BT_MMC_CARD_DETECTED:
 		// Signal SD-Card manager to attempt to initialise this host.
+		pHost->ulFlags |= MMC_HOST_FLAGS_INITIALISE_REQUEST;
 		break;
 
 	case BT_MMC_CARD_REMOVED:
 		// Signal SD-Card manager to invalidate all handles dependent on this host.
+		pHost->ulFlags |= MMC_HOST_FLAGS_INVALIDATE;
 		break;
 
 	default:
 		break;
 	}
 
+	BT_TaskletSchedule(&sm_tasklet);
 }
-
 
 BT_ERROR BT_RegisterSDHostController(BT_HANDLE hHost, const BT_MMC_OPS *pOps) {
 
@@ -53,12 +61,113 @@ BT_ERROR BT_RegisterSDHostController(BT_HANDLE hHost, const BT_MMC_OPS *pOps) {
 		pHost->pOps->pfnEventSubscribe(hHost, card_event_handler, pHost);
 	}
 
-
 	BT_ListAddItem(&g_oSDHosts, &pHost->oItem);
 
 	return BT_ERR_NONE;
 }
 
+
+static void sd_manager_sm(void *pData) {
+
+	BT_ERROR Error;
+
+	MMC_HOST *pHost = (MMC_HOST *) BT_ListGetHead(&g_oSDHosts);
+	while(pHost) {
+
+		if(pHost->ulFlags & MMC_HOST_FLAGS_INITIALISE_REQUEST) {
+			pHost->ulFlags &= ~MMC_HOST_FLAGS_INITIALISE_REQUEST;
+
+			BT_ThreadSleep(100);
+
+			if(pHost->pOps->pfnIsCardPresent(pHost->hHost, &Error)) {
+
+				// Attempt basic SDIO initialisation (host-specific).
+				pHost->pOps->pfnInitialise(pHost->hHost);
+
+				// Send GO-IDLE (CMD0), expecting no response!
+				MMC_COMMAND oCommand;
+				oCommand.arg 			= 0;
+				oCommand.opcode 		= 0;
+				oCommand.bCRC			= BT_FALSE;
+				oCommand.ulResponseType = 0;
+
+				pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+				// Send CMD8 (SEND_IF_COND -- Helps determine SDHC support).
+				oCommand.arg 			= 0x000001AA;		//	0xAA is the test field, it can be anything, 0x100 is the voltage range, 2.7-3.6V).
+				oCommand.opcode 		= 8;
+				oCommand.bCRC 			= BT_TRUE;
+				oCommand.ulResponseType = 48;
+
+				pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+				if((oCommand.response[0] & 0xFFF) != 0x1AA) {
+					oCommand.bCRC = BT_TRUE;
+				}
+
+				while(1) {	/* Loop while the SD-Card initialised itself. */
+
+					oCommand.opcode = 55;				// Send CMD55 to tell card we are doing an Application Command.
+					oCommand.arg 	= 0;
+					oCommand.bCRC 	= BT_TRUE;
+					oCommand.ulResponseType = 48;
+
+					pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+					oCommand.opcode = 41;				// Send ACMD41
+					oCommand.arg 	= 0x40FF8000;		// Tell card  High Capacity mode is supported, and specify valid voltage windows.
+					oCommand.bCRC	= BT_FALSE;
+					oCommand.ulResponseType = 48;
+
+					pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+					// Check if the card is ready?
+					BT_u32 card_ready = oCommand.response[0] >> 31;
+					if(!card_ready) {
+						continue;
+					}
+					break;
+				}
+
+				// Read the CID register
+				oCommand.arg 			= 0;
+				oCommand.opcode 		= 2;
+				oCommand.bCRC 			= BT_TRUE;
+				oCommand.ulResponseType = 136;
+
+				pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+				// We can use the information in the CID register to get things like the CARD S/N etc and manufacturer code.
+
+
+				// Place the command into the data stat (CMD3).
+				oCommand.arg = 0;
+				oCommand.opcode = 3;
+				oCommand.bCRC = BT_TRUE;
+				oCommand.ulResponseType = 48;
+
+				pHost->pOps->pfnRequest(pHost->hHost, &oCommand);
+
+				// Card initialised -- regster block device driver :)
+
+				//BT_RegisterBlockDevice();
+
+				BT_GpioSet(7, BT_TRUE);
+			}
+		}
+
+		if(pHost->ulFlags & MMC_HOST_FLAGS_INVALIDATE) {
+			pHost->ulFlags &= ~MMC_HOST_FLAGS_INVALIDATE;
+			if(!pHost->pOps->pfnIsCardPresent(pHost->hHost, &Error)) {
+				BT_GpioSet(7, BT_FALSE);
+			}
+		}
+
+		pHost = (MMC_HOST *) BT_ListGetNext(&pHost->oItem);
+	}
+}
+
+static BT_TASKLET sm_tasklet = {NULL, BT_TASKLET_IDLE, sd_manager_sm, NULL};
 
 static BT_ERROR bt_sdcard_manager_init() {
 
@@ -66,6 +175,8 @@ static BT_ERROR bt_sdcard_manager_init() {
 
 	return BT_ERR_NONE;
 }
+
+
 
 BT_MODULE_INIT_DEF oModuleEntry = {
 	BT_MODULE_NAME,
