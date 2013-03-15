b9c720d73294d060663404b0840b1a1c5d7a8817
Robert Steinbauer <rsteinbauer@riegl.com>
2013-03-15 15:27:48 +0100

    [BSP] LPC11xx - compiler flags added for optimizing for code size

--- bsp/arm/lpcexpresso/11c24/dbuild.config.mk
+++ bsp/arm/lpcexpresso/11c24/dbuild.config.mk
@@ -1,1 +1,1 @@
-CFLAGS += -mcpu=cortex-m0 -g -mthumb -Wall -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
+CFLAGS += -mcpu=cortex-m0 -g -mthumb -Wall -Os -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
