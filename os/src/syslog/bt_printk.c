d6dec402b9a9e7d165dbd02cf63ae278ff46ecdd
James Walmsley <james@fullfat-fs.co.uk>
2013-03-18 13:16:04 +0100

    [OS] SysLog - BT_kPrint() - Basic implementation adds newline, and seconds.milliseconds timestamp.

--- os/src/syslog/bt_printk.c
+++ os/src/syslog/bt_printk.c
@@ -9,11 +9,14 @@
 BT_ERROR BT_kPrint(const char *format, ... ) {
 	va_list ap;
 
-	bt_printf("[%5d.%06d] : ", BT_GetKernelTick(), 0);
+	BT_TICK oTicks = BT_GetKernelTick();
+	bt_printf("[%5d.%03d] : ", oTicks / 1000, oTicks % 1000);
 
 	va_start(ap, format);
 	bt_kvprintf(format, bt_putc, (void *) BT_GetStandardHandle(), 10, ap);
 	va_end(ap);
+
+	bt_printf("\n");
 
 	return BT_ERR_NONE;
 }
