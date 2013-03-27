c99bc6f6b1f6267f450e19c90e52fc497e1b4def
Robert Steinbauer <rsteinbauer@riegl.com>
2013-03-27 11:45:28 +0100

    [HAL] LPC11xx - extended support for IO configuration

--- arch/arm/mach/lpc11xx/ioconfig.c
+++ arch/arm/mach/lpc11xx/ioconfig.c
@@ -7,5 +7,6 @@ void BT_LPC11xx_SetIOConfig(BT_u32 * pIOCON, BT_u32 config)
 	volatile LPC11xx_RCC_REGS 	*pRCC   = LPC11xx_RCC;
 
 	pRCC->SYSAHBCLKCTRL	|= LPC11xx_RCC_SYSAHBCLKCTRL_IOCON;
-	*pIOCON = config;
+	*pIOCON &= 0xFFFFFFF8;
+	*pIOCON |= config;
 }
