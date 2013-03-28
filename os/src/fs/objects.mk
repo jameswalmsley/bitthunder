0d5887e47316d6c3815d41e358d9f85892c9ceb7
James Walmsley <james@fullfat-fs.co.uk>
2013-03-28 09:40:07 +0100

    [OS] FS - Added FullFAT to build.

---
+++ os/src/fs/objects.mk
@@ -0,0 +1,23 @@
+BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_devfs.o
+
+
+# FullFAT Objects
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_blk.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_crc.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_dir.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_error.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_fat.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_file.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_hash.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_ioman.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_memory.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_safety.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_string.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_time.o
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_unicode.o
+
+$(FF_OBJECTS-y): CFLAGS += -D FF_BITTHUNDER_CONFIG
+$(FF_OBJECTS-y): MODULE_NAME="FullFAT"
+
+
+OBJECTS += $(FF_OBJECTS-y)
