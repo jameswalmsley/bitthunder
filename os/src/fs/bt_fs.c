95a16e8a789c34536305b88581c59d07eae7d241
James Walmsley <james@fullfat-fs.co.uk>
2013-04-11 13:07:03 +0200

    [FS] Added BT_Mount() API, allowing volumes/block devices to be mounted.

--- os/src/fs/bt_fs.c
+++ os/src/fs/bt_fs.c
@@ -2,6 +2,8 @@
  *	BitThunder - File-system Manager.
  *
  **/
+#include <bitthunder.h>
+#include <string.h>
 
 BT_DEF_MODULE_NAME			("Filesystem Manager")
 BT_DEF_MODULE_DESCRIPTION	("Filesystem Mountpoint management")
@@ -20,6 +22,13 @@ typedef struct _BT_FILESYSTEM {
 	BT_HANDLE	 hFS;
 } BT_FILESYSTEM;
 
+typedef struct _BT_MOUNTPOINT {
+	BT_LIST_ITEM 	oItem;
+	BT_HANDLE 		hMount;
+	BT_i8 		   *szpPath;
+	BT_HANDLE 		hFS;
+} BT_MOUNTPOINT;
+
 BT_ERROR BT_RegisterFilesystem(BT_HANDLE hFS) {
 	if(hFS->h.pIf->eType != BT_HANDLE_T_FILESYSTEM) {
 		return BT_ERR_GENERIC;
@@ -37,13 +46,78 @@ BT_ERROR BT_RegisterFilesystem(BT_HANDLE hFS) {
 	return BT_ERR_NONE;
 }
 
+static BT_MOUNTPOINT *GetMountPoint(const BT_i8 *szpPath) {
+	BT_MOUNTPOINT *pMountPoint = (BT_MOUNTPOINT *) BT_ListGetHead(&g_oMountPoints);
+	while(pMountPoint) {
+		BT_i8 *common = (BT_i8 *) strstr(szpPath, pMountPoint->szpPath);
+		if(common == szpPath) {	// Ensure the common section is from root of szpPath!
+			return pMountPoint;
+		}
 
+		pMountPoint = (BT_MOUNTPOINT *) BT_ListGetNext(&pMountPoint->oItem);
+	}
 
+	return pMountPoint;
+}
+
+BT_ERROR BT_Mount(BT_HANDLE hVolume, const BT_i8 *szpPath) {
+	BT_ERROR Error;
+
+	if(hVolume->h.pIf->eType != BT_HANDLE_T_VOLUME &&
+	   hVolume->h.pIf->eType != BT_HANDLE_T_PARTITION &&
+	   hVolume->h.pIf->eType != BT_HANDLE_T_BLOCK) {
+
+		return BT_ERR_GENERIC;
+	}
+
+	BT_HANDLE hMount = NULL;
+
+	// Is path already mounted?
+	if(GetMountPoint(szpPath)) {
+		return BT_ERR_GENERIC;
+	}
+
+	// Can any file-system mount this partition?
+	BT_FILESYSTEM *pFilesystem = (BT_FILESYSTEM *) BT_ListGetHead(&g_oFileSystems);
+	while(pFilesystem) {
+		const BT_IF_FS *pFs = pFilesystem->hFS->h.pIf->oIfs.pFilesystemIF;
+		hMount = pFs->pfnMount(pFilesystem->hFS, hVolume, &Error);
+		if(hMount) {
+			break;
+		}
+
+		pFilesystem = (BT_FILESYSTEM *) BT_ListGetNext(&pFilesystem->oItem);
+	}
+
+	if(!hMount) {
+		BT_kPrint("FS: Could not mount volume, no compatible filesystem.");
+		return BT_ERR_GENERIC;
+	}
+
+	// A filesystem was able to mount the volume, now we can handle this under our own namespace.
+	BT_MOUNTPOINT *pMountPoint = (BT_MOUNTPOINT *) BT_kMalloc(sizeof(BT_MOUNTPOINT));
+	if(!pMountPoint) {
+		Error = BT_ERR_GENERIC;
+		goto err_unmount_out;
+	}
+
+	pMountPoint->hMount 	= hMount;
+	pMountPoint->hFS 		= pFilesystem->hFS;
+	pMountPoint->szpPath 	= BT_kMalloc(strlen(szpPath) + 1);
+	strcpy(pMountPoint->szpPath, szpPath);
+
+	return BT_ERR_NONE;
+
+err_unmount_out:
+	pFilesystem->hFS->h.pIf->oIfs.pFilesystemIF->pfnUnmount(hMount);
+
+	return Error;
+}
 
 
 static BT_ERROR bt_fs_init() {
-	BT_ListInit(&oFileSystems);
-	BT_ListInit(&oMountPoints);
+	BT_ListInit(&g_oFileSystems);
+	BT_ListInit(&g_oMountPoints);
 	return BT_ERR_NONE;
 }
 
