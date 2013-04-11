fe32515f4d33eeda3a369546cba2d06d6f2a73b4
James Walmsley <james@fullfat-fs.co.uk>
2013-04-11 11:08:18 +0200

    [FS] Added filesystem registration API.

---
+++ os/include/fs/bt_fs.h
@@ -0,0 +1,9 @@
+#ifndef _BT_FS_H_
+#define _BT_FS_H_
+
+BT_ERROR BT_RegisterFilesystem(BT_HANDLE hFS);
+
+
+
+
+#endif
--- os/src/fs/bt_fs.c
+++ os/src/fs/bt_fs.c
@@ -8,22 +8,34 @@ BT_DEF_MODULE_DESCRIPTION	("Filesystem Mountpoint management")
 BT_DEF_MODULE_AUTHOR	  	("James Walmsley")
 BT_DEF_MODULE_EMAIL			("james@fullfat-fs.co.uk")
 
-static BT_LIST oFileSystems = {0};
-static BT_LIST oMountPoints = {0};
+static BT_LIST g_oFileSystems = {0};
+static BT_LIST g_oMountPoints = {0};
 
+struct _BT_OPAQUE_HANDLE {
+	BT_HANDLE_HEADER h;
+};
 
+typedef struct _BT_FILESYSTEM {
+	BT_LIST_ITEM oItem;
+	BT_HANDLE	 hFS;
+} BT_FILESYSTEM;
 
+BT_ERROR BT_RegisterFilesystem(BT_HANDLE hFS) {
+	if(hFS->h.pIf->eType != BT_HANDLE_T_FILESYSTEM) {
+		return BT_ERR_GENERIC;
+	}
 
+	BT_FILESYSTEM *pFilesystem = BT_kMalloc(sizeof(BT_FILESYSTEM));
+	if(!pFilesystem) {
+		return BT_ERR_NO_MEMORY;
+	}
 
+	pFilesystem->hFS = hFS;
 
+	BT_ListAddItem(&g_oFileSystems, &pFilesystem->oItem);
 
-
-
-
-
-
-
-
+	return BT_ERR_NONE;
+}
 
 
 
