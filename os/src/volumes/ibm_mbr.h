67e26c719ee5322c53a45e8e890e669da9489049
James Walmsley <james@fullfat-fs.co.uk>
2013-04-15 11:34:58 +0200

    [VOLUME] Partition Enumeration - IBM_MBR definition.

---
+++ os/src/volumes/ibm_mbr.h
@@ -0,0 +1,18 @@
+#ifndef _IBM_MBR_H_
+#define _IBM_MBR_H_
+
+
+
+
+
+#define IBM_MBR_BYTES_PER_SECTOR	0x00B
+#define IBM_MBR_PTBL				0x1BE
+#define IBM_MBR_PTBL_ACTIVE			0x000
+#define IBM_MBR_PTBL_ID				0x004
+#define IBM_MBR_PTBL_LBA			0x008
+#define IBM_MBR_PTBL_SECTORS		0x00C
+
+#define IBM_MBR_SIGNATURE        	0x1FE
+
+
+#endif
