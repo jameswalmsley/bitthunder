ce2a38369279c723ef99e422e89f5959d1fee7c3
James Walmsley <james@fullfat-fs.co.uk>
2013-04-15 11:09:08 +0200

    [FS] FullFAT - Implement file-operations and open API.

--- os/src/fs/bt_fullfat.c
+++ os/src/fs/bt_fullfat.c
@@ -5,6 +5,7 @@
 
 #include <bitthunder.h>
 #include <fs/bt_fs.h>
+#include <volumes/bt_volume.h>
 #include "fullfat/fullfat.h"
 
 BT_DEF_MODULE_NAME			("FullFAT Filesystem")
@@ -17,9 +18,10 @@ struct _BT_OPAQUE_HANDLE {
 };
 
 typedef struct _BT_FF_MOUNT {
-	BT_HANDLE_HEADER h;
-	FF_IOMAN *pIoman;
-	BT_HANDLE hVolume;
+	BT_HANDLE_HEADER 	h;
+	FF_IOMAN 		   *pIoman;
+	BT_HANDLE 			hVolume;
+	void 			   *pBlockCache;
 } BT_FF_MOUNT;
 
 typedef struct _BT_FF_FILE {
@@ -28,34 +30,86 @@ typedef struct _BT_FF_FILE {
 	FF_FILE			*pFile;
 } BT_FF_FILE;
 
+static const BT_IF_HANDLE oHandleInterface;
+static const BT_IF_HANDLE oFileHandleInterface;
+
 static BT_ERROR fullfat_cleanup(BT_HANDLE h) {
 	return BT_ERR_NONE;
 }
 
 static FF_T_SINT32 fullfat_readblocks(FF_T_UINT8 *pBuffer, FF_T_UINT32 Address, FF_T_UINT32 Count, void *pParam) {
 	BT_FF_MOUNT *pMount = (BT_FF_MOUNT *) pParam;
+	BT_ERROR Error;
+	BT_u32 ulRead = BT_VolumeRead(pMount->hVolume, Address, Count, pBuffer, &Error);
 
-	return 0;
+	return (FF_T_SINT32) ulRead;
 }
 
 static FF_T_SINT32 fullfat_writeblocks(FF_T_UINT8 *pBuffer, FF_T_UINT32 Address, FF_T_UINT32 Count, void *pParam) {
 	BT_FF_MOUNT *pMount = (BT_FF_MOUNT *) pParam;
+	BT_ERROR Error;
+	BT_u32 ulWritten = BT_VolumeWrite(pMount->hVolume, Address, Count, pBuffer, &Error);
 
-
-	return 0;
+	return (FF_T_SINT32) ulWritten;
 }
 
 static BT_HANDLE fullfat_mount(BT_HANDLE hFS, BT_HANDLE hVolume, BT_ERROR *pError) {
 
+	FF_ERROR ffError;
 	BT_ERROR Error = BT_ERR_GENERIC;
+	BT_FF_MOUNT *pMount = (BT_FF_MOUNT *) BT_CreateHandle(&oHandleInterface, sizeof(BT_FF_MOUNT), pError);
+	if(!pMount) {
+		return NULL;
+	}
 
+	pMount->pBlockCache = BT_kMalloc(8192);
+	pMount->hVolume = hVolume;
+	pMount->pIoman = FF_CreateIOMAN(pMount->pBlockCache, 8192, 512, &ffError);
+	if(!pMount->pIoman) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_out;
+	}
+
+	ffError = FF_RegisterBlkDevice(pMount->pIoman, 512, fullfat_writeblocks, fullfat_readblocks, pMount);
+
+	ffError = FF_MountPartition(pMount->pIoman, 0);
+
+return (BT_HANDLE) pMount;
+
+err_free_out:
+	BT_kFree(pMount->pBlockCache);
+	BT_kFree(pMount);
 
 	*pError = Error;
 	return NULL;
 }
 
 static BT_ERROR fullfat_unmount(BT_HANDLE hMount) {
+	return BT_ERR_NONE;
+}
 
+static BT_HANDLE fullfat_open(BT_HANDLE hMount, BT_i8 *szpPath, BT_u32 ulModeFlags, BT_ERROR *pError) {
+
+	FF_ERROR ffError;
+
+	BT_FF_FILE *pFile = (BT_FF_FILE *) BT_CreateHandle(&oFileHandleInterface, sizeof(BT_FF_FILE), pError);
+	if(!pFile) {
+		return NULL;
+	}
+
+	BT_FF_MOUNT *pMount = (BT_FF_MOUNT *) hMount;
+
+	pFile->pFile = FF_Open(pMount->pIoman, szpPath, FF_MODE_READ, &ffError);
+	if(!pFile->pFile) {
+		return NULL;
+	}
+
+	pFile->pMount = pMount;
+
+	return (BT_HANDLE) pFile;
+}
+
+static BT_ERROR fullfat_file_cleanup(BT_HANDLE hFile) {
 	return BT_ERR_NONE;
 }
 
@@ -63,12 +117,11 @@ static BT_u32 fullfat_read(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void 
 
 	BT_FF_FILE *pFile = (BT_FF_FILE *) hFile;
 
-	FF_Read(pFile->pFile, 1, ulSize, (FF_T_UINT8 *) pBuffer);
-	return 0;
+	FF_T_SINT32 sRead = FF_Read(pFile->pFile, 1, ulSize, (FF_T_UINT8 *) pBuffer);
+	return (BT_u32) sRead;
 }
 
 static BT_u32 fullfat_write(BT_HANDLE hFile, BT_u32 ulFlags, BT_u32 ulSize, void *pBuffer, BT_ERROR *pError) {
-
 	return 0;
 }
 
@@ -100,7 +153,7 @@ static const BT_IF_FILE oFileOperations = {
 static const BT_IF_FS oFilesystemInterface = {
 	.pfnMount 	= fullfat_mount,
 	.pfnUnmount = fullfat_unmount,
-	.pFileOps = &oFileOperations,
+	.pfnOpen	= fullfat_open,
 };
 
 static const BT_IF_HANDLE oHandleInterface = {
@@ -112,6 +165,14 @@ static const BT_IF_HANDLE oHandleInterface = {
 	.eType = BT_HANDLE_T_FILESYSTEM,
 };
 
+static const BT_IF_HANDLE oFileHandleInterface = {
+	BT_MODULE_DEF_INFO,
+	.pfnCleanup = fullfat_file_cleanup,
+	.oIfs = {
+		.pFileIF = &oFileOperations,
+	},
+	.eType = BT_HANDLE_T_FILE,
+};
 
 static BT_ERROR fullfat_init() {
 	BT_ERROR Error;
