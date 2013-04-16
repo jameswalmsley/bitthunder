c09ea12367fe621493919fdca8605bed1d07aee8
Martin Steininger <msteininger@riegl.com>
2013-04-16 10:30:28 +0200

    [FS] Only build the fullfat plugin if FullFAT is enabled.

--- os/src/fs/objects.mk
+++ os/src/fs/objects.mk
@@ -1,10 +1,10 @@
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_devfs.o
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_fs.o
 BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_file.o
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_fullfat.o
 
 
 # FullFAT Objects
+FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/bt_fullfat.o
 FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_blk.o
 FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_crc.o
 FF_OBJECTS-$(BT_CONFIG_FS_FULLFAT) += $(BUILD_DIR)os/src/fs/fullfat/ff_dir.o
