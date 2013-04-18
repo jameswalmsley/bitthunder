e1f02dfde9f431d59651f56caeefd6a2d485a47d
Robert Steinbauer <rsteinbauer@riegl.com>
2013-04-18 13:07:34 +0200

    [HAL] LPC11xx -gpio driver for LPC11Axx series

---
+++ arch/arm/mach/lpc11xx/gpio_a.c
@@ -0,0 +1,185 @@
+/**
+ *	LPC11xx GPIO driver.
+ *
+ **/
+#include <bitthunder.h>
+#include "gpio_a.h"
+//#include "rcc.h"
+
+BT_DEF_MODULE_NAME				("LPC11xx GPIO")
+BT_DEF_MODULE_DESCRIPTION		("Provides abstract control of GPIO signals for LPC11xx platform.")
+BT_DEF_MODULE_AUTHOR			("James Walmsley")
+BT_DEF_MODULE_EMAIL				("james@fullfat-fs.co.uk")
+
+struct _BT_OPAQUE_HANDLE {
+	BT_HANDLE_HEADER 			h;
+	volatile LPC11Axx_GPIO_REGS   *pRegs;
+};
+
+static BT_ERROR gpio_irq_handler(BT_u32 ulIRQ, void *pParam) {
+	//BT_HANDLE hGPIO = (BT_HANDLE) pParam;
+
+	//hGPIO->pRegs->bank[0].INT_STAT = 1;		///< Acknowledge the interrupt.
+
+	//hGPIO->pRegs->DATA[0] ^= 1 << 12;
+
+	return BT_ERR_NONE;
+}
+
+static BT_ERROR gpio_cleanup(BT_HANDLE hGPIO) {
+	return BT_ERR_NONE;
+}
+
+static BT_ERROR gpio_set(BT_HANDLE hGPIO, BT_u32 ulGPIO, BT_BOOL bValue) {
+	BT_u32 ulBank 	= ulGPIO / 32;
+
+	hGPIO->pRegs->WORD[ulBank] = bValue;
+
+	return BT_ERR_NONE;
+}
+
+static BT_BOOL gpio_get(BT_HANDLE hGPIO, BT_u32 ulGPIO, BT_ERROR *pError) {
+	if(pError) {
+		*pError = BT_ERR_NONE;
+	}
+
+	BT_u32 ulBank 	= ulGPIO / 32;
+
+	if (hGPIO->pRegs->WORD[ulBank]) return BT_TRUE;
+
+	return BT_FALSE;
+}
+
+static BT_ERROR gpio_set_direction(BT_HANDLE hGPIO, BT_u32 ulGPIO, BT_GPIO_DIRECTION eDirection) {
+	BT_u32 ulBank 	= ulGPIO / 32;
+	BT_u32 ulBit	= ulGPIO % 32;
+
+	switch(eDirection) {
+	case BT_GPIO_DIR_OPEN_DRAIN:
+	case BT_GPIO_DIR_OUTPUT: {
+		hGPIO->pRegs->DIR[ulBank] |= 1 << ulBit;
+		break;
+	}
+
+
+	case BT_GPIO_DIR_INPUT:
+		hGPIO->pRegs->DIR[ulBank] &= ~(1 << ulBit);
+		break;
+
+	default:
+		return BT_ERR_GENERIC;
+
+	}
+
+	return BT_ERR_NONE;
+}
+
+static BT_GPIO_DIRECTION gpio_get_direction(BT_HANDLE hGPIO, BT_u32 ulGPIO, BT_ERROR *pError) {
+	if(pError) {
+		*pError = BT_ERR_NONE;
+	}
+	return BT_GPIO_DIR_UNKNOWN;
+}
+
+static BT_ERROR gpio_enable_interrupt(BT_HANDLE hGPIO, BT_u32 ulGPIO) {
+	/*BT_u32 ulBank = ulGPIO / 32;
+	BT_u32 ulBit  = ulGPIO % 32;
+
+	hGPIO->pRegs->bank[ulBank].INT_EN = (1 << ulBit);*/
+
+	return BT_ERR_NONE;
+}
+
+static BT_ERROR gpio_disable_interrupt(BT_HANDLE hGPIO, BT_u32 ulGPIO) {
+	/*BT_u32 ulBank = ulGPIO / 32;
+	BT_u32 ulBit  = ulGPIO % 32;
+
+	hGPIO->pRegs->bank[ulBank].INT_DIS = (1 << ulBit);*/
+
+	return BT_ERR_NONE;
+}
+
+static const BT_DEV_IF_GPIO gpio_ops = {
+	.pfnSet					= gpio_set,
+	.pfnGet					= gpio_get,
+	.pfnSetDirection		= gpio_set_direction,
+	.pfnGetDirection		= gpio_get_direction,
+	.pfnEnableInterrupt		= gpio_enable_interrupt,
+	.pfnDisableInterrupt 	= gpio_disable_interrupt,
+};
+
+static const BT_IF_DEVICE oDeviceInterface = {
+	.eConfigType = BT_DEV_IF_T_GPIO,
+	.unConfigIfs = {
+		.pGpioIF = &gpio_ops,
+	},
+};
+
+static const BT_IF_HANDLE oHandleInterface = {
+	BT_MODULE_DEF_INFO,
+	.pfnCleanup = gpio_cleanup,
+	.oIfs = {
+		.pDevIF = &oDeviceInterface,
+	},
+};
+
+BT_HANDLE gpio_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError) {
+
+	BT_ERROR Error;
+	if(pError) {
+		*pError = BT_ERR_NONE;
+	}
+
+	BT_HANDLE hGPIO = BT_CreateHandle(&oHandleInterface, sizeof(struct _BT_OPAQUE_HANDLE), pError);
+	if(!hGPIO) {
+		goto err_out;
+	}
+
+	const BT_RESOURCE *pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_IO, 0);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_out;
+	}
+
+	BT_u32 base 	= pResource->ulStart;
+	BT_u32 total 	= (pResource->ulEnd - pResource->ulStart) + 1;
+
+	pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_MEM, 0);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_out;
+	}
+
+	hGPIO->pRegs = (LPC11Axx_GPIO_REGS *) pResource->ulStart;
+
+	pResource = BT_GetIntegratedResource(pDevice, BT_RESOURCE_IRQ, 0);
+	if(!pResource) {
+		Error = BT_ERR_GENERIC;
+		goto err_free_out;
+	}
+
+	//Error = BT_EnableInterrupt(pResource->ulStart);
+
+	Error = BT_RegisterGpioController(base, total, hGPIO);
+	if(Error) {
+		goto err_free_out;
+	}
+
+	return hGPIO;
+
+err_free_out:
+	BT_kFree(hGPIO);
+
+	if(pError) {
+		*pError = Error;
+	}
+
+err_out:
+	return NULL;
+}
+
+
+BT_INTEGRATED_DRIVER_DEF gpio_driver = {
+	.name 		= "LPC11xx,gpio",
+	.pfnProbe	= gpio_probe,
+};
---
+++ arch/arm/mach/lpc11xx/gpio_a.h
@@ -0,0 +1,27 @@
+#ifndef _GPIO_A_H_
+#define _GPIO_A_H_
+
+#include <bitthunder.h>
+
+typedef struct _LPC11Axx_GPIO_REGS {
+	BT_u8  BYTE[42];
+	BT_u8  padding[2];
+	BT_STRUCT_RESERVED_u32(0, 0x002C, 0x1000);
+	BT_u32 WORD[42];
+	BT_STRUCT_RESERVED_u32(1, 0x10A4, 0x2000);
+	BT_u32 DIR[2];		///< Direction register.
+	BT_STRUCT_RESERVED_u32(2, 0x2004, 0x2080);
+	BT_u32 MASK[2];		///< Mask register.
+	BT_STRUCT_RESERVED_u32(3, 0x2084, 0x2100);
+	BT_u32 PIN[2];		///< port pin register.
+	BT_STRUCT_RESERVED_u32(4, 0x2104, 0x2180);
+	BT_u32 MPIN[2];		///< masked port pin register.
+	BT_STRUCT_RESERVED_u32(5, 0x2184, 0x2200);
+	BT_u32 SET[2];		///< set register port.
+	BT_STRUCT_RESERVED_u32(6, 0x2204, 0x2280);
+	BT_u32 CLR[2];		///< clear port.
+	BT_STRUCT_RESERVED_u32(7, 0x2284, 0x2300);
+	BT_u32 NOT[2];		///< toggle port.
+} LPC11Axx_GPIO_REGS;
+
+#endif
