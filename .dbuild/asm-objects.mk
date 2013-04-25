59c7fddeaf5c5f1a14ebb114d95a57548343ce92
James Walmsley <james@fullfat-fs.co.uk>
2013-04-25 13:48:26 +0200

    [DBUILD] Allow applications to build assembler objects.

--- .dbuild/asm-objects.mk
+++ .dbuild/asm-objects.mk
@@ -5,3 +5,9 @@ $(BUILD_DIR)%.o: $(BASE)%.s
 	$(Q)$(AS) $(ASFLAGS) $< -o $@
 
 
+$(BUILD_DIR)application/%.o: $(APP_DIR)%.s
+	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(subst $(BUILD_DIR),"",$@)
+	@mkdir -p $(dir $@)
+	$(Q)$(AS) $(ASFLAGS) $< -o $@
+
+
