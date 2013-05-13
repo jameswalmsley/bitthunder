1acd3606dc56825c1f7db503a35ce37d45925ce6
Robert Steinbauer <rsteinbauer@riegl.com>
2013-05-13 14:13:32 +0200

    [MACH] lpc Kconfig extension

--- arch/arm/mach/lpc11xx/ioconfig.c
+++ arch/arm/mach/lpc11xx/ioconfig.c
@@ -2,11 +2,11 @@
 #include "ioconfig.h"
 #include "rcc.h"
 
-void BT_LPC11xx_SetIOConfig(BT_u32 * pIOCON, BT_u32 config)
+void BT_LPC11xx_SetIOConfig(BT_u32 * pIOCON, BT_u32 ulFunction, BT_u32 ulMode, BT_u32 ulAnalogInput, BT_u32 ulOpenDrain)
 {
 	volatile LPC11xx_RCC_REGS 	*pRCC   = LPC11xx_RCC;
 
 	pRCC->SYSAHBCLKCTRL	|= LPC11xx_RCC_SYSAHBCLKCTRL_IOCON;
-	*pIOCON &= 0xFFFFFFF8;
-	*pIOCON |= config;
+	*pIOCON &= 0xFFFFFB60;
+	*pIOCON |= ulFunction | ulMode | ulAnalogInput | ulOpenDrain;
 }
--- arch/arm/mach/lpc17xx/ioconfig.c
+++ arch/arm/mach/lpc17xx/ioconfig.c
@@ -2,8 +2,21 @@
 #include "ioconfig.h"
 #include "rcc.h"
 
-void BT_LPC17xx_SetIOConfig(BT_u32 * pIOCON, BT_u32 ulIO, BT_u32 config) {
-	if (ulIO > 15) pIOCON++;
-	*pIOCON &= ~(0x3 << (2*ulIO));
-	*pIOCON |= (config << (2*ulIO));
+void BT_LPC17xx_SetIOConfig(BT_u32 ulIO, BT_u32 ulFunction, BT_u32 ulMode, BT_BOOL bOpenDrain) {
+	volatile LPC17xx_IOCON_REGS * pRegs = LPC17xx_IOCON;
+
+	BT_u32 ulPort 	 = ulIO / 32;
+	BT_u32 ulPin 	 = ulIO % 32;
+	BT_u32 ulPinsel  = (ulIO % 16)*2;
+	BT_u32 ulPortSel = ulIO / 16;
+
+	pRegs->LPC17xx_PINSELP[ulPortSel] &= ~(0x03 << ulPinsel);
+	pRegs->LPC17xx_PINSELP[ulPortSel] |= ((ulFunction & 0x03) << ulPinsel);
+
+	pRegs->LPC17xx_PINMODE[ulPortSel] &= ~(0x03 << ulPinsel);
+	pRegs->LPC17xx_PINMODE[ulPortSel] |= ((ulMode & 0x03) << ulPinsel);
+
+	pRegs->LPC17xx_PINMODE_OD[ulPort] &= ~ulPin;
+	if (bOpenDrain)
+		pRegs->LPC17xx_PINMODE_OD[ulPort] |= (0x01 << ulPin);
 }
