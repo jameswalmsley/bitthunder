5be678d87e28a7bac49bccafb9ae35b9bcf5a046
James Walmsley <james@fullfat-fs.co.uk>
2013-05-15 11:48:23 +0200

    [CONFIG] Added configurable removal of FILE I/O block and volume managers.

--- os/src/fs/objects.mk
+++ os/src/fs/objects.mk
@@ -1,8 +1,8 @@
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_devfs.o
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_fs.o
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_file.o
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_dir.o
-BT_OS_OBJECTS-$(BT_CONFIG_OS) += $(BUILD_DIR)os/src/fs/bt_inode.o
+BT_OS_OBJECTS-$(BT_CONFIG_OS) 		+= $(BUILD_DIR)os/src/fs/bt_devfs.o
+BT_OS_OBJECTS-$(BT_CONFIG_FS) 		+= $(BUILD_DIR)os/src/fs/bt_fs.o
+BT_OS_OBJECTS-$(BT_CONFIG_FILE) 	+= $(BUILD_DIR)os/src/fs/bt_file.o
+BT_OS_OBJECTS-$(BT_CONFIG_DIR) 		+= $(BUILD_DIR)os/src/fs/bt_dir.o
+BT_OS_OBJECTS-$(BT_CONFIG_INODE) 	+= $(BUILD_DIR)os/src/fs/bt_inode.o
 
 
 # FullFAT Objects
