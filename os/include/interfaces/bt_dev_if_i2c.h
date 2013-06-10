cfdca4d3a141423ac147dd4cd94fd4603c6ea7ca
James Walmsley <james@fullfat-fs.co.uk>
2013-06-10 09:05:24 +0200

    [I2C] Simplified I2C driver model, closely resembling the Linux model.

--- os/include/interfaces/bt_dev_if_i2c.h
+++ os/include/interfaces/bt_dev_if_i2c.h
@@ -21,49 +21,67 @@ typedef enum {
 } BT_I2C_ACCESS_MODE;
 
 typedef struct {
-	BT_I2C_OPERATING_MODE	eMode;
-	BT_I2C_CLOCKRATE		ulClockrate;
-	BT_u16					ulRxBufferSize;		///<
-	BT_u16					ulTxBufferSize;		///<
+	BT_I2C_OPERATING_MODE						eMode;
+	BT_I2C_CLOCKRATE							ulClockrate;
+	BT_u16										ulRxBufferSize;		///<
+	BT_u16										ulTxBufferSize;		///<
 } BT_I2C_CONFIG;
 
+
 typedef struct {
-	BT_ERROR (*pfnSetClockrate)	(BT_HANDLE hI2C, BT_I2C_CLOCKRATE eClockrate);
-	BT_ERROR (*pfnSetConfig)	(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig);
-	BT_ERROR (*pfnGetConfig)	(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig);
-	BT_ERROR (*pfnEnable)		(BT_HANDLE hI2C);
-	BT_ERROR (*pfnDisable)		(BT_HANDLE hI2C);
-	BT_ERROR (*pfnStart)		(BT_HANDLE hI2C);
-	BT_ERROR (*pfnSendAddress)	(BT_HANDLE hI2C, BT_u32 ulAddress, BT_I2C_ACCESS_MODE eAccessMode);
-	BT_ERROR (*pfnGetData)		(BT_HANDLE hI2C, BT_u8 *pDest, BT_u32 ulLength);
-	BT_ERROR (*pfnSendData)		(BT_HANDLE hI2C, BT_u8 *pSrc, BT_u32 ulLength);
-	BT_ERROR (*pfnSendNack)		(BT_HANDLE hI2C);
-	BT_ERROR (*pfnSendAck)		(BT_HANDLE hI2C);
-	BT_BOOL  (*pfnGetAck)		(BT_HANDLE hI2C, BT_ERROR *pError);
-	BT_ERROR (*pfnStop)			(BT_HANDLE hI2C);
-	BT_ERROR (*pfnWrite)		(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucSource, BT_u32 ulLength);
-	BT_ERROR (*pfnRead)			(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucDest  , BT_u32 ulLength);
+	BT_u32    	ulFunctionality;
+	#define BT_I2C_FUNC_I2C						0x00000001
+	#define BT_I2C_FUNC_10BIT_ADDR				0x00000002
+    #define BT_I2C_FUNC_PROTOCOL_MANGLING		0x00000004 			///< I2C_M_IGNORE_NAK etc.
+	#define BT_I2C_FUNC_SMBUS_PEC				0x00000008
+	#define BT_I2C_FUNC_NOSTART					0x00000010 /* I2C_M_NOSTART */
+	#define BT_I2C_FUNC_SMBUS_BLOCK_PROC_CALL	0x00008000 /* SMBus 2.0 */
+	#define BT_I2C_FUNC_SMBUS_QUICK				0x00010000
+	#define BT_I2C_FUNC_SMBUS_READ_BYTE			0x00020000
+	#define BT_I2C_FUNC_SMBUS_WRITE_BYTE		0x00040000
+	#define BT_I2C_FUNC_SMBUS_READ_BYTE_DATA	0x00080000
+	#define BT_I2C_FUNC_SMBUS_WRITE_BYTE_DATA	0x00100000
+	#define BT_I2C_FUNC_SMBUS_READ_WORD_DATA	0x00200000
+	#define BT_I2C_FUNC_SMBUS_WRITE_WORD_DATA	0x00400000
+	#define BT_I2C_FUNC_SMBUS_PROC_CALL			0x00800000
+	#define BT_I2C_FUNC_SMBUS_READ_BLOCK_DATA	0x01000000
+	#define BT_I2C_FUNC_SMBUS_WRITE_BLOCK_DATA 	0x02000000
+	#define BT_I2C_FUNC_SMBUS_READ_I2C_BLOCK	0x04000000 /* I2C-like block xfer  */
+	#define BT_I2C_FUNC_SMBUS_WRITE_I2C_BLOCK	0x08000000 /* w/ 1-byte reg. addr. */
+	#define BT_I2C_FUNC_SMBUS_BYTE				(BT_I2C_FUNC_SMBUS_READ_BYTE | \
+	                                             BT_I2C_FUNC_SMBUS_WRITE_BYTE)
+	#define BT_I2C_FUNC_SMBUS_BYTE_DATA			(BT_I2C_FUNC_SMBUS_READ_BYTE_DATA | \
+	 		    			                     BT_I2C_FUNC_SMBUS_WRITE_BYTE_DATA)
+	#define BT_I2C_FUNC_SMBUS_WORD_DATA			(BT_I2C_FUNC_SMBUS_READ_WORD_DATA | \
+	 		    			                     BT_I2C_FUNC_SMBUS_WRITE_WORD_DATA)
+	#define BT_I2C_FUNC_SMBUS_BLOCK_DATA		(BT_I2C_FUNC_SMBUS_READ_BLOCK_DATA | \
+	 		    			                     BT_I2C_FUNC_SMBUS_WRITE_BLOCK_DATA)
+	#define BT_I2C_FUNC_SMBUS_I2C_BLOCK			(BT_I2C_FUNC_SMBUS_READ_I2C_BLOCK | \
+	 		    			                     BT_I2C_FUNC_SMBUS_WRITE_I2C_BLOCK)
+
+	#define BT_I2C_FUNC_SMBUS_EMUL				(BT_I2C_FUNC_SMBUS_QUICK | \
+					                             BT_I2C_FUNC_SMBUS_BYTE | \
+					                             BT_I2C_FUNC_SMBUS_BYTE_DATA | \
+					                             BT_I2C_FUNC_SMBUS_WORD_DATA | \
+					                             BT_I2C_FUNC_SMBUS_PROC_CALL | \
+					                             BT_I2C_FUNC_SMBUS_WRITE_BLOCK_DATA | \
+					                             BT_I2C_FUNC_SMBUS_I2C_BLOCK | \
+					                             BT_I2C_FUNC_SMBUS_PEC)
+
+	BT_u32		(*pfnMasterTransfer)	(BT_HANDLE hI2C, BT_I2C_MESSAGE *msgs, BT_u32 num, BT_ERROR *pError);
+
 } BT_DEV_IF_I2C;
 
 /*
- *	Define the unified API for I2C devices in BlueThunder
+ *	Define the unified API for I2C devices in BitThunder
  */
 BT_ERROR BT_I2CSetClockrate			(BT_HANDLE hI2C, BT_I2C_CLOCKRATE eClockrate);
 BT_ERROR BT_I2CSetConfiguration		(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig);
 BT_ERROR BT_I2CGetConfiguration		(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig);
-BT_ERROR BT_I2CEnable				(BT_HANDLE hI2C);
-BT_ERROR BT_I2CDisable				(BT_HANDLE hI2C);
-BT_ERROR BT_I2CStart				(BT_HANDLE hI2C);
-BT_ERROR BT_I2CSendAddress			(BT_HANDLE hI2C, BT_u32 ulAddress, BT_I2C_ACCESS_MODE eAccessMode);
-BT_ERROR BT_I2CGetData				(BT_HANDLE hI2C, BT_u8 *pDest, BT_u32 ulLength);
-BT_ERROR BT_I2CSendData				(BT_HANDLE hI2C, BT_u8 *pSrc, BT_u32 ulLength);
-BT_ERROR BT_I2CSendNack				(BT_HANDLE hI2C);
-BT_ERROR BT_I2CSendAck				(BT_HANDLE hI2C);
-BT_BOOL  BT_I2CGetAck				(BT_HANDLE hI2C, BT_ERROR *pError);
-BT_ERROR BT_I2CStop					(BT_HANDLE hI2C);
-BT_ERROR BT_I2CWrite(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucSource, BT_u32 ulLength);
-BT_ERROR BT_I2CRead(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucDest, BT_u32 ulLength);
 
+BT_u32		BT_I2C_Transfer			(BT_HANDLE hI2C, BT_I2C_MESSAGE *pMessages, BT_u32 ulMessages, BT_ERROR *pError);
+BT_u32		BT_I2C_MasterSend		(BT_I2C_CLIENT *pClient, const BT_u8 *pucSource, BT_u32 ulLength, BT_ERROR *pError);
+BT_u32		BT_I2C_MasterReceive	(BT_I2C_CLIENT *pClient, BT_u8 *pucDest, BT_u32 ulLength, BT_ERROR *pError);
 
 
 #endif
--- os/src/interfaces/bt_dev_if_i2c.c
+++ os/src/interfaces/bt_dev_if_i2c.c
@@ -6,8 +6,8 @@
 #include <bitthunder.h>
 struct _BT_OPAQUE_HANDLE {
 	BT_HANDLE_HEADER h;
+	BT_HANDLE 		 hBusMutex;
 };
-
 
 static BT_BOOL isI2CHandle(BT_HANDLE hI2C) {
 	if(!hI2C || !BT_IF_DEVICE(hI2C) || (BT_IF_DEVICE_TYPE(hI2C) != BT_DEV_IF_T_I2C)) {
@@ -16,154 +16,79 @@ static BT_BOOL isI2CHandle(BT_HANDLE hI2C) {
 	return BT_TRUE;
 }
 
-/**
- *	@brief	Set the Baudrate of the I2C device specified by the provided BT_HANDLE.
- *
- **/
-BT_ERROR BT_I2CSetClockrate(BT_HANDLE hI2C, BT_I2C_CLOCKRATE eClockrate) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
+BT_ERROR BT_I2C_LockBus(BT_HANDLE hI2C) {
+	return BT_PendMutex(hI2C->hBusMutex, 0);
+}
+
+BT_ERROR BT_I2C_UnlockBus(BT_HANDLE hI2C) {
+	return BT_ReleaseMutex(hI2C->hBusMutex);
+}
+
+BT_u32 BT_I2C_Transfer(BT_HANDLE hI2C, BT_I2C_MESSAGE *pMessages, BT_u32 ulMessages, BT_ERROR *pError) {
+
+	BT_ERROR Error = BT_ERR_NONE;
+
+	if(pError) {
+		*pError = Error;
 	}
 
-	// If we did kernel mode switching we'd do it here, but for now its now supported.
+	if(!isI2CHandle(hI2C)) {
+		Error = BT_ERR_INVALID_HANDLE;
+		goto err_out;
+	}
 
-	return BT_IF_I2C_OPS(hI2C)->pfnSetClockrate(hI2C, eClockrate);
+	const BT_DEV_IF_I2C *pOps = BT_IF_I2C_OPS(hI2C);
+	if(!pOps->pfnMasterTransfer) {
+		Error = BT_ERR_GENERIC;		// This I2C controller doesn't support standard I2C messages.
+		goto err_out;
+	}
+
+	BT_u32 RetVal;
+
+	BT_I2C_LockBus(hI2C);
+	{
+		RetVal = pOps->pfnMasterTransfer(hI2C, pMessages, ulMessages, pError);
+	}
+	BT_I2C_UnlockBus(hI2C);
+
+	return RetVal;
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
+	return 0;
 }
 
 
-/**
- *	@brief	Set a Complete I2C configuration for the I2C device specified by the BT_HANDLE.
- *
- **/
-BT_ERROR BT_I2CSetConfiguration(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-	return BT_IF_I2C_OPS(hI2C)->pfnSetConfig(hI2C, pConfig);
+BT_u32 BT_I2C_MasterSend(BT_I2C_CLIENT *pClient, const BT_u8 *pucSource, BT_u32 ulLength, BT_ERROR *pError) {
+
+	BT_I2C_MESSAGE oMessage;
+	oMessage.addr 	= pClient->addr;
+	oMessage.flags 	= pClient->flags & BT_I2C_M_TEN;
+	oMessage.len	= ulLength;
+	oMessage.buf	= (BT_u8 *) pucSource;
+
+	BT_u32 RetVal = BT_I2C_Transfer(pClient->hBus, &oMessage, 1, pError);
+
+	return (RetVal == 1) ? ulLength : RetVal;
 }
 
+BT_u32 BT_I2C_MasterReceive(BT_I2C_CLIENT *pClient, BT_u8 *pucDest, BT_u32 ulLength, BT_ERROR *pError) {
 
-/**
- *	@brief	Get a Complete I2C configuration for the I2C device specified by the BT_HANDLE.
- *
- **/
-BT_ERROR BT_I2CGetConfiguration(BT_HANDLE hI2C, BT_I2C_CONFIG *pConfig) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-	return BT_IF_I2C_OPS(hI2C)->pfnGetConfig(hI2C, pConfig);
+	BT_I2C_MESSAGE oMessage;
+	oMessage.addr 	= pClient->addr;
+	oMessage.flags 	= pClient->flags & BT_I2C_M_TEN;
+	oMessage.flags |= BT_I2C_M_RD;
+	oMessage.len 	= ulLength;
+	oMessage.buf	= pucDest;
+
+	BT_u32 RetVal = BT_I2C_Transfer(pClient->hBus, &oMessage, 1, pError);
+
+	return (RetVal == 1) ? ulLength : RetVal;
 }
 
-BT_ERROR BT_I2CEnable(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-	return BT_IF_I2C_OPS(hI2C)->pfnEnable(hI2C);
+BT_I2C_CLIENT *BT_I2C_NewDevice(BT_HANDLE hI2C, BT_I2C_BOARD_INFO *pInfo, BT_ERROR *pError) {
+	return NULL;
 }
-
-BT_ERROR BT_I2CDisable(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnDisable(hI2C);
-}
-
-
-BT_ERROR BT_I2CStart(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnStart(hI2C);
-}
-
-BT_ERROR BT_I2CSendAddress(BT_HANDLE hI2C, BT_u32 ulAddress, BT_I2C_ACCESS_MODE eAccessMode) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnSendAddress(hI2C, ulAddress, eAccessMode);
-}
-
-BT_ERROR BT_I2CGetData(BT_HANDLE hI2C, BT_u8 *pDest, BT_u32 ulLength) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnGetData(hI2C, pDest, ulLength);
-}
-
-BT_ERROR BT_I2CSendData(BT_HANDLE hI2C, BT_u8 *pSrc, BT_u32 ulLength) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnSendData(hI2C, pSrc, ulLength);
-}
-
-BT_ERROR BT_I2CSendNack(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnSendNack(hI2C);
-}
-
-BT_ERROR BT_I2CSendAck(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnSendAck(hI2C);
-}
-
-BT_BOOL  BT_I2CGetAck(BT_HANDLE hI2C, BT_ERROR *pError) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnGetAck(hI2C, pError);
-}
-
-BT_ERROR BT_I2CStop(BT_HANDLE hI2C) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnStop(hI2C);
-}
-
-
-BT_ERROR BT_I2CWrite(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucSource, BT_u32 ulLength) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnWrite(hI2C, ucDevice, pucSource, ulLength);
-}
-
-BT_ERROR BT_I2CRead(BT_HANDLE hI2C, BT_u8 ucDevice, BT_u8 *pucDest, BT_u32 ulLength) {
-	if(!isI2CHandle(hI2C)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return (BT_ERROR) -1;
-	}
-
-	return BT_IF_I2C_OPS(hI2C)->pfnRead(hI2C, ucDevice, pucDest, ulLength);
-}
-
-
