b71b7393dab63ef517c9a123bd203db87b11164e
James Walmsley <james@fullfat-fs.co.uk>
2013-06-14 15:34:44 +0200

    [SHELL] Added a boot_jtag command to get CPU to wait for JTAG loading.

---
+++ os/src/shell/commands/boot_jtag.c
@@ -0,0 +1,57 @@
+#include <bitthunder.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef void (*jump) (void);
+
+static void wait_jtag() {
+	while(1) {
+		__asm("");
+	}
+}
+
+static int bt_boot_jtag(int argc, char **argv) {
+
+	if(argc != 3) {
+		bt_printf("Usage: %s --core [coreID]\n", argv[0]);
+		return -1;
+	}
+
+	BT_u32 coreID = 0;
+
+	if(strcmp(argv[1], "--core")) {
+		bt_printf("Invalid argument %s\n", argv[1]);
+		return -1;
+	}
+
+	coreID = strtol(argv[2], NULL, 10);
+
+	void *p = (void *) wait_jtag;
+
+	BT_DCacheFlush();
+	BT_ICacheInvalidate();
+
+	if(!coreID) {
+		BT_StopSystemTimer();
+		BT_DisableInterrupts();
+
+		jump jmp = p;
+		jmp();
+
+		while(1) {
+			__asm__ ("");
+		}
+
+	} else {
+		// Must use MACH core boot interface.
+		BT_BootCore(coreID, p);
+	}
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oCommand = {
+	.szpName 	= "boot_jtag",
+	.eType 		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand = bt_boot_jtag,
+};
--- os/src/shell/objects.mk
+++ os/src/shell/objects.mk
@@ -3,6 +3,7 @@ BT_OS_OBJECTS-$(BT_CONFIG_SHELL) += $(BUILD_DIR)os/src/shell/bt_shell.o
 
 # Commands
 BT_OS_OBJECTS-$(BT_CONFIG_SHELL_CMD_BOOT) 		+= $(BUILD_DIR)os/src/shell/commands/boot.o
+BT_OS_OBJECTS-$(BT_CONFIG_SHELL_CMD_BOOT_JTAG) 	+= $(BUILD_DIR)os/src/shell/commands/boot_jtag.o
 BT_OS_OBJECTS-$(BT_CONFIG_SHELL_CMD_HELP) 		+= $(BUILD_DIR)os/src/shell/commands/help.o
 BT_OS_OBJECTS-$(BT_CONFIG_SHELL_CMD_LOAD) 		+= $(BUILD_DIR)os/src/shell/commands/load.o
 BT_OS_OBJECTS-$(BT_CONFIG_SHELL_CMD_LOAD_FPGA) 	+= $(BUILD_DIR)os/src/shell/commands/load_fpga.o
