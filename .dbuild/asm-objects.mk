04084e097a0f340c6ba2483cef85ed1fd2f5febb
James Walmsley <james@fullfat-fs.co.uk>
2013-06-19 16:42:45 +0200

    [BUILD] Add support for building .S files directly.

--- .dbuild/asm-objects.mk
+++ .dbuild/asm-objects.mk
@@ -4,10 +4,13 @@ $(BUILD_DIR)%.o: $(BASE)%.s
 	@mkdir -p $(dir $@)
 	$(Q)$(AS) $(ASFLAGS) $< -o $@
 
+$(BUILD_DIR)%.o: $(BASE)%.S
+	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(subst $(BUILD_DIR),"",$@)
+	@mkdir -p $(dir $@)
+	$(Q)$(CC) -c $(CFLAGS) $< -o $@
+
 
 $(BUILD_DIR)application/%.o: $(APP_DIR)%.s
 	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(subst $(BUILD_DIR),"",$@)
 	@mkdir -p $(dir $@)
 	$(Q)$(AS) $(ASFLAGS) $< -o $@
-
-
