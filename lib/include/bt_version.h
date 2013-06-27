793164d138169ddd8a17f8f35ea7d244b86d5079
James Walmsley <james@fullfat-fs.co.uk>
2013-06-27 12:01:11 +0200

    [RELEASE] Version 0.7.0 Milestone Completed.

--- lib/include/bt_version.h
+++ lib/include/bt_version.h
@@ -5,13 +5,13 @@
 #define BT_V(x)				BT_V_STRINGIFY(x)
 
 #define BT_VERSION_MAJOR	0
-#define BT_VERSION_MINOR	6
+#define BT_VERSION_MINOR	7
 #define BT_VERSION_REVISION	0
 #ifndef BT_VERSION_SUFFIX
 #define BT_VERSION_SUFFIX	"stable"
 #endif
 
-#define BT_VERSION_NAME		"Incomplete Turing"
+#define BT_VERSION_NAME		"2,4,6-trinitrotoluene"
 #define BT_VERSION_STRING	"BitThunder v"BT_V(BT_VERSION_MAJOR)"."BT_V(BT_VERSION_MINOR)"."BT_V(BT_VERSION_REVISION)"-"BT_VERSION_SUFFIX
 
 #endif
