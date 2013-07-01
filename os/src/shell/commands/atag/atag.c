33f0e8d2e0677fcf1541d4bbe6aac1faf84505f9
James Walmsley <james@fullfat-fs.co.uk>
2013-07-01 13:03:07 +0200

    [SHELL] Added some atag blob generation commands for BootThunder - Based on the ARM atag reference implementation.

---
+++ os/src/shell/commands/atag/atag.c
@@ -0,0 +1,215 @@
+/**
+ *	Some commands for generating ATAG structures in memory.
+ *
+ *	@author James Walmsley
+ *
+ *	This command forms one of the core functions of the BootThunder bootloader.
+ *
+ *	Note this set of commands are not re-entrant. You must run a sequence to generate
+ *	a complete ATAG blob.
+ *
+ *	Valid sequences are:
+ *
+ *	1.. atag_start 0x01000000		# Initialise an ATAG blob at the address.
+ *	{
+ *		// Call the atag commands to construct an appropriate blob.
+ *		atag_{mem|videotext|ramdisk|initrd2|serial|revision|videolfb|cmdline}
+ *	}
+ *	n..	atag_finish
+ *
+ *	Now a complete atag blob was generated, and a new blob can be started with atag_start
+ *	using a different address.
+ *
+ *	This is useful for e.g. booting an AMP linux configuration.
+ *
+ **/
+#include <bitthunder.h>
+#include "atag.h"
+
+static struct atag *params; /* used to point at the current tag */
+
+static void
+setup_core_tag(void * address,long pagesize)
+{
+    params = (struct tag *)address;         /* Initialise parameters to start at given address */
+
+    params->hdr.tag = ATAG_CORE;            /* start with the core tag */
+    params->hdr.size = tag_size(atag_core); /* size the tag */
+
+    params->u.core.flags = 1;               /* ensure read-only */
+    params->u.core.pagesize = pagesize;     /* systems pagesize (4k) */
+    params->u.core.rootdev = 0;             /* zero root device (typicaly overidden from commandline )*/
+
+    params = tag_next(params);              /* move pointer to next tag */
+}
+
+static void
+setup_ramdisk_tag(u32_t size)
+{
+    params->hdr.tag = ATAG_RAMDISK;         /* Ramdisk tag */
+    params->hdr.size = tag_size(atag_ramdisk);  /* size tag */
+
+    params->u.ramdisk.flags = 0;            /* Load the ramdisk */
+    params->u.ramdisk.size = size;          /* Decompressed ramdisk size */
+    params->u.ramdisk.start = 0;            /* Unused */
+
+    params = tag_next(params);              /* move pointer to next tag */
+}
+
+static void
+setup_initrd2_tag(u32_t start, u32_t size)
+{
+    params->hdr.tag = ATAG_INITRD2;         /* Initrd2 tag */
+    params->hdr.size = tag_size(atag_initrd2);  /* size tag */
+
+    params->u.initrd2.start = start;        /* physical start */
+    params->u.initrd2.size = size;          /* compressed ramdisk size */
+
+    params = tag_next(params);              /* move pointer to next tag */
+}
+
+static void
+setup_mem_tag(u32_t start, u32_t len)
+{
+    params->hdr.tag = ATAG_MEM;             /* Memory tag */
+    params->hdr.size = tag_size(atag_mem);  /* size tag */
+
+    params->u.mem.start = start;            /* Start of memory area (physical address) */
+    params->u.mem.size = len;               /* Length of area */
+
+    params = tag_next(params);              /* move pointer to next tag */
+}
+
+static void
+setup_cmdline_tag(const char * line)
+{
+    int linelen = strlen(line);
+
+    if(!linelen)
+        return;                             /* do not insert a tag for an empty commandline */
+
+    params->hdr.tag = ATAG_CMDLINE;         /* Commandline tag */
+    params->hdr.size = (sizeof(struct atag_header) + linelen + 1 + 4) >> 2;
+
+    strcpy(params->u.cmdline.cmdline,line); /* place commandline into tag */
+
+    params = tag_next(params);              /* move pointer to next tag */
+}
+
+static void
+setup_end_tag(void)
+{
+    params->hdr.tag = ATAG_NONE;            /* Empty tag ends list */
+    params->hdr.size = 0;                   /* zero length */
+}
+
+
+static int bt_atag_core(int argc, char **argv) {
+
+	if(argc != 2) {
+		bt_printf("Usage: %s [0x{start-address}]\n", argv[0]);
+		return -1;
+	}
+
+	BT_u32 addr = strtol(argv[1], NULL, 16);
+
+	setup_core_tag((void *) addr, BT_PAGE_SIZE);
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oCoreCommand = {
+	.szpName 	= "atag_start",
+	.eType 	 	= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_core,
+};
+
+static int bt_atag_ramdisk(int argc, char **argv) {
+	if(argc != 2) {
+		bt_printf("Usage: %s [0x{size(bytes)}]\n", argv[0]);
+		return -1;
+	}
+
+	BT_u32 size = strtol(argv[1], NULL, 16);
+
+	setup_ramdisk_tag(size);
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oRamDiskCommand = {
+	.szpName 	= "atag_ramdisk",
+	.eType 		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_ramdisk,
+};
+
+static int bt_atag_initrd2(int argc, char **argv) {
+
+	if(argc != 3) {
+		bt_printf("Usage: %s [0x{start_address}] [0x{size(bytes)}]\n", argv[0]);
+		return -1;
+	}
+
+	BT_u32 addr, size;
+	addr = strtol(argv[1], NULL, 16);
+	size = strtol(argv[1], NULL, 16);
+
+	setup_atag_initrd2_tag(addr, size);
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oInitrdCommand = {
+	.szpName	= "atag_initrd2",
+	.eType		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_initrd2,
+};
+
+static int bt_atag_mem(int argc, char **argv) {
+	if(argc != 3) {
+		bt_printf("Usage: %s [0x{start_address}] [0x{size(bytes)}]\n", argv[0]);
+		return -1;
+	}
+
+	BT_u32 addr, size;
+	addr = strtol(argv[1], NULL, 16);
+	size = strtol(argv[1], NULL, 16);
+
+	setup_atag_mem_tag(addr, size);
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oMemCommand = {
+	.szpName 	= "atag_mem",
+	.eType		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_mem,
+};
+
+static int bt_atag_cmdline(int argc, char **argv) {
+	if(argc != 2) {
+		bt_printf("Usage: %s \"[kernel command line]\"\n", argv[0]);
+		return -1;
+	}
+
+	setup_cmdline_tag(argv[1]);
+
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oCmdlineCommand = {
+	.szpName	= "atag_cmdline",
+	.eType		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_cmdline,
+};
+
+static int bt_atag_finish(int argc, char **argv) {
+	setup_end_tag();
+	return 0;
+}
+
+BT_SHELL_COMMAND_DEF oFinishCommand = {
+	.szpName 	= "atag_finish",
+	.eType 		= BT_SHELL_NORMAL_COMMAND,
+	.pfnCommand	= bt_atag_finish,
+};
---
+++ os/src/shell/commands/atag/atag.h
@@ -0,0 +1,105 @@
+/**
+ *	ATAG structure definition.
+ *
+ *
+ **/
+
+#include <bt_types.h>
+
+#define ATAG_NONE       0x00000000
+#define ATAG_CORE       0x54410001
+#define ATAG_MEM        0x54410002
+#define ATAG_VIDEOTEXT  0x54410003
+#define ATAG_RAMDISK    0x54410004
+#define ATAG_INITRD2    0x54420005
+#define ATAG_SERIAL     0x54410006
+#define ATAG_REVISION   0x54410007
+#define ATAG_VIDEOLFB   0x54410008
+#define ATAG_CMDLINE    0x54410009
+
+struct atag_header {
+	BT_u32 	size;		// Length of TAG in words, including this header.
+	BT_u32 	tag;		// Tag type ID.
+};
+
+struct atag_core {
+	BT_u32 	flags;
+	BT_u32	pagesize;
+	BT_u32	rootdev;
+};
+
+struct atag_mem {
+	BT_u32	size;
+	BT_u32	start;
+};
+
+struct atag_videotext {
+	BT_u8	x;
+	BT_u8	y;
+	BT_u16	vide_page;
+	BT_u8	video_mode;
+	BT_u8	video_cols;
+	BT_u16	video_ega_bx;
+	BT_u8	video_lines;
+	BT_u8	video_isvga;
+	BT_u16	video_points;
+};
+
+struct atag_ramdisk {
+	BT_u32	flags;
+	BT_u32	size;
+	BT_u32 	start;
+};
+
+struct atag_initrd2 {
+	BT_u32	start;
+	BT_u32	size;
+};
+
+struct atag_serialnr {
+	BT_u32 	low;
+	BT_u32	high;
+};
+
+struct atag_revision {
+	BT_u32 	rev;
+};
+
+struct atag_videolfb {
+	BT_u16	lfb_width;
+	BT_u16  lfb_height;
+	BT_u16	lfb_depth;
+	BT_u16	lfb_linelength;
+	BT_u32	lfb_base;
+	BT_u32	lfb_size;
+	BT_u8	red_size;
+	BT_u8	red_pos;
+	BT_u8	green_size;
+	BT_u8	green_pos;
+	BT_u8	blue_size;
+	BT_u8	blue_pos;
+	BT_u8	rsvd_size;
+	BT_u8	rsvd_pos;
+};
+
+struct atag_cmdline {
+	BT_u8	*cmdline;
+};
+
+struct atag {
+	struct atag_header hdr;
+	union {
+		struct atag_core		core;
+		struct atag_mem			mem;
+		struct atag_videotext	videotext;
+		struct atag_ramdisk		ramdisk;
+		struct atag_initrd2		initrd2;
+		struct atag_serialnr	serialnr;
+		struct atag_revision	revision;
+		struct atag_videolfb	videolfb;
+		struct atag_cmdline		cmdline;
+	} u;
+};
+
+#define tag_next(t)		((struct tag *) ((u32 *)(t) + (t)->hdr.size))
+#define tag_size(type)	((sizeof(struct tag_header) + sizeof(struct type)) >> 2)
