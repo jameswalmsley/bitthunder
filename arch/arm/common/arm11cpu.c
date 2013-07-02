07d81267f15a2fe2c8fae8445876e65e8c051c16
James Walmsley <james@fullfat-fs.co.uk>
2013-07-02 09:12:55 +0200

    [ARCH] ARM - Implemented GetCoreID() api.

--- arch/arm/common/arm11cpu.c
+++ arch/arm/common/arm11cpu.c
@@ -2,11 +2,16 @@
 
 
 BT_u32 BT_GetCoreID() {
-	return 0;
+	register BT_u32 retval asm("r0");
+
+	__asm volatile("mrc p15,0,r0,c0,c0,5");
+	__asm volatile("and r0, r0, #0xF" : "=r"(retval) : "r"(retval));
+
+	return retval;
 }
 
 BT_u32 BT_GetTotalCores() {
-	return 2;
+	return BT_CONFIG_CPU_CORES;
 }
 
 //BT_u32 BT_SetCoreStartAddress
