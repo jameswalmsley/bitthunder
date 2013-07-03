5fc8e71d10946717b20a03c77d7eb00cd8f794d7
James Walmsley <james@fullfat-fs.co.uk>
2013-07-03 11:26:51 +0200

    [BSP] RPi - Removed unecessary dbuild.config.mk file.

--- bsp/arm/raspberrypi/dbuild.config.mk
+++
@@ -1,5 +0,0 @@
-CFLAGS += -march=armv6z -ggdb3 -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/ -I $(BASE)/os/include/
-
-TOOLCHAIN:=$(shell echo $(BT_CONFIG_TOOLCHAIN))
-
-#TOOLCHAIN=arm-none-eabi-
