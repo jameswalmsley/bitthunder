6436ba4abefb7236fbcd7acdc40451037a1c6cd8
James Walmsley <james@fullfat-fs.co.uk>
2013-08-12 15:55:54 +0200

    [BUILD|CONFIG] Out of the box build and setup is now simpler (stm32 also supported).

--- .config.mk
+++ .config.mk
@@ -21,7 +21,9 @@ test_dir:
 
 include $(BASE)$(BSP_DIR)/objects.mk
 
-$(OBJECTS): CFLAGS += -Wall -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/ $(echo shell $(BT_CONFIG_TOOLCHAIN_DEBUG_FLAGS))
+$(OBJECTS): CFLAGS += -Wall -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
+$(OBJECTS): CFLAGS += $(shell echo $(BT_CONFIG_TOOLCHAIN_DEBUG_FLAGS))
+$(OBJECTS): CFLAGS += -march=$(shell echo $(BT_CONFIG_ARCH_ARM_FAMILY))
 $(OBJECTS): CFLAGS += -mcpu=$(shell echo $(BT_CONFIG_TOOLCHAIN_CPU)) $(shell echo $(BT_CONFIG_TOOLCHAIN_FLAGS)) $(shell echo $(BT_CONFIG_TOOLCHAIN_OPTIMISATION))
 $(OBJECTS): CFLAGS += $(shell echo $(BT_CONFIG_TOOLCHAIN_MACH_FLAGS))
 
