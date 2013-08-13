1717d6308d3578bbac7cefe2a6561b4b5fa00b7c
James Walmsley <james@fullfat-fs.co.uk>
2013-08-13 10:22:52 +0200

    [MACH] BCM2835 - Use common bootcode.

--- arch/arm/mach/bcm2835/objects.mk
+++ arch/arm/mach/bcm2835/objects.mk
@@ -5,7 +5,7 @@ MACH_BCM2835_OBJECTS += $(BUILD_DIR)arch/arm/mach/bcm2835/bcm2835.o			# Provides
 MACH_BCM2835_OBJECTS += $(BUILD_DIR)arch/arm/mach/bcm2835/irq_chip.o
 MACH_BCM2835_OBJECTS += $(BUILD_DIR)arch/arm/mach/bcm2835/timer.o
 MACH_BCM2835_OBJECTS += $(BUILD_DIR)arch/arm/mach/bcm2835/gpio.o
-MACH_BCM2835_OBJECTS-$(BT_CONFIG_MACH_BCM2835_USE_STARTUP) += $(BUILD_DIR)arch/arm/mach/bcm2835/startup.o
+
 
 
 MACH_BCM2835_OBJECTS += $(MACH_BCM2835_OBJECTS-y)
