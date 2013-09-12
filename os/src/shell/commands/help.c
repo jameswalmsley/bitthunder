854def7bf739c7c9ed21418d4297369c420a5022
James Walmsley <james@fullfat-fs.co.uk>
2013-09-12 14:10:28 +0200

    [SHELL] help - List all available commands.

--- os/src/shell/commands/help.c
+++ os/src/shell/commands/help.c
@@ -6,14 +6,23 @@
 #include <bitthunder.h>
 #include <shell/bt_shell.h>
 
+extern const BT_SHELL_COMMAND * __bt_shell_commands_start;
+extern const BT_SHELL_COMMAND * __bt_shell_commands_end;
+
 static int bt_help_command(int argc, char **argv) {
 
-	bt_printf("BT Shell (help):\n");
-	bt_printf("argc = %d\n", argc);
-
+	BT_u32 size = (BT_u32) ((BT_u32) &__bt_shell_commands_end - (BT_u32) &__bt_shell_commands_start);
 	BT_u32 i;
-	for(i = 0; i < argc; i++) {
-		bt_printf("arg %d : %s\n", i, argv[i]);
+
+	size /= sizeof(BT_SHELL_COMMAND);
+
+	const BT_SHELL_COMMAND *pCommand = (BT_SHELL_COMMAND *) &__bt_shell_commands_start;
+
+	bt_printf(BT_VERSION_STRING"\n");
+
+	for(i = 0; i < size; i++) {
+		bt_printf("%s\n", pCommand->szpName);
+		pCommand++;
 	}
 
 	return 0;
