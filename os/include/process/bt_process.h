053f0bf8f6bb9d6ec0241360724376360864ee15
Martin Steininger <msteininger@riegl.com>
2013-09-17 15:45:09 +0200

    [OS] Added BT_CreateProcess() prototype.

--- os/include/process/bt_process.h
+++ os/include/process/bt_process.h
@@ -1,6 +1,8 @@
 #ifndef _BT_PROCESS_H_
 #define _BT_PROCESS_H_
 
+BT_HANDLE BT_CreateProcess(BT_FN_THREAD_ENTRY pfnStartRoutine, const BT_i8 *szpName, BT_THREAD_CONFIG *pConfig, BT_ERROR *pError);
+
 BT_HANDLE BT_GetProcessHandle(void);
 
 
