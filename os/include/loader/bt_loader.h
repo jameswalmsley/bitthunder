dc602b0777eab809ad900b26a41c187a95817950
James Walmsley <james@fullfat-fs.co.uk>
2013-09-26 16:21:26 +0200

    [LOADER] Refactored loader interface, and renamed callback to segment.

--- os/include/loader/bt_loader.h
+++ os/include/loader/bt_loader.h
@@ -1,12 +1,21 @@
 #ifndef _BT_LOADER_H_
 #define _BT_LOADER_H_
 
-typedef void (*BT_LOADER_SECTION_CB) (void *addr, void *data, BT_u32 flags, BT_u32 len);
+typedef struct _BT_LOADER_SEGMENT {
+	void   *v_addr;		// Virtual address where section should appear.
+	void   *data;		// Pointer to data/code to be copied into the section.
+	BT_u32 	data_len;	// Length of data to be copied.
+	BT_u32 	size;		// Section length in memory. >= len.
+	BT_u32	flags;		// Memory segment flags.
+} BT_LOADER_SEGMENT;
+
+
+typedef void (*BT_LOADER_SEGMENT_CB) (BT_LOADER_SEGMENT *pSegment, void *pParam);
 
 typedef struct _BT_LOADER {
 	const BT_i8 *name;
 	BT_BOOL (*pfnCanDecode)(void *image_start, BT_u32 len);
-	void   *(*pfnDecode)(void *image_start, BT_u32 len, BT_LOADER_SECTION_CB pfnLoad, BT_ERROR *pError);
+	void   *(*pfnDecode)(void *image_start, BT_u32 len, BT_LOADER_SEGMENT_CB pfnLoad, void *pParam, BT_ERROR *pError);
 } BT_LOADER;
 
 #define BT_LOADER_DEF	static const BT_ATTRIBUTE_SECTION(".bt.module.loaders") BT_LOADER
