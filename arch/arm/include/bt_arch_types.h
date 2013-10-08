abf6528f879685205c4bdaa6bce71dbdf3c81a0d
James Walmsley <james@fullfat-fs.co.uk>
2013-10-08 19:41:08 +0200

    [ARCH] ARM - MMU Types are corrected to pointered types.

--- arch/arm/include/bt_arch_types.h
+++ arch/arm/include/bt_arch_types.h
@@ -23,7 +23,7 @@ typedef	char				BT_i8;
  */
 typedef BT_u32				bt_paddr_t;		///< Physical address type.
 typedef	BT_u32				bt_vaddr_t;		///< Virtual address type.
-typedef BT_u32				bt_pgd_t;		///< Page Global Directory type. (Opaque pointer).
-typedef BT_u32				bt_pte_t;		///< Page table entry type.
+typedef BT_u32				*bt_pgd_t;		///< Page Global Directory type. (Opaque pointer).
+typedef BT_u32				*bt_pte_t;		///< Page table entry type.
 
 #endif
