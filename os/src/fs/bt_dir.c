be197e1088d460443a62693404e1f78da9fa8b1e
James Walmsley <james@fullfat-fs.co.uk>
2013-10-11 12:57:00 +0200

    [FS] ReadDir - Check valid handle.

--- os/src/fs/bt_dir.c
+++ os/src/fs/bt_dir.c
@@ -5,5 +5,9 @@ struct _BT_OPAQUE_HANDLE {
 };
 
 BT_ERROR BT_ReadDir(BT_HANDLE hDir, BT_DIRENT *pDirent) {
+	if(!hDir && hDir->h.pIf->eType != BT_HANDLE_T_DIRECTORY) {
+		return BT_ERR_INVALID_HANDLE_TYPE;
+	}
+
 	return hDir->h.pIf->oIfs.pDirIF->pfnReadDir(hDir, pDirent);
 }
