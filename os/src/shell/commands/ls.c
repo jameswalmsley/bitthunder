0593243f7bdd14aabf9b6b5983befde168c9ffcb
James Walmsley <james@fullfat-fs.co.uk>
2013-10-11 12:59:39 +0200

    [SHELL] ls - Improved ls command.

--- os/src/shell/commands/ls.c
+++ os/src/shell/commands/ls.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 static int bt_ls(int argc, char **argv) {
-	int ret = 0;
+
 	BT_ERROR Error;
 	char *szpDir = 0;
 	BT_DIRENT oDirent;
@@ -18,11 +18,23 @@ static int bt_ls(int argc, char **argv) {
 	}
 
 	BT_HANDLE hDir = BT_OpenDir(szpDir, &Error);
+	if(!hDir) {
+		bt_printf("ls: cannot access %s: No such file or directory\n", szpDir);
+		return 0;
+	}
 
 	while (!BT_ReadDir(hDir, &oDirent))
 	{
-		bt_printf("%s %d\n", oDirent.szpName, oDirent.ullFileSize);
+		BT_i8 attr[11] = "----------";
+		if(oDirent.attr & BT_ATTR_DIR) {
+			attr[0] = 'd';
+		}
+
+		bt_printf("%s %10d ", attr, oDirent.ullFileSize);
+		bt_printf("%s\n", oDirent.szpName);
 	}
+
+	BT_CloseHandle(hDir);
 
 	return 0;
 }
