397d4147a6b9f1dfcdf54b416fa37f9c324c3b4b
James Walmsley <james@fullfat-fs.co.uk>
2013-10-25 13:50:45 +0200

    [SYSLOG] BT_kPrint - Use the global timer for µs accurate timing.

--- os/src/syslog/bt_printk.c
+++ os/src/syslog/bt_printk.c
@@ -13,8 +13,10 @@ BT_ERROR BT_kPrint(const char *format, ... ) {
 	va_list ap;
 
 #ifndef BT_CONFIG_KERNEL_NONE
-	BT_TICK oTicks = BT_GetKernelTick();
-	bt_printf("[%5d.%03d] : ", oTicks / 1000, oTicks % 1000);
+	BT_u64 gt 	= BT_GetGlobalTimer();
+	BT_u32 rate = BT_GetGlobalTimerRate();
+
+	bt_printf("[%5d.%06d] : ", (BT_u32) (gt / (BT_u64)rate), (BT_u32) ((1000000 * (gt % (BT_u64)rate)) / rate ));
 #endif
 
 	va_start(ap, format);
