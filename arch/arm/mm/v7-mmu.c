8069a038917e6e97d290f919b976a376becc0353
Andreas Friedl <afriedl@riegl.com>
2013-10-29 09:48:07 +0100

    [ARCH] ARM - v7-mmu - Allow initialisation of memory map with or without a coherent memory pool.

--- arch/arm/mm/v7-mmu.c
+++ arch/arm/mm/v7-mmu.c
@@ -225,12 +225,15 @@ void bt_mmu_init(struct bt_mmumap *mmumap) {
 		// Page Table is now valid. We can make the pgd point to it for these regions.
 		// If region is within the coherent pool, then configure as an uncached section
 		bt_paddr_t phys 			= bt_virt_to_phys(index * 0x00100000);
-
+#ifdef BT_CONFIG_MEM_PAGE_COHERENT_POOL
 		if(params->coherent && (phys >= params->coherent && phys <= params->coherent + BT_SECTION_ALIGN(BT_CONFIG_MEM_PAGE_COHERENT_LENGTH))) {
 			pgd[index] = phys | MMU_SECTION | MMU_SECTION_SYSTEM;
 		} else {
 			pgd[index] = (bt_paddr_t) bt_virt_to_phys(pte) | MMU_PDE_PRESENT;
 		}
+#else 
+		pgd[index] = (bt_paddr_t) bt_virt_to_phys(pte) | MMU_PDE_PRESENT;
+#endif
 	}
 
 	bt_mmu_flush_tlb();
