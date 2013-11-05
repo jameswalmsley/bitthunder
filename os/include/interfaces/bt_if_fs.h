733403fc4adb7d7e0be08a6e6fd893b100e33384
James Walmsley <james@fullfat-fs.co.uk>
2013-11-05 10:33:03 +0100

    [INTERFACE] FS - Filesystems can define flags, and provide a pseudo mount or block mount method.

--- os/include/interfaces/bt_if_fs.h
+++ os/include/interfaces/bt_if_fs.h
@@ -6,7 +6,13 @@
 #include "bt_if_inode.h"
 
 typedef struct _BT_IF_FS {
-	BT_HANDLE 	(*pfnMount)		(BT_HANDLE hFS, BT_HANDLE hVolume, BT_ERROR *pError);
+	BT_u32		ulFlags;
+	const BT_i8 *name;
+    #define 	BT_FS_FLAG_NODEV	0x00000001	// No underlying block device is required
+	union {
+	    BT_HANDLE 	(*pfnMount)			(BT_HANDLE hFS, BT_HANDLE hVolume, const void *data, BT_ERROR *pError);
+	    BT_HANDLE 	(*pfnMountPseudo) 	(BT_HANDLE hFS, const void *data, BT_ERROR *pError);
+	};
 	BT_ERROR	(*pfnUnmount)	(BT_HANDLE hMount);
 	BT_HANDLE	(*pfnOpen)		(BT_HANDLE hMount, const BT_i8 *szpPath, BT_u32 ulModeFlags, BT_ERROR *pError);
 	BT_ERROR	(*pfnMkDir)		(BT_HANDLE hMount, const BT_i8 *szpPath);
