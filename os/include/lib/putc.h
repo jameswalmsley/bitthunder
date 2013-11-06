050afd6235fdc6d6b68365032f28f1a939613713
James Walmsley <james@fullfat-fs.co.uk>
2013-11-06 10:56:00 +0100

    [PUTC] Replaced stdin/stdout with GetStdin and GetStdout functions to use kernel fds.

--- os/include/lib/putc.h
+++ os/include/lib/putc.h
@@ -10,31 +10,16 @@
 
 #include <bt_config.h>
 
-#define BT_stdin 	(BT_HANDLE)(-1)
-#define BT_stdout 	(BT_HANDLE)(-1)
-
-BT_ERROR BT_SetStdin(BT_HANDLE h);
-
-#if (BT_CONFIG_LIB_PRINTF_SUPPORT_MULTIPLE_STDOUT)
-
-	#define BT_MULTIPLE_STDOUT_MAX	3
-
-	BT_ERROR BT_AddStdout(BT_HANDLE h);
-
-	void BT_RemoveStdout(BT_HANDLE h);
-
-#else 
-
-	BT_ERROR BT_SetStdout(BT_HANDLE h);
-
-#endif
+BT_ERROR 	BT_SetStdin(BT_HANDLE h);
+BT_HANDLE 	BT_GetStdin();
+BT_ERROR 	BT_SetStdout(BT_HANDLE h);
+BT_HANDLE	BT_GetStdout();
+BT_ERROR 	BT_SetStderr(BT_HANDLE h);
+BT_HANDLE 	BT_GetStderr();
 
 int bt_getc(void);
-	
 int bt_fgetc(void *stream);
-
 void bt_putc(int c);
-	
 void bt_fputc(int c, void *stream);
 
 #endif /* PUTC_H_ */
--- os/src/lib/putc.c
+++ os/src/lib/putc.c
@@ -8,51 +8,35 @@
 #include <bitthunder.h>
 #include <lib/putc.h>
 
-static BT_HANDLE g_hStdin = NULL;
-
-#if (BT_CONFIG_LIB_PRINTF_SUPPORT_MULTIPLE_STDOUT)
-	static BT_HANDLE g_hStdout[BT_MULTIPLE_STDOUT_MAX + 1] = { NULL };
-#else
-	static BT_HANDLE g_hStdout = NULL;
-#endif
-
 BT_ERROR BT_SetStdin(BT_HANDLE h) {
-	g_hStdin = h;
+	BT_SetFileDescriptor(0, h);
 	return BT_ERR_NONE;
 }
 
-#if (BT_CONFIG_LIB_PRINTF_SUPPORT_MULTIPLE_STDOUT)
+BT_HANDLE BT_GetStdin() {
+	BT_ERROR Error;
+	return BT_GetFileDescriptor(0, &Error);
+}
 
-	BT_ERROR BT_AddStdout(BT_HANDLE h) {
-		BT_s32 i;
-		for(i=0;i<BT_MULTIPLE_STDOUT_MAX;i++) {
-			if(g_hStdout[i] == NULL) {
-				g_hStdout[i] = h;
-				break;
-			}
-		}
-		if(i>=BT_MULTIPLE_STDOUT_MAX) return BT_ERR_GENERIC;
-		return BT_ERR_NONE;
-	}
+BT_ERROR BT_SetStdout(BT_HANDLE h) {
+	BT_SetFileDescriptor(1, h);
+	return BT_ERR_NONE;
+}
 
-	void BT_RemoveStdout(BT_HANDLE h) {
-		BT_s32 i;
-		for(i=0;i<BT_MULTIPLE_STDOUT_MAX;i++) {
-			if(g_hStdout[i] == h) {
-				g_hStdout[i] = NULL;
-				break;
-			}
-		}
-	}
+BT_HANDLE BT_GetStdout() {
+	BT_ERROR Error;
+	return BT_GetFileDescriptor(1, &Error);
+}
 
-#else
+BT_ERROR BT_SetStderr(BT_HANDLE h) {
+	BT_SetFileDescriptor(2, h);
+	return BT_ERR_NONE;
+}
 
-	BT_ERROR BT_SetStdout(BT_HANDLE h) {
-		g_hStdout = h;
-		return BT_ERR_NONE;
-	}
-
-#endif
+BT_HANDLE BT_GetStderr() {
+	BT_ERROR Error;
+	return BT_GetFileDescriptor(2, &Error);
+}
 
 static int fgetc(void *stream) {
 	BT_HANDLE h = (BT_HANDLE) stream;
@@ -73,41 +57,26 @@ static void fputc(int c, void *stream) {
 }
 
 int bt_getc(void) {
-	if(g_hStdin) return fgetc(g_hStdin);
-	else return 0;
+	BT_HANDLE h = BT_GetStdin();
+	if(h) {
+		return fgetc(h);
+	}
+
+	return 0;
 }
 
 int bt_fgetc(void *stream) {
-	if(!stream || (stream == BT_stdin)) {
-		return bt_getc();
-	} else {
-		return fgetc(stream);
+	return fgetc(stream);
+}
+
+void bt_putc(int c) {
+	BT_HANDLE h = BT_GetStdout();
+	if(h) {
+		fputc(c, h);
 	}
 }
 
-#if (BT_CONFIG_LIB_PRINTF_SUPPORT_MULTIPLE_STDOUT)
-
-	void bt_putc(int c) {
-		int i;
-		for(i=0;i<BT_MULTIPLE_STDOUT_MAX;i++) {
-			if(g_hStdout[i]) {
-				fputc(c, g_hStdout[i]);
-			}
-		}
-	}
-
-#else
-
-	void bt_putc(int c) {
-		if(g_hStdout) fputc(c, g_hStdout);
-	}
-
-#endif
 
 void bt_fputc(int c, void *stream) {
-	if(!stream || (stream == BT_stdout)) {
-		bt_putc(c);
-	} else {
-		fputc(c, stream);
-	}
+	fputc(c, stream);
 }
