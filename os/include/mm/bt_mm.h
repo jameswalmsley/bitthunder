6dc2e3cb9f0adf216774f4b6bb377c2136c13637
Martin Steininger <msteininger@riegl.com>
2013-11-26 08:59:25 +0100

    [MM] BT_PAGE_SIZE - Moved macro definition above first use.

--- os/include/mm/bt_mm.h
+++ os/include/mm/bt_mm.h
@@ -1,6 +1,8 @@
 #ifndef _BT_MM_H_
 #define _BT_MM_H_
 
+#define BT_PAGE_SIZE		4096
+
 #define BT_MM_USERLIMIT		0xC0000000
 #define BT_TOTAL_PAGES		(BT_CONFIG_LINKER_RAM_LENGTH/BT_PAGE_SIZE)
 
@@ -10,7 +12,6 @@
 #define BT_SECTION_TRUNC(x)	((x) & ~BT_SECTION_MASK)
 #define BT_SECTION_ALIGN(x)	(((x) + BT_SECTION_MASK) & ~BT_SECTION_MASK)
 
-#define BT_PAGE_SIZE		4096
 #define BT_PAGE_MASK		(BT_PAGE_SIZE-1)
 
 #define BT_PAGE_TRUNC(x)	((x) & ~BT_PAGE_MASK)					// Round down to nearest PAGE alignment
