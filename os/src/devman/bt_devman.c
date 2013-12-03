d023ee55902b26538547a508b6e1fb2111d39ad1
James Walmsley <james@fullfat-fs.co.uk>
2013-12-03 13:40:10 +0100

    [DEVMAN] Clean compile.

--- os/src/devman/bt_devman.c
+++ os/src/devman/bt_devman.c
@@ -134,7 +134,7 @@ BT_ERROR BT_ProbeIntegratedDevices(BT_HANDLE hLogDevice) {
 	for(i = 0; i < size; i++) {
 		BT_INTEGRATED_DEVICE *pDevice = (BT_INTEGRATED_DEVICE *) &__bt_arch_devices_start;
 		pDevice += i;
-		if(pDevice->eType & BT_DEVICE_TYPE_CODE_MASK != BT_DEVICE_INTEGRATED) {
+		if((pDevice->eType & BT_DEVICE_TYPE_CODE_MASK) != BT_DEVICE_INTEGRATED) {
 			continue;
 		}
 
