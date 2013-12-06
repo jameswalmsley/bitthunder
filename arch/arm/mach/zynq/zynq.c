da46c3f04eff16d74eae6029e7a07ea273193539
James Walmsley <james@fullfat-fs.co.uk>
2013-12-06 09:24:00 +0100

    [MACH] ZYNQ - QSPI definition when not using OF is now correct.

--- arch/arm/mach/zynq/zynq.c
+++ arch/arm/mach/zynq/zynq.c
@@ -218,14 +218,9 @@ static const BT_RESOURCE oZynq_qspi_resources[] = {
 };
 
 BT_INTEGRATED_DEVICE_DEF oZynq_qspi_device = {
-	.name					= "xlnx,ps7-qspi-1.00-a",
+	.name					= "xlnx,ps7-qspi-1.00.a",
 	.ulTotalResources		= BT_ARRAY_SIZE(oZynq_qspi_resources),
 	.pResources				= oZynq_qspi_resources,
-};
-
-BT_DEVFS_INODE_DEF oZynq_qspi_inode = {
-	.szpName				= "qspi",
-	.pDevice				= &oZynq_qspi_device,
 };
 #endif
 #endif
