ebc4569dc06786ede3c4853b1bb0c3d81c541a63
James Walmsley <james@fullfat-fs.co.uk>
2013-12-06 12:28:17 +0100

    [SHELL] slabtop - Cache slab state to provide a slab delta.

--- os/src/shell/commands/slabtop.c
+++ os/src/shell/commands/slabtop.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+static struct bt_slab_info slab_cached;
+static BT_BOOL g_cached_valid = BT_FALSE;
+
 static int bt_slabtop(BT_HANDLE hShell, int argc, char **argv) {
 
 	BT_HANDLE hStdout = BT_ShellGetStdout(hShell);
@@ -10,11 +13,20 @@ static int bt_slabtop(BT_HANDLE hShell, int argc, char **argv) {
 	bt_slab_info(&oInfo);
 
 	BT_u32 i;
-	bt_fprintf(hStdout, "OBJECTS    USED   USE   OBJSIZE  CACHESIZE\n");
+	bt_fprintf(hStdout, "OBJECTS (delta)    USED (delta)   USE   OBJSIZE  CACHESIZE\n");
 	for(i = 0; i < BT_SLAB_MAX_ORDER; i++) {
 		struct bt_cache_info *slab = &oInfo.slabs[i];
-		bt_fprintf(hStdout, " %6d  %6d  %3d%%  %8d   %8d\n", slab->available, slab->allocated, (slab->allocated * 100) / slab->available, slab->ulObjectSize, slab->ulObjectSize * slab->available);
+		BT_s32 d_allocated = 0, d_available = 0;
+		if(g_cached_valid) {
+			d_allocated = slab_cached.slabs[i].allocated - slab->allocated;
+			d_available = slab_cached.slabs[i].available - slab->available;
+		}
+
+		bt_fprintf(hStdout, " %6d (%5d)  %6d (%5d)  %3d%%  %8d   %8d\n", slab->available, d_available, slab->allocated, d_allocated, (slab->allocated * 100) / slab->available, slab->ulObjectSize, slab->ulObjectSize * slab->available);
 	}
+
+	slab_cached = oInfo;
+	g_cached_valid = BT_TRUE;
 
 	return 0;
 }
