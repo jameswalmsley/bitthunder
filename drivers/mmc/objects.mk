3071c6665edadbe810d912e447707d3b249fb439
James Walmsley <james@fullfat-fs.co.uk>
2013-12-14 12:47:44 +0100

    [DRIVERS] Updated Kconfig and objects.mk include order to be alphanumerical, and updated sdcard Kconfig to be more consistent.

--- drivers/mmc/objects.mk
+++ drivers/mmc/objects.mk
@@ -1,10 +1,9 @@
-MMC_OBJECTS += $(BUILD_DIR)drivers/mmc/sdcard.o
+MMC_OBJECTS-$(BT_CONFIG_DRIVERS_MMC) += $(BUILD_DIR)drivers/mmc/sdcard.o
 MMC_OBJECTS-$(BT_CONFIG_DRIVERS_SDCARD_SDHCI) += $(BUILD_DIR)drivers/mmc/host/sdhci.o
 
 
 MMC_OBJECTS += $(MMC_OBJECTS-y)
 
 $(MMC_OBJECTS): MODULE_NAME="drivers-SDIO"
-
 
 OBJECTS += $(MMC_OBJECTS)
--- drivers/objects.mk
+++ drivers/objects.mk
@@ -4,20 +4,20 @@ ifeq ($(BT_CONFIG_DRIVERS_BLOCK), y)
 include $(BASE)drivers/block/objects.mk
 endif
 
-ifeq ($(BT_CONFIG_DRIVERS_NET), y)
-include $(BASE)drivers/net/objects.mk
-endif
-
-ifeq ($(BT_CONFIG_DRIVERS_SDCARD), y)
-include $(BASE)drivers/mmc/objects.mk
-endif
-
 ifeq ($(BT_CONFIG_DRIVERS_GPIO), y)
 include $(BASE)drivers/gpio/objects.mk
 endif
 
+ifeq ($(BT_CONFIG_DRIVERS_MMC), y)
+include $(BASE)drivers/mmc/objects.mk
+endif
+
 ifeq ($(BT_CONFIG_DRIVERS_MTD), y)
 include $(BASE)drivers/mtd/objects.mk
+endif
+
+ifeq ($(BT_CONFIG_DRIVERS_NET), y)
+include $(BASE)drivers/net/objects.mk
 endif
 
 ifeq ($(BT_CONFIG_DRIVERS_RTC), y)
