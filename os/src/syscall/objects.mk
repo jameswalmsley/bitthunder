4682bd9e2e6bead1d4fef9f7b983c3474d3f9d44
James Walmsley <james@fullfat-fs.co.uk>
2013-12-15 16:28:03 +0100

    [SYSCALL] Correctly compile syscall objects when adding syscall support.

--- os/src/syscall/objects.mk
+++ os/src/syscall/objects.mk
@@ -1,2 +1,2 @@
-BT_OS_OBJECTS-$(BT_CONFIG_SYSCALLS)	+= $(BUILD_DIR)os/src/syscall/bt_syscall.o
-BT_OS_OBJECTS-$(BT_CONFIG_SYSCALLS) += $(BUILD_DIR)os/src/syscall/getpid/getpid.o
+BT_OS_OBJECTS-$(BT_CONFIG_SYSCALL)	+= $(BUILD_DIR)os/src/syscall/bt_syscall.o
+BT_OS_OBJECTS-$(BT_CONFIG_SYSCALL) += $(BUILD_DIR)os/src/syscall/getpid/getpid.o
