7099815d20209b2aabee238298dfab5e8fd49b6a
Robert Steinbauer <rsteinbauer@riegl.com>
2013-12-17 15:25:59 +0100

    [OS] Modified BT_kprint() to support either Tickcount or Globaltimer

--- os/src/syslog/bt_printk.c
+++ os/src/syslog/bt_printk.c
@@ -13,10 +13,17 @@ BT_ERROR BT_kPrint(const char *format, ... ) {
 	va_list ap;
 
 #ifndef BT_CONFIG_KERNEL_NONE
+
+#ifdef BT_CONFIG_SYSLOG_SYSTICK
+	BT_TICK oTicks = BT_GetKernelTick();
+	bt_printf("[%5d.%03d] : ", oTicks / 1000, oTicks % 1000);
+#else
 	BT_u64 gt 	= BT_GetGlobalTimer();
 	BT_u32 rate = BT_GetGlobalTimerRate();
 
 	bt_printf("[%5d.%06d] : ", (BT_u32) (gt / (BT_u64)rate), (BT_u32) ((1000000 * (gt % (BT_u64)rate)) / rate ));
+#endif
+
 #endif
 
 	va_start(ap, format);
