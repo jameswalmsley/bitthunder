797601f3de570bff2dd9d7fafeda729d41b9cb82
James Walmsley <james@fullfat-fs.co.uk>
2013-12-20 18:06:56 +0100

    [DBUILD] Allow applications to compile .S files.

--- .dbuild/asm-objects.mk
+++ .dbuild/asm-objects.mk
@@ -14,3 +14,8 @@ $(BUILD_DIR)application/%.o: $(APP_DIR)%.s
 	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(subst $(BUILD_DIR),"",$@)
 	@mkdir -p $(dir $@)
 	$(Q)$(AS) $(ASFLAGS) $< -o $@
+
+$(BUILD_DIR)application/%.o: $(APP_DIR)%.S
+	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(subst $(BUILD_DIR),"",$@)
+	@mkdir -p $(dir $@)
+	$(Q)$(CC) -c $(CFLAGS) $< -o $@
