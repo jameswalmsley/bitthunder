7450667492d51f117fc0cfb28f9b979040961c4d
Robert Steinbauer <rsteinbauer@riegl.com>
2014-01-09 15:27:16 +0100

    [OS] Re-implemented the FIFO construct to utilise kernel based Queues and scheduling primitives.

--- lib/include/collections/bt_fifo.h
+++ lib/include/collections/bt_fifo.h
@@ -15,11 +15,11 @@
 
 BT_HANDLE BT_FifoCreate(BT_u32 ulElements, BT_u32 ulElementWidth, BT_u32 ulFlags, BT_ERROR *pError);
 BT_u32 BT_FifoWrite(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ERROR *pError);
-BT_u32 BT_FifoRead(BT_HANDLE hFifo, BT_u32 ulElements, void * pData, BT_ERROR *pError);
+BT_u32 BT_FifoWriteFromISR(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ERROR *pError);
+BT_u32 BT_FifoRead(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError);
+BT_u32 BT_FifoReadFromISR(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError);
 BT_BOOL BT_FifoIsEmpty(BT_HANDLE hFifo, BT_ERROR *pError);
 BT_BOOL BT_FifoIsFull(BT_HANDLE hFifo, BT_ERROR *pError);
-BT_ERROR BT_FifoWaitFull(BT_HANDLE hFifo);
-BT_ERROR BT_FifoWaitEmpty(BT_HANDLE hFifo);
 BT_u32 BT_FifoFillLevel(BT_HANDLE hFifo, BT_ERROR *pError);
 BT_u32 BT_FifoSize(BT_HANDLE hFifo, BT_ERROR *pError);
 
--- lib/src/collections/bt_fifo.c
+++ lib/src/collections/bt_fifo.c
@@ -21,10 +21,8 @@ BT_DEF_MODULE_EMAIL						("rsteinbauer@riegl.com")
  **/
 struct _BT_OPAQUE_HANDLE {
 	BT_HANDLE_HEADER h;			///< All handles must include a handle header.
-	BT_u8			*pBuf;						///< Pointer to the start of the ring-buffer.
-	BT_u8			*pIn;						///< Input pointer.
-	BT_u8			*pOut;						///< Output pointer.
-	BT_u8			*pEnd;						///< Pointer to end of buffer.
+	BT_HANDLE		 oQueue;						///< Pointer to the start of the ring-buffer.
+	BT_u32	 		 ulElements;
 	BT_u32	 		 ulElementWidth;
 	BT_u32	 		 ulFlags;
 };
@@ -51,17 +49,15 @@ BT_HANDLE BT_FifoCreate(BT_u32 ulElements, BT_u32 ulElementWidth, BT_u32 ulFlags
 		goto err_out;
 	}
 
-	hFifo->pBuf 	= BT_kMalloc((ulElements+1) * ulElementWidth);
-	// Check malloc succeeded!
-	if (!hFifo->pBuf) {
+	hFifo->oQueue   = BT_CreateQueue(ulElements, ulElementWidth, &Error);
+	// Check BT_CreateQueue succeeded!
+	if (!hFifo->oQueue) {
 		Error = BT_ERR_NO_MEMORY;
 		goto err_free_out;
 	}
-	hFifo->pIn 	= hFifo->pBuf;
-	hFifo->pOut = hFifo->pBuf;
-	hFifo->pEnd = hFifo->pBuf + (ulElements+1) * ulElementWidth;
 	hFifo->ulElementWidth = ulElementWidth;
-	hFifo->ulFlags = ulFlags;
+	hFifo->ulElements     = ulElements;
+	hFifo->ulFlags        = ulFlags;
 
 	return hFifo;
 
@@ -80,67 +76,96 @@ BT_u32 BT_FifoWrite(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ER
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	const BT_u8 *pSrc  = pData;
-	BT_u8 *pDest = hFifo->pIn;
-//	BT_u8 *pOut  = hFifo->pOut;
-	BT_u32 ulElementWidth = hFifo->ulElementWidth;
+	const BT_u8 *pSrc = pData;
 	BT_u32 ulWritten;
+	BT_BOOL bHigherPriorityTaskWoken;
 
 	for (ulWritten = 0; ulWritten < ulElements; ulWritten++) {
 		// We should prevent overflow, and block!
 		if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
-			if (BT_FifoIsFull(hFifo, pError)) return ulWritten;
+			if (BT_FifoIsFull(hFifo->oQueue,pError))
+				return ulWritten;
 		}
-
-		if (!(hFifo->ulFlags & BT_FIFO_OVERWRITE))
-			BT_FifoWaitFull(hFifo);
-		memcpy(pDest, pSrc, ulElementWidth);
-		pDest += ulElementWidth;
-		pSrc += ulElementWidth;
-		if(pDest >= hFifo->pEnd) {
-			pDest = hFifo->pBuf;
-		}
-//			if (pDest == pOut) {
-//				pOut += ulElementWidth;
-//				if(pOut >= hFifo->pEnd) {
-//					pOut = hFifo->pBuf;
-//				}
-//				hFifo->pOut = pOut;
-//			}
-		hFifo->pIn = pDest;
+		if (BT_GetActiveInterrupt(pError) >= 0)
+			BT_QueueSendFromISR(hFifo->oQueue, pSrc, &bHigherPriorityTaskWoken);
+		else
+			BT_QueueSend(hFifo->oQueue, pSrc, -1);
+		pSrc += hFifo->ulElementWidth;
 	}
+
 	return ulWritten;
 }
 
-BT_u32 BT_FifoRead(BT_HANDLE hFifo, BT_u32 ulElements, void * pData, BT_ERROR *pError) {
+BT_u32 BT_FifoWriteFromISR(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ERROR *pError) {
 	if(!isFifoHandle(hFifo)) {
 		// ERR_INVALID_HANDLE_TYPE
 		return BT_ERR_INVALID_HANDLE_TYPE;
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	BT_u8 *pSrc = hFifo->pOut;
-	BT_u8 *pDest = pData;
-	BT_u32 ulElementWidth = hFifo->ulElementWidth;
+	const BT_u8 *pSrc = pData;
+	BT_u32 ulWritten;
+	BT_BOOL bHigherPriorityTaskWoken;
+
+	for (ulWritten = 0; ulWritten < ulElements; ulWritten++) {
+		// We should prevent overflow, and block!
+		if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
+			if (BT_FifoIsFull(hFifo->oQueue,pError))
+				return ulWritten;
+		}
+
+		BT_QueueSendFromISR(hFifo->oQueue, pSrc, &bHigherPriorityTaskWoken);
+		pSrc += hFifo->ulElementWidth;
+	}
+
+	return ulWritten;
+}
+
+BT_u32 BT_FifoRead(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError) {
+	if(!isFifoHandle(hFifo)) {
+		// ERR_INVALID_HANDLE_TYPE
+		return BT_ERR_INVALID_HANDLE_TYPE;
+	}
+	if (pError) *pError = BT_ERR_NONE;
+
+	BT_u8 *pSrc = pData;
 	BT_u32 ulRead;
+	BT_BOOL bHigherPriorityTaskWoken;
 
 	// Get bytes from RX buffer very quickly.
 	for (ulRead = 0; ulRead < ulElements; ulRead++) {
-		if(hFifo->pOut == hFifo->pIn) {
+		if(BT_FifoIsEmpty(hFifo, pError)) {
 			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING)
 				return ulRead;
 		}
-		BT_FifoWaitEmpty(hFifo);
-		if (hFifo->ulFlags & BT_FIFO_OVERWRITE) {
-			pSrc = hFifo->pOut;
+		if (BT_GetActiveInterrupt(pError) >= 0)
+			BT_QueueReceiveFromISR(hFifo->oQueue, pData, &bHigherPriorityTaskWoken);
+		else
+			BT_QueueReceive(hFifo->oQueue, pData, -1);
+		pSrc += hFifo->ulElementWidth;
+	}
+	return ulRead;
+}
+
+BT_u32 BT_FifoReadFromISR(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError) {
+	if(!isFifoHandle(hFifo)) {
+		// ERR_INVALID_HANDLE_TYPE
+		return BT_ERR_INVALID_HANDLE_TYPE;
+	}
+	if (pError) *pError = BT_ERR_NONE;
+
+	BT_u8 *pSrc = pData;
+	BT_u32 ulRead;
+	BT_BOOL bHigherPriorityTaskWoken;
+
+	// Get bytes from RX buffer very quickly.
+	for (ulRead = 0; ulRead < ulElements; ulRead++) {
+		if(BT_FifoIsEmpty(hFifo, pError)) {
+			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING)
+				return ulRead;
 		}
-		memcpy(pDest, pSrc, ulElementWidth);
-		pDest += ulElementWidth;
-		pSrc += ulElementWidth;
-		if(pSrc >= hFifo->pEnd) {
-			pSrc = hFifo->pBuf;
-		}
-		hFifo->pOut = pSrc;
+		BT_QueueReceiveFromISR(hFifo->oQueue, pData, &bHigherPriorityTaskWoken);
+		pSrc += hFifo->ulElementWidth;
 	}
 	return ulRead;
 }
@@ -152,7 +177,7 @@ BT_BOOL BT_FifoIsEmpty(BT_HANDLE hFifo, BT_ERROR *pError) {
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	return (hFifo->pIn == hFifo->pOut);
+	return (BT_QueueMessagesWaiting(hFifo->oQueue) == 0);
 }
 
 BT_BOOL BT_FifoIsFull(BT_HANDLE hFifo, BT_ERROR *pError) {
@@ -162,41 +187,9 @@ BT_BOOL BT_FifoIsFull(BT_HANDLE hFifo, BT_ERROR *pError) {
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	BT_u8 *pWrite  = hFifo->pIn + hFifo->ulElementWidth;
-	BT_u8 *pRead = hFifo->pOut;
-
-	if(pWrite >= hFifo->pEnd) {
-		pWrite = hFifo->pBuf;
-	}
-	return (pRead == pWrite);
+	return (hFifo->ulElements == BT_QueueMessagesWaiting(hFifo->oQueue));
 }
 
-BT_ERROR BT_FifoWaitFull(BT_HANDLE hFifo) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	BT_ERROR Error = BT_ERR_NONE;
-
-	while (BT_FifoIsFull(hFifo, &Error))
-		BT_ThreadYield();
-
-	return Error;
-}
-
-BT_ERROR BT_FifoWaitEmpty(BT_HANDLE hFifo) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-
-	BT_ERROR Error = BT_ERR_NONE;
-
-	while (BT_FifoIsEmpty(hFifo, &Error))
-		BT_ThreadYield();
-
-	return Error;
-}
 
 BT_u32 BT_FifoFillLevel(BT_HANDLE hFifo, BT_ERROR *pError) {
 	if(!isFifoHandle(hFifo)) {
@@ -206,17 +199,7 @@ BT_u32 BT_FifoFillLevel(BT_HANDLE hFifo, BT_ERROR *pError) {
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	BT_u8 *pWrite  = hFifo->pIn;
-	BT_u8 *pRead   = hFifo->pOut;
-	BT_u32 ulSize  = (hFifo->pEnd - hFifo->pBuf);
-	BT_u32 ulLevel;
-
-	if (pRead <= pWrite)
-		ulLevel = pWrite - pRead;
-	else
-		ulLevel = ulSize - (pRead - pWrite);
-
-	return ulLevel/hFifo->ulElementWidth;
+	return BT_QueueMessagesWaiting(hFifo->oQueue);
 }
 
 BT_u32 BT_FifoSize(BT_HANDLE hFifo, BT_ERROR *pError) {
@@ -227,12 +210,13 @@ BT_u32 BT_FifoSize(BT_HANDLE hFifo, BT_ERROR *pError) {
 	}
 	if (pError) *pError = BT_ERR_NONE;
 
-	return (hFifo->pEnd - hFifo->pBuf) / hFifo->ulElementWidth;
+
+	return hFifo->ulElements;
 }
 
 
 BT_ERROR fifo_cleanup(BT_HANDLE hFifo) {
-	BT_kFree(hFifo->pBuf);
+	BT_CloseHandle(hFifo->oQueue);
 
 	return BT_ERR_NONE;
 }
