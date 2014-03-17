18770597ccda34181f30a7d06b26be1b9045a87f
James Walmsley <james@fullfat-fs.co.uk>
2014-03-17 11:24:57 +0100

    [COLLECTIONS] FIFO - Improved the BT_FIFO code especially relating to error conditions.

--- lib/src/collections/bt_fifo.c
+++ lib/src/collections/bt_fifo.c
@@ -7,21 +7,14 @@
 #include <collections/bt_fifo.h>
 #include <string.h>
 
-/**
- *	All driver modules in the system shall be tagged with some helpful information.
- *	This way we know who to blame when things go wrong!
- **/
 BT_DEF_MODULE_NAME						("FIFO")
 BT_DEF_MODULE_DESCRIPTION				("Simple fifo for bitthunder")
 BT_DEF_MODULE_AUTHOR					("Robert Steinbauer")
 BT_DEF_MODULE_EMAIL						("rsteinbauer@riegl.com")
 
-
-/**
- **/
 struct _BT_OPAQUE_HANDLE {
-	BT_HANDLE_HEADER h;			///< All handles must include a handle header.
-	BT_HANDLE		 oQueue;						///< Pointer to the start of the ring-buffer.
+	BT_HANDLE_HEADER h;
+	BT_HANDLE		 oQueue;
 	BT_u32	 		 ulElements;
 	BT_u32	 		 ulElementWidth;
 	BT_u32	 		 ulFlags;
@@ -40,7 +33,7 @@ static BT_BOOL isFifoHandle(BT_HANDLE hFifo) {
 BT_HANDLE BT_FifoCreate(BT_u32 ulElements, BT_u32 ulElementWidth, BT_u32 ulFlags, BT_ERROR *pError) {
 
 	BT_HANDLE hFifo;
-	BT_ERROR Error;
+	BT_ERROR Error = BT_ERR_NONE;
 
 	hFifo = BT_CreateHandle(&oHandleInterface, sizeof(struct _BT_OPAQUE_HANDLE), pError);
 
@@ -70,160 +63,216 @@ BT_HANDLE BT_FifoCreate(BT_u32 ulElements, BT_u32 ulElementWidth, BT_u32 ulFlags
 }
 
 BT_u32 BT_FifoWrite(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ERROR *pError) {
+
+	BT_ERROR Error 		= BT_ERR_NONE;
+	const BT_u8 *pSrc 	= pData;
+	BT_u32 ulWritten 	= 0;
+
 	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
 	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	const BT_u8 *pSrc = pData;
-	BT_u32 ulWritten;
-	BT_BOOL bHigherPriorityTaskWoken;
-
-	for (ulWritten = 0; ulWritten < ulElements; ulWritten++) {
+	for(ulWritten = 0; ulWritten < ulElements; ulWritten++) {
 		// We should prevent overflow, and block!
 		if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
-			if (BT_FifoIsFull(hFifo->oQueue,pError))
-				return ulWritten;
+			if (BT_FifoIsFull(hFifo->oQueue,pError)) {
+				break;
+			}
 		}
-		if (BT_GetActiveInterrupt(pError) >= 0)
-			BT_QueueSendFromISR(hFifo->oQueue, pSrc, &bHigherPriorityTaskWoken);
-		else
-			BT_QueueSend(hFifo->oQueue, pSrc, -1);
+
+		BT_QueueSend(hFifo->oQueue, pSrc, -1);
 		pSrc += hFifo->ulElementWidth;
+	}
+
+err_out:
+	if(pError) {
+		*pError = Error;
 	}
 
 	return ulWritten;
 }
 
 BT_u32 BT_FifoWriteFromISR(BT_HANDLE hFifo, BT_u32 ulElements, const void *pData, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	const BT_u8 *pSrc = pData;
-	BT_u32 ulWritten;
+	BT_ERROR Error 		= BT_ERR_NONE;
+	const BT_u8 *pSrc 	= pData;
+	BT_u32 ulWritten 	= 0;
 	BT_BOOL bHigherPriorityTaskWoken;
 
-	for (ulWritten = 0; ulWritten < ulElements; ulWritten++) {
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
+
+	for(ulWritten = 0; ulWritten < ulElements; ulWritten++) {
 		// We should prevent overflow, and block!
 		if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
-			if (BT_FifoIsFull(hFifo->oQueue,pError))
-				return ulWritten;
+			if (BT_FifoIsFull(hFifo->oQueue,pError)) {
+				break;
+			}
 		}
 
 		BT_QueueSendFromISR(hFifo->oQueue, pSrc, &bHigherPriorityTaskWoken);
 		pSrc += hFifo->ulElementWidth;
 	}
 
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
 	return ulWritten;
 }
 
 BT_u32 BT_FifoRead(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	BT_u8 *pSrc = pData;
-	BT_u32 ulRead;
-	BT_BOOL bHigherPriorityTaskWoken;
+	BT_ERROR Error 	= BT_ERR_NONE;
+	BT_u8 *pSrc 	= pData;
+	BT_u32 ulRead 	= 0;
+
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
 
 	// Get bytes from RX buffer very quickly.
-	for (ulRead = 0; ulRead < ulElements; ulRead++) {
+	for(ulRead = 0; ulRead < ulElements; ulRead++) {
 		if(BT_FifoIsEmpty(hFifo, pError)) {
-			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING)
-				return ulRead;
+			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
+				break;
+			}
 		}
-		if (BT_GetActiveInterrupt(pError) >= 0)
-			BT_QueueReceiveFromISR(hFifo->oQueue, pData, &bHigherPriorityTaskWoken);
-		else
-			BT_QueueReceive(hFifo->oQueue, pData, -1);
+		BT_QueueReceive(hFifo->oQueue, pData, BT_INFINITE_TIMEOUT);
 		pSrc += hFifo->ulElementWidth;
 	}
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
 	return ulRead;
 }
 
 BT_u32 BT_FifoReadFromISR(BT_HANDLE hFifo, BT_u32 ulElements, void *pData, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	BT_u8 *pSrc = pData;
-	BT_u32 ulRead;
+	BT_ERROR Error 	= BT_ERR_NONE;
+	BT_u8 *pSrc 	= pData;
+	BT_u32 ulRead 	= 0;
 	BT_BOOL bHigherPriorityTaskWoken;
 
-	// Get bytes from RX buffer very quickly.
-	for (ulRead = 0; ulRead < ulElements; ulRead++) {
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
+
+	for(ulRead = 0; ulRead < ulElements; ulRead++) {
 		if(BT_FifoIsEmpty(hFifo, pError)) {
-			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING)
-				return ulRead;
+			if (hFifo->ulFlags & BT_FIFO_NONBLOCKING) {
+				break;
+			}
 		}
 		BT_QueueReceiveFromISR(hFifo->oQueue, pData, &bHigherPriorityTaskWoken);
 		pSrc += hFifo->ulElementWidth;
 	}
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
 	return ulRead;
 }
 
 BT_BOOL BT_FifoIsEmpty(BT_HANDLE hFifo, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	return (BT_QueueMessagesWaiting(hFifo->oQueue) == 0);
+	BT_ERROR Error = BT_ERR_NONE;
+	BT_u32 messages = 0;
+
+
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
+
+	messages = BT_QueueMessagesWaiting(hFifo->oQueue);
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
+	return Error ? BT_FALSE : (messages == 0);
 }
 
 BT_BOOL BT_FifoIsFull(BT_HANDLE hFifo, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		return BT_ERR_INVALID_HANDLE_TYPE;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	return (hFifo->ulElements == BT_QueueMessagesWaiting(hFifo->oQueue));
+	BT_ERROR Error 	= BT_ERR_NONE;
+	BT_u32 messages = 0;
+
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
+
+	messages = BT_QueueMessagesWaiting(hFifo->oQueue);
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
+	return Error ? BT_FALSE : (hFifo->ulElements == messages);
 }
 
 
 BT_u32 BT_FifoFillLevel(BT_HANDLE hFifo, BT_ERROR *pError) {
-	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		*pError = BT_ERR_INVALID_HANDLE_TYPE;
-		return 0;
-	}
-	if (pError) *pError = BT_ERR_NONE;
 
-	return BT_QueueMessagesWaiting(hFifo->oQueue);
+	BT_ERROR Error = BT_ERR_NONE;
+	BT_u32 messages = 0;
+
+	if(!isFifoHandle(hFifo)) {
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
+	}
+
+	messages = BT_QueueMessagesWaiting(hFifo->oQueue);
+
+err_out:
+	if(pError) {
+		*pError = Error;
+	}
+
+	return messages;
 }
 
 BT_u32 BT_FifoSize(BT_HANDLE hFifo, BT_ERROR *pError) {
+
+	BT_ERROR Error = BT_ERR_NONE;
+
 	if(!isFifoHandle(hFifo)) {
-		// ERR_INVALID_HANDLE_TYPE
-		*pError = BT_ERR_INVALID_HANDLE_TYPE;
-		return 0;
+		Error = BT_ERR_INVALID_HANDLE_TYPE;
+		goto err_out;
 	}
-	if (pError) *pError = BT_ERR_NONE;
 
+err_out:
+	if (pError) {
+		*pError = Error;
+	}
 
-	return hFifo->ulElements;
+	return Error ? 0 : hFifo->ulElements;
 }
 
 
-BT_ERROR fifo_cleanup(BT_HANDLE hFifo) {
+static BT_ERROR fifo_cleanup(BT_HANDLE hFifo) {
 	BT_CloseHandle(hFifo->oQueue);
-
 	return BT_ERR_NONE;
 }
 
 
 static const BT_IF_HANDLE oHandleInterface = {
 	BT_MODULE_DEF_INFO,
-	.eType		= BT_HANDLE_T_FIFO,											///< Handle Type!
-	.pfnCleanup	= fifo_cleanup,												///< Handle's cleanup routine.
+	.eType		= BT_HANDLE_T_FIFO,
+	.pfnCleanup	= fifo_cleanup,
 };
