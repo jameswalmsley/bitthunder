7179e15bc834ed5452c100a64dc937b89913e1ec
James Walmsley <james@fullfat-fs.co.uk>
2014-03-17 10:45:05 +0100

    [MUTEX] Documented the PendMutex API to explain the changes.

--- os/include/process/bt_mutex.h
+++ os/include/process/bt_mutex.h
@@ -2,6 +2,14 @@
 #define _BT_MUTEX_H_
 
 BT_HANDLE 	BT_CreateMutex			(BT_ERROR *pError);
+
+/**
+ *	@brief	Pends on a Mutex resource for the specified timeout.
+ *
+ *	@param[in]	hMutex			HANDLE to Mutex object/event on which to pend.
+ *	@param[in]	oTimeoutTicks	Number of kernel ticks to pend.
+ *
+ **/
 BT_ERROR 	BT_PendMutex			(BT_HANDLE hMutex, BT_TICK oTimeoutTicks);
 BT_ERROR 	BT_ReleaseMutex			(BT_HANDLE hMutex);
 BT_ERROR 	BT_PendMutexRecursive	(BT_HANDLE hMutex, BT_TICK oTimeoutTicks);
