2ba8c95631ecc145a885f4e649a34abbc52e5aa6
James Walmsley <james@fullfat-fs.co.uk>
2014-03-19 09:04:06 +0100

    [SYSCALL] read - Updated read() to support the modern FILE api.

--- os/src/syscall/calls/read.c
+++ os/src/syscall/calls/read.c
@@ -1,5 +1,5 @@
 #include <bitthunder.h>
 
-long bt_sys_read(BT_HANDLE h, BT_u32 len, void *pBuffer, BT_ERROR *pError) {
-	return BT_Read(h, 0, len, pBuffer, pError);
+long bt_sys_read(BT_HANDLE h, BT_u32 ulFlags, BT_u32 len, void *pBuffer) {
+	return BT_Read(h, 0, len, pBuffer);
 }
