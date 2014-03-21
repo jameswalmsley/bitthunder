20de30981549b66a84ad90d0885d99fe73811bda
James Walmsley <james@fullfat-fs.co.uk>
2014-03-21 10:51:25 +0100

    [BUILD] Don't use echo to perform string unquoting. Use make's builtin subst function.

--- .config.mk
+++ .config.mk
@@ -6,9 +6,9 @@ include $(BASE).config
 
 CONFIG_:=BT_CONFIG_
 
-ARCH:=$(shell echo $(BT_CONFIG_ARCH))
-SUBARCH:=$(shell echo $(BT_CONFIG_SUBARCH))
-TOOLCHAIN:=$(shell echo $(BT_CONFIG_TOOLCHAIN))
+ARCH		:=$(subst $(DB_QUOTES),,$(BT_CONFIG_ARCH))
+SUBARCH		:=$(subst $(DB_QUOTES),,$(BT_CONFIG_SUBARCH))
+TOOLCHAIN	:=$(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN))
 
 include $(BASE)kernel/objects.mk
 include $(BASE)os/objects.mk
@@ -16,7 +16,7 @@ include $(BASE)lib/objects.mk
 
 PYTHON:=$(BT_CONFIG_DBUILD_PYTHON)
 
-BSP_DIR:=$(shell echo $(BT_CONFIG_BSP_DIR))
+BSP_DIR:=$(subst $(DB_QUOTES),,$(BT_CONFIG_BSP_DIR))
 
 GIT_DESCRIBE:=$(shell git --git-dir=$(BASE).git describe)
 
@@ -29,16 +29,16 @@ test_git:
 
 include $(BASE)$(BSP_DIR)/objects.mk
 
-CC_MARCH 		:= $(shell echo $(BT_CONFIG_ARCH_ARM_FAMILY))
-CC_MTUNE 		:= $(shell echo $(BT_CONFIG_TOOLCHAIN_CPU))
-CC_TCFLAGS 		:= $(shell echo $(BT_CONFIG_TOOLCHAIN_FLAGS))
-CC_TCDEBUGFLAGS := $(shell echo $(BT_CONFIG_TOOLCHAIN_DEBUG_FLAGS))
-CC_OPTIMISE 	:= $(shell echo $(BT_CONFIG_TOOLCHAIN_OPTIMISATION))
-CC_MACHFLAGS 	:= $(shell echo $(BT_CONFIG_TOOLCHAIN_MACH_FLAGS))
-CC_MFPU			:= $(shell echo $(BT_CONFIG_TOOLCHAIN_MFPU))
-CC_FPU_ABI		:= $(shell echo $(BT_CONFIG_TOOLCHAIN_FPU_ABI))
+CC_MARCH 		:= $(subst $(DB_QUOTES),,$(BT_CONFIG_ARCH_ARM_FAMILY))
+CC_MTUNE		:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_CPU))
+CC_TCFLAGS		:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_FLAGS))
+CC_TCDEBUGFLAGS := $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_DEBUG_FLAGS))
+CC_OPTIMISE		:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_OPTIMISATION))
+CC_MACHFLAGS 	:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_MACH_FLAGS))
+CC_MFPU			:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_MFPU))
+CC_FPU_ABI		:= $(subst $(DB_QUOTES),,$(BT_CONFIG_TOOLCHAIN_FPU_ABI))
 
-$(OBJECTS) $(OBJECTS-y): CFLAGS += -fstack-usage -Wall -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
+$(OBJECTS) $(OBJECTS-y): CFLAGS += -fstack-usage $(CC_WARNING) -I $(BASE)/lib/include/ -I $(BASE)/arch/arm/include/
 $(OBJECTS) $(OBJECTS-y): CFLAGS += $(CC_TCDEBUGFLAGS)
 $(OBJECTS) $(OBJECTS-y): CFLAGS += -march=$(CC_MARCH)
 $(OBJECTS) $(OBJECTS-y): CFLAGS += -mtune=$(CC_MTUNE) $(CC_TCFLAGS) $(CC_OPTIMISE) $(CC_MFPU) $(CC_FPU_ABI)
