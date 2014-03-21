8aeba1977f158a22bcf97997f509efb2bb995f94
James Walmsley <james@fullfat-fs.co.uk>
2014-03-21 11:43:01 +0100

    [DOC] HANDLE - Added doxy comments for the BT_HANDLE API.

--- lib/include/handles/bt_handle.h
+++ lib/include/handles/bt_handle.h
@@ -15,13 +15,76 @@ typedef struct _BT_HANDLE_HEADER {
 	BT_u32					ulClaimedMemory;	///< Amount of memory claimed by this handle.
 } BT_HANDLE_HEADER;
 
-
 #define BT_HANDLE_TYPE(x)	(x->h.pIf->eType)
 
-BT_ERROR 	BT_AttachHandle(BT_HANDLE hProcess, const BT_IF_HANDLE *pIf, BT_HANDLE h);
-BT_HANDLE 	BT_CreateHandle	(const BT_IF_HANDLE *pIf, BT_u32 ulHandleMemory, BT_ERROR *pError);
-BT_ERROR 	BT_DestroyHandle(BT_HANDLE h);
-BT_ERROR 	BT_CloseHandle	(BT_HANDLE hHandle);
+/**
+ *	@brief	Attach a BT_HANDLE to a process.
+ *
+ *	This function can be used to attach a manually created handle to a running process.
+ *	If you created a BT_HANDLE using the @ref BT_CreateHandle() function then you don't
+ *	need to invoke this function.
+ *
+ *	This function also initialises the HANDLE header to use the HANDLE interface that
+ *	required to make the handle work.
+ *
+ *	@param[in] 	hProcess	Handle of the process that the handle should be attached to.
+ *	@param[in]	pIf			The Handle interface that your HANDLE (h) will use.
+ *	@param[in]	h			The Handle that is to be attached.
+ *
+ *	@return	BT_ERR_NONE on success.
+ *
+ **/
+BT_ERROR BT_AttachHandle(BT_HANDLE hProcess, const BT_IF_HANDLE *pIf, BT_HANDLE h);
+
+/**
+ *	@brief 	Create and Attach a new BT_HANDLE to currently running process.
+ *
+ *	This function is really an allocator, and will create a HANDLE object and initialise
+ *	it with the required HANDLE interface.
+ *
+ *	@param[in]		pIf				The Handle interface that the HANDLE will use.
+ *	@param[in]		ulHandleMemory	The total size of the handle object to be created in bytes.
+ *	@param[out,opt]	pError			A BT_ERROR code to provide information about any failure.
+ *
+ *	@return A valid, initialised and attached BT_HANDLE object ready for use.
+ *	@return NULL on error, check the value of pError.
+ *
+ **/
+BT_HANDLE BT_CreateHandle(const BT_IF_HANDLE *pIf, BT_u32 ulHandleMemory, BT_ERROR *pError);
+
+/**
+ *	@brief	Destroy a BT_HANDLE object, and detach it from any associated process.
+ *
+ *	This function is used to destroy a BT_HANDLE cleanly. It will correctly detach the handle
+ *	from any associated processes, and if applicable notify any HANDLE notifiers.
+ *
+ *	@note 	This function does not invoke the HANDLE cleanup mechanism!
+ *	@note	In most cases you should use @ref BT_CloseHandle()
+ *
+ *	@param[in]	h	Handle to be destroyed.
+ *
+ *	@return	BT_ERR_NONE on success.
+ **/
+BT_ERROR BT_DestroyHandle(BT_HANDLE h);
+
+/**
+ *	@brief	Gracefully cleanup, detach and destroy a BT_HANDLE object.
+ *
+ *	This function acts as the primary destructor for any HANDLE (objects) within the
+ *	BitThunder operating system.
+ *
+ *	All HANDLE notifiers are notified.
+ *	All cleanup routines for the handle are invoked.
+ *	The HANDLE is detached.
+ *	The HANDLE is destroyed (if applicable).
+ *
+ *	@param[in]	h	Handle to be closed.
+ *
+ *	@return	BT_ERR_NONE on sucess.
+ *
+ **/
+BT_ERROR BT_CloseHandle(BT_HANDLE h);
+
 
 BT_HANDLE 	BT_CreateMux(BT_u32 ulFlags, BT_ERROR *pError);
 BT_ERROR 	BT_MuxOpen(BT_HANDLE hMux, const BT_i8 *path);
