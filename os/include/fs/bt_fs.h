f81574439cd8d60be91fe99c3cf0a60144c55823
James Walmsley <james@fullfat-fs.co.uk>
2014-04-01 10:03:42 +0200

    [FS] Added a BT_OpenFile() api macro, accepts the stdio mode strings directly.

--- os/include/fs/bt_fs.h
+++ os/include/fs/bt_fs.h
@@ -17,12 +17,15 @@ typedef struct _BT_MOUNTPOINT {
 	BT_HANDLE 		hMount;
 	BT_i8 		   *szpPath;
 	BT_FILESYSTEM  *pFS;
+	BT_HANDLE		hVolume;
 } BT_MOUNTPOINT;
 
 BT_ERROR BT_RegisterFilesystem(BT_HANDLE hFS);
 
 BT_u32 BT_GetModeFlags(const BT_i8 *mode);
 BT_HANDLE BT_Open(const BT_i8 *szpPath, BT_u32 mode, BT_ERROR *pError);
+#define BT_OpenFile(path, mode_string, perror)	BT_Open(path, BT_GetModeFlags(mode_string), perror)
+
 BT_ERROR BT_Mount(const BT_i8 *src, const BT_i8 *target, const BT_i8 *filesystem, BT_u32 mountflags, const void *data);
 BT_ERROR BT_MkDir(const BT_i8 *szpPath);
 BT_ERROR BT_RmDir(const BT_i8 *szpPath);
