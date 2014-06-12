6195284ea45190fd87eb4bfb1581f52e0bc7d8d4
Robert Steinbauer <rsteinbauer@riegl.com>
2014-06-12 11:48:54 +0200

    [MACH] LM3Sxx - Drivers for the LM3Sxx series

--- arch/arm/mach/lm3sxx/spi.c
+++ arch/arm/mach/lm3sxx/spi.c
@@ -415,6 +415,31 @@ static BT_ERROR spi_setup(BT_HANDLE hspi, BT_SPI_DEVICE *pDevice)
 	return spi_setup_transfer(hspi, pDevice, NULL);
 }
 
+static BT_ERROR spi_setup(BT_HANDLE hspi, BT_SPI_DEVICE *pDevice)
+{
+	if (pDevice->mode & SPI_LSB_FIRST)
+		return BT_ERR_INVALID_VALUE;
+
+	if (!pDevice->max_speed_hz)
+		return BT_ERR_INVALID_VALUE;
+
+	if (!pDevice->bits_per_word)
+		pDevice->bits_per_word = 8;
+
+	return spi_setup_transfer(hspi, pDevice, NULL);
+}
+
+
+/**
+ *	Make the SPI active (Set the Enable bit).
+ **/
+static BT_ERROR spi_init_hw(BT_HANDLE hSpi) {
+	volatile LM3Sxx_SPI_REGS *pRegs = hSpi->pRegs;
+
+	pRegs->CR1 |= LM3Sxx_SPI_CR1_SSP_ENABLE;
+
+	return BT_ERR_NONE;
+}
 
 /**
  *	Make the SPI active (Set the Enable bit).
@@ -476,6 +501,8 @@ static BT_HANDLE spi_probe(const BT_INTEGRATED_DEVICE *pDevice, BT_ERROR *pError
 		Error = BT_ERR_GENERIC;
 		goto err_free_out;
 	}
+
+	hSpi->spi_master.bus_num = pResource->ulStart;
 
 	hSpi = BT_CreateHandle(&oHandleInterface, sizeof(struct _BT_OPAQUE_HANDLE), pError);
 	if(!hSpi) {
