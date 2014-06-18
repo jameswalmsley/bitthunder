45a5c4610821a7bae1f54e5f2864989f2a4f4b60
James Walmsley <james@fullfat-fs.co.uk>
2014-06-18 11:01:04 +0200

    [ARCH] ARM - types - Corrected BT_s16 type.

--- arch/arm/include/bt_arch_types.h
+++ arch/arm/include/bt_arch_types.h
@@ -1,8 +1,8 @@
 #ifndef _BT_ARCH_TYPES_H_
 #define _BT_ARCH_TYPES_H_
 
-typedef long long			BT_i64;
 typedef unsigned long long	BT_u64;
+typedef long long			BT_i64;
 typedef signed long long	BT_s64;
 
 typedef unsigned long		BT_u32;
@@ -10,13 +10,12 @@ typedef signed long			BT_s32;
 typedef long				BT_i32;
 
 typedef unsigned short		BT_u16;
-typedef signed short		BT_d16;
+typedef signed short		BT_s16;
 typedef short				BT_i16;
 
 typedef unsigned char		BT_u8;
 typedef signed char			BT_s8;
 typedef	char				BT_i8;
-
 
 /*
  *	Kernel Mode types used for MM code.
