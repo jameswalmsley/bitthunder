2859c178e192ae63ff9f95b46d0c3b9a4150a42c
James Walmsley <james@fullfat-fs.co.uk>
2014-06-23 17:10:19 +0200

    [LIB] putc|printf - Added symbol exports.

--- os/src/lib/putc.c
+++ os/src/lib/putc.c
@@ -1,10 +1,3 @@
-/*
- * putc.c
- *
- *  Created on: Mar 16, 2013
- *      Author: wl
- */
-
 #include <bitthunder.h>
 #include <lib/putc.h>
 
@@ -12,31 +5,37 @@ BT_ERROR BT_SetStdin(BT_HANDLE h) {
 	BT_SetFileDescriptor(0, h);
 	return BT_ERR_NONE;
 }
+BT_EXPORT_SYMBOL(BT_SetStdin);
 
 BT_HANDLE BT_GetStdin() {
 	BT_ERROR Error;
 	return BT_GetFileDescriptor(0, &Error);
 }
+BT_EXPORT_SYMBOL(BT_GetStdin);
 
 BT_ERROR BT_SetStdout(BT_HANDLE h) {
 	BT_SetFileDescriptor(1, h);
 	return BT_ERR_NONE;
 }
+BT_EXPORT_SYMBOL(BT_SetStdout);
 
 BT_HANDLE BT_GetStdout() {
 	BT_ERROR Error;
 	return BT_GetFileDescriptor(1, &Error);
 }
+BT_EXPORT_SYMBOL(BT_GetStdout);
 
 BT_ERROR BT_SetStderr(BT_HANDLE h) {
 	BT_SetFileDescriptor(2, h);
 	return BT_ERR_NONE;
 }
+BT_EXPORT_SYMBOL(BT_SetStderr);
 
 BT_HANDLE BT_GetStderr() {
 	BT_ERROR Error;
 	return BT_GetFileDescriptor(2, &Error);
 }
+BT_EXPORT_SYMBOL(BT_GetStderr);
 
 static int fgetc(void *stream) {
 	BT_HANDLE h = (BT_HANDLE) stream;
@@ -64,10 +63,12 @@ int bt_getc(void) {
 
 	return 0;
 }
+BT_EXPORT_SYMBOL(bt_getc);
 
 int bt_fgetc(void *stream) {
 	return fgetc(stream);
 }
+BT_EXPORT_SYMBOL(bt_fgetc);
 
 void bt_putc(int c) {
 	BT_HANDLE h = BT_GetStdout();
@@ -75,8 +76,9 @@ void bt_putc(int c) {
 		fputc(c, h);
 	}
 }
-
+BT_EXPORT_SYMBOL(bt_putc);
 
 void bt_fputc(int c, void *stream) {
 	fputc(c, stream);
 }
+BT_EXPORT_SYMBOL(bt_fputc);
