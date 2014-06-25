a9452cc462bd674d52d1630ecb3a8bd89d1b2df2
James Walmsley <james@fullfat-fs.co.uk>
2014-06-25 11:07:06 +0200

    [SYSCALL] Fixed some definitions in the syscalls.

--- os/include/syscall/bt_syscall.h
+++ os/include/syscall/bt_syscall.h
@@ -10,15 +10,19 @@ long bt_sys_write(BT_HANDLE h, BT_u32 len, const void *pBuffer, BT_ERROR *pError
 long bt_sys_klog(const BT_i8 *path);
 long bt_sys_sleep(BT_u32 ticks);
 long bt_sys_gpioset(BT_u32 flag, BT_BOOL state);
+long bt_sys_gettimeofday(struct bt_timeval *tv, struct bt_timezone *tz);
+long bt_sys_settimeofday(struct bt_timeval *tv, struct bt_timezone *tz);
 
-#define BT_SYS_yield	0
-#define BT_SYS_getpid	1
-#define BT_SYS_open		2
-#define BT_SYS_close	3
-#define BT_SYS_read		4
-#define BT_SYS_write	5
-#define BT_SYS_klog		6
-#define BT_SYS_sleep	7
-#define BT_SYS_gpioset	8
+#define BT_SYS_yield		0
+#define BT_SYS_getpid		1
+#define BT_SYS_open			2
+#define BT_SYS_close		3
+#define BT_SYS_read			4
+#define BT_SYS_write		5
+#define BT_SYS_klog			6
+#define BT_SYS_sleep		7
+#define BT_SYS_gpioset		8
+#define BT_SYS_gettimeofday	9
+#define BT_SYS_settimeofday	10
 
 #endif
