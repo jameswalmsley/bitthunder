0135c2668331a77fc37411f7a1af0320e0f1170c
James Walmsley <james@fullfat-fs.co.uk>
2014-09-16 15:08:25 +0200

    [SYSCALL] write - Provided POSIX compliant write() syscall.

--- os/src/syscall/calls/write.c
+++ os/src/syscall/calls/write.c
@@ -1,5 +1,19 @@
 #include <bitthunder.h>
+#include <syscall/errno.h>
 
-long bt_sys_write(BT_HANDLE h, BT_u32 len, void *pBuffer, BT_ERROR *pError) {
-	return BT_Write(h, 0, len, pBuffer);
+long bt_sys_write(int fd, const void *ptr, size_t count) {
+
+	long written = 0;
+
+	BT_ERROR Error = BT_ERR_NONE;
+	BT_HANDLE hFile = BT_GetFileDescriptor(fd, &Error);
+	if(!hFile) {
+		return -EBADF;
+	}
+
+	BT_s32 slWritten = BT_Write(hFile, 0, count, &Error);
+
+	// Convert return code to POSIX error code.
+
+	return (long) slWritten;
 }
