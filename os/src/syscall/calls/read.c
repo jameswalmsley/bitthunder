a301279380adb4ac0ef87d9b3e5495935cf60891
James Walmsley <james@fullfat-fs.co.uk>
2014-09-16 15:07:50 +0200

    [SYSCALL] read - Provided POSIX compliant read() syscall.

--- os/src/syscall/calls/read.c
+++ os/src/syscall/calls/read.c
@@ -1,5 +1,27 @@
 #include <bitthunder.h>
+#include <syscall/errno.h>
 
-long bt_sys_read(BT_HANDLE h, BT_u32 ulFlags, BT_u32 len, void *pBuffer) {
-	return BT_Read(h, 0, len, pBuffer);
+/**
+ *	@brief	POSIX read() call.
+ *
+ *	This function implements a fully POSIX compliant READ() call for the BitThunder ABI 0.9.2+.
+ *	Note, that the userspace calls call directly into this from read() (which becomes a trap in the libc).
+ *	However, when calling read() from kernel-space, the the libc simply directs to bt_sys_read().
+ *
+ **/
+long bt_sys_read(int fd, void *ptr, size_t count) {
+
+	long read = 0;
+
+	BT_ERROR Error = BT_ERR_NONE;
+	BT_HANDLE hFile = BT_GetFileDescriptor(fd, &Error);
+	if(!hFile) {
+		return -EBADF;
+	}
+
+	BT_s32 slRead = BT_Read(hFile, 0, count, ptr);
+
+	// Convert return code to POSIX error code.
+
+	return (long) slRead;
 }
