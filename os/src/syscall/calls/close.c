2b6d7c03e4c9ab6a7924d752627b3811d03207f6
James Walmsley <james@fullfat-fs.co.uk>
2014-09-16 15:07:19 +0200

    [SYSCALL] close - Provided POSIX compliant close() syscall.

--- os/src/syscall/calls/close.c
+++ os/src/syscall/calls/close.c
@@ -1,6 +1,11 @@
 #include <bitthunder.h>
 
-long bt_sys_close(BT_HANDLE h) {
-	BT_CloseHandle(h);
+long bt_sys_close(int fd) {
+	BT_ERROR Error = BT_ERR_NONE;
+	BT_HANDLE hFile = BT_GetFileDescriptor(fd, &Error);
+	BT_SetFileDescriptor(fd, NULL);
+	BT_CloseHandle(hFile);
+	BT_FreeFileDescriptor(fd);
+
 	return 0;
 }
