af984e256a87cbcdbdb82d7c4517788310bb8952
James Walmsley <james@fullfat-fs.co.uk>
2014-10-21 11:21:13 +0200

    [MICROPYTHON] Added build objects, and corrected line-endings for BitThunder convention.

---
+++ os/src/shell/micropython/objects.mk
@@ -0,0 +1,132 @@
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/nlrthumb.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/nlrsetjmp.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/malloc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/gc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/qstr.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/vstr.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/unicode.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/mpz.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/lexer.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/lexerstr.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/lexerunix.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/parse.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/parsehelper.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/scope.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/compile.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitcommon.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitpass1.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitcpy.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitbc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/asmx64.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitnative.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/asmx86.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitnative.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/asmthumb.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitnative.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitinlinethumb.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/asmarm.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitnative.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/formatfloat.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/parsenumbase.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/parsenum.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/emitglue.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/runtime.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/nativeglue.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/stackctrl.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/argcheck.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/map.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/obj.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objarray.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objbool.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objboundmeth.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objcell.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objclosure.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objcomplex.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objdict.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objenumerate.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objexcept.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objfilter.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objfloat.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objfun.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objgenerator.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objgetitemiter.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objint.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objint_longlong.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objint_mpz.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objlist.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objmap.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objmodule.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objobject.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objproperty.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objnone.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objnamedtuple.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objrange.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objreversed.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objset.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objslice.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objstr.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objstrunicode.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objstringio.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objtuple.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objtype.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/objzip.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/opmethods.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/sequence.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/stream.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/binary.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/builtin.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/builtinimport.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/builtinevex.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/builtintables.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modarray.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modcollections.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modgc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modio.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modmath.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modcmath.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modmicropython.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modstruct.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/modsys.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/vm.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/bc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/showbc.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/repl.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/smallint.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/pfenv.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/pfenv_printf.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/../extmod/moductypes.o
+#MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/../extmod/modzlibd.o
+MICROPYTHON_OBJECTS += $(BUILD_DIR)/os/src/shell/micropython/py/../extmod/modujson.o
+
+
+MP_HEADER_BUILD = $(BUILD_DIR)/os/src/shell/micropython/bitthunder/genhdr
+MP_PY_SRC = $(BASE)/os/src/shell/micropython/py
+
+$(MICROPYTHON_OBJECTS): CFLAGS += -std=gnu99 -I $(BASE)/os/src/shell/micropython/py -I $(BASE)/os/src/shell/micropython/bitthunder -I $(BUILD_DIR)/os/src/shell/micropython/bitthunder
+$(MICROPYTHON_OBJECTS): MODULE_NAME=MicroPython
+
+
+OBJECTS += $(MICROPYTHON_OBJECTS)
+
+
+$(MP_HEADER_BUILD):
+	mkdir -p $@
+
+MP_FORCE:
+.PHONY: MP_FORCE
+
+
+MP_PY_QSTR_DEFS = $(MP_PY_SRC)/qstrdefs.h
+MP_QSTR_DEFS = $(BASE)/os/src/shell/micropython/bitthunder/qstrdefsport.h
+
+$(MP_HEADER_BUILD)/py-version.h: MP_FORCE
+	$(Q)$(MP_PY_SRC)/py-version.sh > $@.tmp
+	$(Q)if [ -f "$@" ] && cmp -s $@ $@.tmp; then rm $@.tmp; else echo "Generating $@"; mv $@.tmp $@; fi
+
+
+$(MP_HEADER_BUILD)/qstrdefs.generated.h: CFLAGS += -E
+$(MP_HEADER_BUILD)/qstrdefs.generated.h: $(MP_PY_QSTR_DEFS) $(MP_QSTR_DEFS) $(MP_PY_SRC)/makeqstrdata.py $(BASE)/os/src/shell/micropython/bitthunder/mpconfigport.h $(MP_PY_SRC)/mpconfig.h | $(MP_HEADER_BUILD)
+	$(Q)$(CC) $(CFLAGS) $(MP_PY_QSTR_DEFS) -o $(MP_HEADER_BUILD)/qstrdefs.preprocessed.h
+	$(Q)$(PYTHON) $(MP_PY_SRC)/makeqstrdata.py $(MP_HEADER_BUILD)/qstrdefs.preprocessed.h $(MP_QSTR_DEFS) > $@
+
+$(MICROPYTHON_OBJECTS): | $(MP_HEADER_BUILD)/qstrdefs.generated.h $(MP_HEADER_BUILD)/py-version.h
