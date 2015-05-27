a4339d1d3bbb9161507ff5c23c19c90ce7ba5d41
James Walmsley <james@fullfat-fs.co.uk>
2015-05-28 00:48:18 +0200

    [DBUILD] Ensure mkconfig is built for local platform.

--- .dbuild/dbuild.mk
+++ .dbuild/dbuild.mk
@@ -162,7 +162,8 @@ mkconfig:
 	$(Q)mkdir -p $(CONFIG_HEADER_PATH)
 	$(Q)$(BUILD_ROOT).dbuild/scripts/mkconfig/mkconfig $(BASE)/ > $(CONFIG_HEADER_PATH)/$(CONFIG_HEADER_NAME)
 
-$(CONFIG_HEADER_PATH)/$(CONFIG_HEADER_NAME): $(CONFIG_PATH)/.config | dbuild_splash
+$(CONFIG_HEADER_PATH)/$(CONFIG_HEADER_NAME): $(CONFIG_PATH)/.config $(DBUILD_ROOT).dbuild/scripts/mkconfig/mkconfig | dbuild_splash
+$(CONFIG_HEADER_PATH)/$(CONFIG_HEADER_NAME):
 	$(Q)$(PRETTY) --dbuild "CONF" $(MODULE_NAME) $(subst $(BASE)/,"",$@)
 	$(Q)$(MAKE) mkconfig
 
